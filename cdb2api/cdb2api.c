88065ceab297a2481d4e9b816b10f4c3359d32c9
Isaac Elbaz <isaac@odemgroup.com>
2017-05-23 23:59:08 -0400

    avoid overflow in hosts

--- cdb2api/cdb2api.c
+++ cdb2api/cdb2api.c
@@ -4763,7 +4763,7 @@ int cdb2_open(cdb2_hndl_tp **handle, const char *dbname, const char *type,
         char *p;
         p = strchr(type, ':');
         if (p) {
-            strcpy(hndl->hosts[0], type);
+            strncpy(hndl->hosts[0], type, sizeof(hndl->hosts[0]));
             p = strchr(hndl->hosts[0], ':');
             *p = 0;
             hndl->ports[0] = atoi(p + 1);
