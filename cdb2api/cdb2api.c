2a2455475267e529da88336bece23e7983819bcc
Isaac Elbaz <script3r@gmail.com>
2017-05-24 17:57:15 +0000

    prevent overflowing in the case of a type

--- cdb2api/cdb2api.c
+++ cdb2api/cdb2api.c
@@ -4768,7 +4768,7 @@ int cdb2_open(cdb2_hndl_tp **handle, const char *dbname, const char *type,
             *p = 0;
             hndl->ports[0] = atoi(p + 1);
         } else {
-            strcpy(hndl->hosts[0], type);
+            strncpy(hndl->hosts[0], type, sizeof(hndl->hosts[0])-1);
             if (!allow_pmux_route) {
                 hndl->ports[0] =
                     cdb2portmux_get(type, "comdb2", "replication", dbname);
