847a386808a448b2de81a144bf8d529c78b28b03
Michael Ponomarenko <mponomarenko@bloomberg.net>
2017-06-01 20:33:55 -0400

    EVENTLOG: More replayer work.

--- common.mk
+++ common.mk
@@ -101,7 +101,6 @@ GENH+=$(dfp_MEMGEN)
 # cson Module
 cson_SOURCES:=cson/cson_amalgamation_core.c
 cson_OBJS:=$(patsubst %.c,%.o,$(cson_SOURCES))
-$(cson_OBJS): CFLAGS+=-O2
 
 cson/libcson.a: $(cson_OBJS)
 	$(AR) $(ARFLAGS) $@ $^
