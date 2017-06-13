052afaf48fac7a28242e0d1303f995be8a0d9578
Michael Ponomarenko <mponomarenko@bloomberg.net>
2017-06-13 16:18:27 -0400

    fix client_context testcase

--- tests/client_context.test/cdb2_client.c
+++ tests/client_context.test/cdb2_client.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 #include <string.h>
 
-void fail()
+void fail(const char *msg)
 {
+    fprintf(stderr, "%s\n", msg);
     exit(1);
 }
 
-void likely(int rc, int expected)
+void likely(int rc, int expected, char *msg)
 {
-    if (rc != expected) fail();
+    if (rc != expected) fail(msg);
 }
 
 void opendb(cdb2_hndl_tp **hndl, const char *db, const char *type)
@@ -22,7 +23,7 @@ void opendb(cdb2_hndl_tp **hndl, const char *db, const char *type)
     ret = cdb2_open(hndl, db, type, 0);
 
     if (ret != 0) {
-        fail(*hndl, "cdb2_open");
+        fail("cdb2_open");
     }
 }
 
@@ -36,7 +37,7 @@ void exec(cdb2_hndl_tp *hndl, const char *sql)
     ret = cdb2_run_statement(hndl, sql);
 
     if (ret != 0) {
-        fail(hndl, "cdb2_run_statement");
+        fail("cdb2_run_statement");
     }
 
     while (1) {
@@ -52,11 +53,11 @@ void exec(cdb2_hndl_tp *hndl, const char *sql)
             case CDB2_CSTRING:
                 printf("%s\n", (const char *)cdb2_column_value(hndl, 0));
                 break;
-            default: fail(hndl, "cdb2_run_statement");
+            default: fail("cdb2_run_statement");
             }
             break;
         case CDB2_OK_DONE: return;
-        default: fail(hndl, "cdb2_run_statement");
+        default: fail("cdb2_run_statement");
         }
     }
 
@@ -75,43 +76,43 @@ int main(int argc, char **argv)
 
     cdb2_hndl_tp *hndl = NULL;
 
-    opendb(&hndl, argv[1], "default");
+    opendb(&hndl, argv[1], "local");
 
     /* Run a command without any client context message. */
     exec(hndl, "SELECT 1");
 
     /* Push a context message. */
-    likely(cdb2_push_context(hndl, "1"), 0);
+    likely(cdb2_push_context(hndl, "1"), 0, "can't push context");
     exec(hndl, "SELECT 1");
 
     /* Fill the stack buffer. */
     for (i = 2; i <= 10; i++) {
         sprintf(buf, "%d", i);
-        likely(cdb2_push_context(hndl, buf), 0);
+        likely(cdb2_push_context(hndl, buf), 0, "can't push context (2)");
     }
     exec(hndl, "SELECT 1");
 
     /* Check stack buffer overflow. */
-    likely(cdb2_push_context(hndl, "MUST NOT BE LOGGED!"), 1);
+    likely(cdb2_push_context(hndl, "MUST NOT BE LOGGED!"), 1, "push didn't fail");
     exec(hndl, "SELECT 1");
 
     /* Remove the message at the top. */
-    likely(cdb2_pop_context(hndl), 0);
+    likely(cdb2_pop_context(hndl), 0, "can't pop");
     exec(hndl, "SELECT 1");
 
     /* Clear all context messages. */
-    likely(cdb2_clear_contexts(hndl), 0);
+    likely(cdb2_clear_contexts(hndl), 0, "can't clear");
 
     /* Check stack buffer underflow. */
-    likely(cdb2_push_context(hndl, "1"), 0);
-    likely(cdb2_pop_context(hndl), 0);
-    likely(cdb2_pop_context(hndl), 1);
+    likely(cdb2_push_context(hndl, "1"), 0, "can't push contest (3)");
+    likely(cdb2_pop_context(hndl), 0, "can't pop (2)");
+    likely(cdb2_pop_context(hndl), 1, "pop didn't fail");
     exec(hndl, "SELECT 1");
 
     /* Test a long message. */
     memset(buf, '#', sizeof(buf));
     buf[sizeof(buf) - 1] = 0;
-    likely(cdb2_push_context(hndl, buf), 0);
+    likely(cdb2_push_context(hndl, buf), 0, "can't push (4)");
     exec(hndl, "SELECT 1");
 
     cdb2_close(hndl);
