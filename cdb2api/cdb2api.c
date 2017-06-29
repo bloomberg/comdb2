3e153ae11b50ea4f0e14084221f4631f7f6ca210
Mohit Khullar <mkhullar1@bloomberg.net>
2017-06-29 16:37:44 -0400

    Add wait for dbinfo queries

--- cdb2api/cdb2api.c
+++ cdb2api/cdb2api.c
@@ -4259,6 +4259,7 @@ static int cdb2_get_dbhosts(cdb2_hndl_tp *hndl)
     int comdb2db_ports[MAX_NODES];
     int num_comdb2db_hosts;
     int master = 0, rc = 0;
+    int num_retry = 0;
     int comdb2db_num = COMDB2DB_NUM;
     char comdb2db_name[32] = COMDB2DB;
 
@@ -4309,6 +4310,13 @@ static int cdb2_get_dbhosts(cdb2_hndl_tp *hndl)
         }
     }
 
+retry:
+    if (rc && num_retry < MAX_RETRIES){
+       num_retry++;
+       poll(NULL, 0, 250); // Sleep for 250ms everytime and total of 5 seconds
+    } else if (rc) {
+       return rc;
+    }
     if (hndl->num_hosts == 0) {
         int i = 0;
         if (!master) {
@@ -4326,7 +4334,7 @@ static int cdb2_get_dbhosts(cdb2_hndl_tp *hndl)
         if (rc != 0) {
             sprintf(hndl->errstr, "cdb2_get_dbhosts: can't do dbinfo "
                                   "query on comdb2db hosts.");
-            return -1;
+            goto retry;
         }
 
         rc = -1;
@@ -4352,7 +4360,7 @@ static int cdb2_get_dbhosts(cdb2_hndl_tp *hndl)
             sprintf(hndl->errstr,
                     "cdb2_get_dbhosts: can't do newsql query on %s hosts.",
                     comdb2db_name);
-            return -1;
+            goto retry;
         }
     }
 
@@ -4379,7 +4387,7 @@ static int cdb2_get_dbhosts(cdb2_hndl_tp *hndl)
         sprintf(hndl->errstr,
                 "cdb2_get_dbhosts: can't do dbinfo query on %s hosts.",
                 hndl->dbname);
-        return rc;
+        goto retry;
     }
     return rc;
 }
