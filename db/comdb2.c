c069b953d2b2966c0afcfd49c84243f683202393
Adi Zaimi <azaimi@bloomberg.net>
2017-07-18 19:35:40 +0000

    put back lrlname as non const parameter to newdbenv

--- db/comdb2.c
+++ db/comdb2.c
@@ -3594,7 +3594,7 @@ int llmeta_dump_mapping_table(struct dbenv *dbenv, const char *table, int err)
     return llmeta_dump_mapping_table_tran(NULL, dbenv, table, err);
 }
 
-static struct dbenv *newdbenv(char *dbname, const char *lrlname)
+static struct dbenv *newdbenv(char *dbname, char *lrlname)
 {
     int rc;
     struct dbenv *dbenv = calloc(1, sizeof(struct dbenv));
