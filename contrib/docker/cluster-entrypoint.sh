6b471f97c792b4b4aa50ad0f38d52d8464ec9818
Tim Mui <timmui123@gmail.com>
2017-08-09 17:53:38 -0400

    REMOVE: docker debugging

--- contrib/docker/cluster-entrypoint.sh
+++ contrib/docker/cluster-entrypoint.sh
@@ -3,5 +3,14 @@
 # Start pmux
 pmux -l
 
+# Route core dumps into dumps volume
+echo '/dumps/core.%e.%p' | tee /proc/sys/kernel/core_pattern
+ulimit -c unlimited
+
 # Start DB
-comdb2 --lrl /db/testdb.lrl testdb
+comdb2 --lrl /db/testdb.lrl testdb &
+
+# Keep Container Alive for debugging
+while :; do
+    sleep 1
+done
