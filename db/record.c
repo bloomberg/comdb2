f6330256515c6834acf18b83fdb1398ce79b7d65
Michael Ponomarenko <mponomarenko@bloomberg.net>
2017-08-14 12:17:41 -0400

    Formatting.

--- db/record.c
+++ db/record.c
@@ -201,7 +201,8 @@ add_record_int(struct ireq *iq, void *trans, const uint8_t *p_buf_tag_name,
             ERR;
         }
 
-        if (iq->usedb == iq->origdb && iq->usedb->tableversion != iq->usedbtablevers) {
+        if (iq->usedb == iq->origdb &&
+            iq->usedb->tableversion != iq->usedbtablevers) {
             if (iq->debug)
                 reqprintf(iq, "Stale buffer: usedb version %d "
                               "vs curr ver %d\n",
@@ -852,7 +853,8 @@ int upd_record(struct ireq *iq, void *trans, void *primkey, int rrn,
         goto err;
     }
 
-    if (iq->usedb == iq->origdb && iq->usedb->tableversion != iq->usedbtablevers) {
+    if (iq->usedb == iq->origdb &&
+        iq->usedb->tableversion != iq->usedbtablevers) {
         if (iq->debug)
             reqprintf(iq, "Stale buffer: usedb version %d "
                           "vs curr ver %d\n",
@@ -1837,7 +1839,8 @@ int del_record(struct ireq *iq, void *trans, void *primkey, int rrn,
         goto err;
     }
 
-    if (iq->usedb == iq->origdb && iq->usedb->tableversion != iq->usedbtablevers) {
+    if (iq->usedb == iq->origdb &&
+        iq->usedb->tableversion != iq->usedbtablevers) {
         if (iq->debug)
             reqprintf(iq, "Stale buffer: usedb version %d "
                           "vs curr ver %d\n",
