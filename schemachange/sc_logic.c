bloomberg/comdb2

commit 24defa59db4c9e2460298c7e134bc397ab168819

Fix DDL schema locking bugs
* Add table-name locking for DDL stmts on replicant (like DML)
* Send usedb for alter/truncate/drop/rebuild (detect stale schema on master)
* Run one schemachange transaction at a time on master

schemachange/sc_logic.c
@@ -372,20 +372,45 @@ static int do_finalize(ddl_t func, struct ireq *iq, tran_type *input_tran,
     return rc;
 }
 
+static int check_table_version(struct ireq *iq)
+{
+    if (iq->sc->addonly)
+        return 0;
+    int rc, bdberr;
+    unsigned long long version;
+    rc = bdb_table_version_select(iq->sc->table, NULL, &version, &bdberr);
+    if (rc != 0) {
+        errstat_set_strf(&iq->errstat,
+                         "failed to get version for table:%s rc:%d\n",
+                         iq->sc->table, rc);
+        iq->errstat.errval = ERR_SC;
+        return SC_INTERNAL_ERROR;
+    }
+    if (iq->usedbtablevers != version) {
+        errstat_set_strf(&iq->errstat,
+                         "stale version for table:%s master:%d replicant:%d\n",
+                         iq->sc->table, version, iq->usedbtablevers);
+        iq->errstat.errval = ERR_SC;
+        return SC_INTERNAL_ERROR;
+    }
+    return 0;
+}
+
 static int do_ddl(ddl_t pre, ddl_t post, struct ireq *iq, tran_type *tran,
                   scdone_t type)
 {
     int rc;
     struct schema_change_type *s = iq->sc;
-
     if (s->finalize_only) {
         return s->sc_rc;
     }
     if (type != alter) wrlock_schema_lk();
     set_original_tablename(s);
+    if ((rc = check_table_version(iq)) != 0) { // non-tran ??
+        goto end;
+    }
     if (!s->resume) set_sc_flgs(s);
     if ((rc = mark_sc_in_llmeta_tran(s, NULL))) goto end; // non-tran ??
-    propose_sc(s);
     rc = pre(iq, NULL); // non-tran ??
     if (type == alter && master_downgrading(s)) {
         s->sc_rc = SC_MASTER_DOWNGRADE;
@@ -401,7 +426,6 @@ static int do_ddl(ddl_t pre, ddl_t post, struct ireq *iq, tran_type *tran,
 end:
     s->sc_rc = rc;
     if (type != alter) unlock_schema_lk();
-    broadcast_sc_end(sc_seed);
     return rc;
 }
 

bbinc/list.h
@@ -106,6 +106,8 @@ extern int listc_size(void *list);
 
 #define LISTC_TOP(listp) ((listp)->top)
 
+#define LISTC_NEXT(currentp, linkv) ((currentp)->linkv.next)
+
 /* Iteration in which it is safe to remove the current element.
  * tmpp is an additional pointer of the same type as currentp,
  * to cache the next element */

bdb/bdb_api.h
@@ -1873,7 +1873,7 @@ int bdb_table_version_delete(bdb_state_type *bdb_state, tran_type *tran,
  *  If an entry doesn't exist, version 0 is returned
  *
  */
-int bdb_table_version_select(bdb_state_type *bdb_state, tran_type *tran,
+int bdb_table_version_select(const char *name, tran_type *tran,
                              unsigned long long *version, int *bdberr);
 
 void bdb_send_analysed_table_to_master(bdb_state_type *bdb_state, char *table);

bdb/llmeta.c
@@ -6996,7 +6996,7 @@ int bdb_table_version_upsert(bdb_state_type *bdb_state, tran_type *tran,
     }
 
     /* find the existing record, if any */
-    rc = bdb_table_version_select(bdb_state, tran, &version, bdberr);
+    rc = bdb_table_version_select(bdb_state->name, tran, &version, bdberr);
     if (rc) {
         *bdberr = BDBERR_MISC;
         return -1;
@@ -7086,7 +7086,7 @@ int bdb_table_version_delete(bdb_state_type *bdb_state, tran_type *tran,
     }
 
     /* find the existing record, if any */
-    rc = bdb_table_version_select(bdb_state, tran, &version, bdberr);
+    rc = bdb_table_version_select(bdb_state->name, tran, &version, bdberr);
     if (rc) {
         *bdberr = BDBERR_MISC;
         return -1;
@@ -7125,14 +7125,13 @@ int bdb_table_version_delete(bdb_state_type *bdb_state, tran_type *tran,
  *  If an entry doesn't exist, version 0 is returned
  *
  */
-int bdb_table_version_select(bdb_state_type *bdb_state, tran_type *tran,
+int bdb_table_version_select(const char *tblname, tran_type *tran,
                              unsigned long long *version, int *bdberr)
 {
     struct llmeta_sane_table_version schema_version;
     char key[LLMETA_IXLEN] = {0};
     char fnddata[sizeof(*version)];
     int fnddatalen;
-    const char *tblname = bdb_state->name;
     int tblnamelen;
     uint8_t *p_buf, *p_buf_end;
     int retries;

db/glue.c
@@ -6139,7 +6139,7 @@ int table_version_upsert(struct dbtable *db, void *trans, int *bdberr)
     //select needs to be done with the same transaction to avoid 
     //undetectable deadlock for writing and reading from same thread
     unsigned long long version;
-    rc = bdb_table_version_select(db->handle, trans, &version, bdberr);
+    rc = bdb_table_version_select(db->dbname, trans, &version, bdberr);
     if (rc || *bdberr) {
         logmsg(LOGMSG_ERROR, "%s error version=%llu rc=%d bdberr=%d\n", __func__,
                 version, rc, *bdberr);
@@ -6160,7 +6160,7 @@ unsigned long long table_version_select(struct dbtable *db, tran_type *tran)
     unsigned long long version;
     int rc;
 
-    rc = bdb_table_version_select(db->handle, tran, &version, &bdberr);
+    rc = bdb_table_version_select(db->dbname, tran, &version, &bdberr);
     if (rc || bdberr) {
         logmsg(LOGMSG_ERROR, "%s error version=%llu rc=%d bdberr=%d\n", __func__,
                 version, rc, bdberr);

db/osqlblockproc.c
@@ -1231,6 +1231,7 @@ int osql_bplog_reqlog_queries(struct ireq *iq)
     return 0;
 }
 
+static pthread_mutex_t ddl_lk = PTHREAD_MUTEX_INITIALIZER;
 static int apply_changes(struct ireq *iq, blocksql_tran_t *tran, void *iq_tran,
                          int *nops, struct block_err *err, SBUF2 *logsb)
 {
@@ -1278,17 +1279,22 @@ static int apply_changes(struct ireq *iq, blocksql_tran_t *tran, void *iq_tran,
 
     listc_init(&iq->bpfunc_lst, offsetof(bpfunc_lstnode_t, linkct));
 
+    if (iq->tranddl && pthread_mutex_trylock(&ddl_lk) != 0) {
+        errstat_set_str(&iq->errstat, "schemachange already in progress");
+        iq->errstat.errval = ERR_SC;
+        return iq->errstat.errval;
+    }
     /* go through the complete list and apply all the changes */
     LISTC_FOR_EACH(&tran->complete, info, c_reqs)
     {
-
-        /* TODO: add an extended error structure to be passed back to the client
-         */
         out_rc = process_this_session(iq, iq_tran, info->sess, &bdberr, nops,
                                       err, logsb, dbc, osql_process_packet);
-
-        if (out_rc)
+        if (out_rc) {
             break;
+        }
+    }
+    if (iq->tranddl) {
+        pthread_mutex_unlock(&ddl_lk);
     }
 #if 0
     /* we will apply these outside a transaction */

db/osqlsqlthr.c
@@ -1632,7 +1632,7 @@ int access_control_check_sql_read(struct BtCursor *pCur, struct sql_thread *thd)
 *
 */
 int osql_schemachange_logic(struct schema_change_type *sc,
-                            struct sql_thread *thd)
+                            struct sql_thread *thd, int usedb)
 {
     struct sqlclntstate *clnt = thd->sqlclntstate;
     osqlstate_t *osql = &clnt->osql;
@@ -1660,6 +1660,13 @@ int osql_schemachange_logic(struct schema_change_type *sc,
                "%s:%d %s - failed to cache socksql schemachange rc=%d\n",
                __FILE__, __LINE__, __func__, rc);
     }
-    return osql_send_schemachange(host, rqid, thd->sqlclntstate->osql.uuid, sc,
-                                  NET_OSQL_BLOCK_RPL_UUID, osql->logsb);
+    if (usedb) {
+        rc = osql_send_usedb(osql->host, osql->rqid, osql->uuid, tblname,
+                             NET_OSQL_BLOCK_RPL_UUID, osql->logsb);
+    }
+    if (rc == SQLITE_OK) {
+        rc = osql_send_schemachange(host, rqid, thd->sqlclntstate->osql.uuid,
+                                    sc, NET_OSQL_BLOCK_RPL_UUID, osql->logsb);
+    }
+    return rc;
 }

db/osqlsqlthr.h
@@ -183,8 +183,8 @@ int osql_sock_restart(struct sqlclntstate *clnt, int maxretries,
 * Returns SQLITE_OK if successful.
 *
 */
-int osql_schemachange_logic(struct schema_change_type *,
-                            struct sql_thread *thd);
+int osql_schemachange_logic(struct schema_change_type *, struct sql_thread *,
+                            int usedb);
 
 int osql_dbq_consume_logic(struct sqlclntstate *, const char *spname, genid_t);
 int osql_dbq_consume(struct sqlclntstate *, const char *spname, genid_t);

commit 446c7f6d9f979640cb499ceab75deb303fc75551

set usedbtablevers in non-tran do_schema_change
This fixes sc_timepart.test. The bug was introduced in (#302)

schemachange/sc_logic.c
@@ -383,6 +383,8 @@ static int check_table_version(struct ireq *iq)
         errstat_set_strf(&iq->errstat,
                          "failed to get version for table:%s rc:%d\n",
                          iq->sc->table, rc);
+        sc_errf(iq->sc, "failed to get version for table:%s rc:%d\n",
+                iq->sc->table, rc);
         iq->errstat.errval = ERR_SC;
         return SC_INTERNAL_ERROR;
     }
@@ -390,6 +392,8 @@ static int check_table_version(struct ireq *iq)
         errstat_set_strf(&iq->errstat,
                          "stale version for table:%s master:%d replicant:%d\n",
                          iq->sc->table, version, iq->usedbtablevers);
+        sc_errf(iq->sc, "stale version for table:%s master:%d replicant:%d\n",
+                iq->sc->table, version, iq->usedbtablevers);
         iq->errstat.errval = ERR_SC;
         return SC_INTERNAL_ERROR;
     }
@@ -542,6 +546,7 @@ int do_schema_change(struct schema_change_type *s)
         s->db = get_dbtable_by_name(s->table);
     }
     iq.usedb = s->db;
+    iq.usedbtablevers = s->db ? s->db->tableversion : 0;
     sc_arg_t *arg = malloc(sizeof(sc_arg_t));
     arg->iq = &iq;
     arg->trans = NULL;