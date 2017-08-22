db90cd856e09fddd6506ee2fadd0d678e79a5d69
Tim Mui <timmui123@gmail.com>
2017-08-22 19:18:39 -0400

    REMOVE: Docker cores

--- contrib/docker/cluster-entrypoint.sh
+++ contrib/docker/cluster-entrypoint.sh
@@ -3,8 +3,10 @@
 # Start pmux
 pmux -l
 
-# Route core dumps into dumps volume
-echo '/dumps/core.%e.%p' | tee /proc/sys/kernel/core_pattern
+# Core Pattern matches host machine
+# If you really need cores, change your
+# host machines' /proc/sys/kernel/core_pattern
+# to something like /dumps/$HOSTNAME/core.%h.%e.%p
 ulimit -c unlimited
 
 # Start DB
