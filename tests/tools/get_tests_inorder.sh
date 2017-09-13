3eb75717c2dcc0069f595ab5dc01ba067d41162e
Nirbhay Choubey <nchoubey@bloomberg.net>
2017-09-13 15:51:51 -0400

    Tests: Disable tests by adding them to tests/TODO file
    The existing method of disabling tests by renaming the
    test suites unnecessarily bloats commit history.

--- tests/tools/get_tests_inorder.sh
+++ tests/tools/get_tests_inorder.sh
@@ -3,9 +3,11 @@
 #get list of tests in order from longest to shortest test to run
 DEFAULT_TIMEOUT=5
 
-for i in ` ls -d *.test` ; do 
-    a=`cat $i/Makefile | grep TEST_TIMEOUT | cut -f2 -d'=' | sed 's/m//'` ; 
-    [ -z $a ] && a=$DEFAULT_TIMEOUT; 
-    echo $a $i; 
+for i in `ls -d *.test` ; do
+    if ! grep -s "^$i" disabled.tests > /dev/null; then
+        a=`cat $i/Makefile | grep TEST_TIMEOUT | cut -f2 -d'=' | sed 's/m//'` ;
+        [ -z $a ] && a=$DEFAULT_TIMEOUT;
+        echo $a $i;
+    fi
 done | sort -nr | cut -d' ' -f2
 
