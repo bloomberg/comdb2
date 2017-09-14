3e8861061f0d4dbeb3ab7518ab379908f9f6f969
Nirbhay Choubey <nchoubey@bloomberg.net>
2017-09-14 10:48:11 -0400

    Follow-up: Create disabled_tests.txt under ${TESTDIR}

--- tests/tools/get_tests_inorder.sh
+++ tests/tools/get_tests_inorder.sh
@@ -4,7 +4,7 @@
 DEFAULT_TIMEOUT=5
 
 for i in `ls -d *.test` ; do
-    if ! grep -s "^$i" disabled.tests > /dev/null; then
+    if ! grep -s "^$i" ${TESTDIR}/disabled_tests.txt > /dev/null; then
         a=`cat $i/Makefile | grep TEST_TIMEOUT | cut -f2 -d'=' | sed 's/m//'` ;
         [ -z $a ] && a=$DEFAULT_TIMEOUT;
         echo $a $i;
