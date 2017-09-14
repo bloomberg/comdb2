a8840a28a09df9fc933dde107afee66b5af19956
Nirbhay Choubey <nchoubey@bloomberg.net>
2017-09-14 14:09:30 -0400

    Parameterize get_tests_inorder to take disabled_tests.txt

--- tests/tools/get_tests_inorder.sh
+++ tests/tools/get_tests_inorder.sh
@@ -3,8 +3,11 @@
 #get list of tests in order from longest to shortest test to run
 DEFAULT_TIMEOUT=5
 
+DISABLED_TESTS=${1}
+
 for i in `ls -d *.test` ; do
-    if ! grep -s "^$i" ${TESTDIR}/disabled_tests.txt > /dev/null; then
+    if [ -z ${DISABLED_TESTS} ] || ! grep -s "^$i" ${DISABLED_TESTS} > /dev/null;
+    then
         a=`cat $i/Makefile | grep TEST_TIMEOUT | cut -f2 -d'=' | sed 's/m//'` ;
         [ -z $a ] && a=$DEFAULT_TIMEOUT;
         echo $a $i;
