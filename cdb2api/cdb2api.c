77de00beed8a76120920df7f3d5309e3b942775e
Mohit Khullar <mohit.khullar@gmail.com>
2017-09-20 10:54:25 -0400

    Fix dbinfo retry code

--- cdb2api/cdb2api.c
+++ cdb2api/cdb2api.c
@@ -953,7 +953,7 @@ static int get_comdb2db_hosts(cdb2_hndl_tp *hndl, char comdb2db_hosts[][64],
     if (num_db_hosts)
         *num_db_hosts = 0;
     if (master)
-        *master = 0;
+        *master = -1;
 
     if (CDB2DBCONFIG_BUF != NULL) {
         read_comdb2db_cfg(NULL, NULL, comdb2db_name, CDB2DBCONFIG_BUF,
@@ -2048,23 +2048,30 @@ static int cdb2_send_query(cdb2_hndl_tp *hndl, SBUF2 *sb, char *dbname,
         n_features++;
     }
 
-    if (hndl && retries_done >= hndl->num_hosts) {
-        features[n_features] = CDB2_CLIENT_FEATURES__ALLOW_QUEUING;
-        n_features++;
-    }
 
+#if WITH_SSL
+    features[n_features] = CDB2_CLIENT_FEATURES__SSL;
+    n_features++;
+#endif
     if (hndl) {
         features[n_features] = CDB2_CLIENT_FEATURES__ALLOW_MASTER_DBINFO;
         n_features++;
-#if WITH_SSL
-        features[n_features] = CDB2_CLIENT_FEATURES__SSL;
-        n_features++;
-#endif
         if ((hndl->flags & CDB2_DIRECT_CPU) ||
             (retries_done && hndl->master == hndl->connected_host)) {
             features[n_features] = CDB2_CLIENT_FEATURES__ALLOW_MASTER_EXEC;
             n_features++;
         }
+        if (retries_done >= hndl->num_hosts) {
+            features[n_features] = CDB2_CLIENT_FEATURES__ALLOW_QUEUING;
+            n_features++;
+        }
+    } else if (retries_done){
+        features[n_features] = CDB2_CLIENT_FEATURES__ALLOW_MASTER_DBINFO;
+        n_features++;
+        features[n_features] = CDB2_CLIENT_FEATURES__ALLOW_MASTER_EXEC;
+        n_features++;
+        features[n_features] = CDB2_CLIENT_FEATURES__ALLOW_QUEUING;
+        n_features++;
     }
 
     if (hndl && hndl->cnonce_len > 0) {
@@ -4048,7 +4055,7 @@ done:
 static int comdb2db_get_dbhosts(cdb2_hndl_tp *hndl, char *comdb2db_name,
                                 int comdb2db_num, char *host, int port,
                                 char hosts[][64], int *num_hosts, char *dbname,
-                                char *cluster, int *dbnum)
+                                char *cluster, int *dbnum, int num_retries)
 {
     char sql_query[256];
     int rc = 0;
@@ -4129,7 +4136,7 @@ static int comdb2db_get_dbhosts(cdb2_hndl_tp *hndl, char *comdb2db_name,
         sbuf2flush(ss);
     }
     rc = cdb2_send_query(NULL, ss, comdb2db_name, sql_query, 0, 0, NULL, 3,
-                         bindvars, 0, NULL, 0, 0, 0, 0, __LINE__);
+                         bindvars, 0, NULL, 0, 0, num_retries, 0, __LINE__);
     int i = 0;
     for (i = 0; i < 3; i++) {
         free(bindvars[i]);
@@ -4334,7 +4341,7 @@ static int cdb2_get_dbhosts(cdb2_hndl_tp *hndl)
     char comdb2db_hosts[MAX_NODES][64];
     int comdb2db_ports[MAX_NODES];
     int num_comdb2db_hosts;
-    int master = 0, rc = 0;
+    int master = -1, rc = 0;
     int num_retry = 0;
     int comdb2db_num = COMDB2DB_NUM;
     char comdb2db_name[32] = COMDB2DB;
@@ -4390,12 +4397,13 @@ retry:
     if (rc && num_retry < MAX_RETRIES) {
         num_retry++;
         poll(NULL, 0, 250); // Sleep for 250ms everytime and total of 5 seconds
+        rc = 0;
     } else if (rc) {
         return rc;
     }
     if (hndl->num_hosts == 0) {
         int i = 0;
-        if (!master) {
+        if (master == -1) {
             for (i = 0; i < num_comdb2db_hosts; i++) {
                 rc = cdb2_dbinfo_query(
                     hndl, cdb2_default_cluster, comdb2db_name, comdb2db_num,
@@ -4420,7 +4428,7 @@ retry:
             rc = comdb2db_get_dbhosts(
                 hndl, comdb2db_name, comdb2db_num, comdb2db_hosts[i],
                 comdb2db_ports[i], hndl->hosts, &hndl->num_hosts, hndl->dbname,
-                hndl->cluster, &hndl->dbnum);
+                hndl->cluster, &hndl->dbnum, num_retry);
             if (rc == 0) {
                 break;
             }
@@ -4429,7 +4437,7 @@ retry:
             rc = comdb2db_get_dbhosts(
                 hndl, comdb2db_name, comdb2db_num, comdb2db_hosts[master],
                 comdb2db_ports[master], hndl->hosts, &hndl->num_hosts,
-                hndl->dbname, hndl->cluster, &hndl->dbnum);
+                hndl->dbname, hndl->cluster, &hndl->dbnum, num_retry);
         }
 
         if (rc != 0) {
