bac1911c272595e3a60d93bbf7dde83d1943f058
Nirbhay Choubey <nchoubey@bloomberg.net>
2017-10-11 23:06:41 -0400

    Use different approach to disable tests.

--- tests/tools/get_tests_inorder.sh
+++ tests/tools/get_tests_inorder.sh
@@ -3,14 +3,9 @@
 #get list of tests in order from longest to shortest test to run
 DEFAULT_TIMEOUT=5
 
-DISABLED_TESTS=${1}
-
-for i in `ls -d *.test` ; do
-    if [ -z ${DISABLED_TESTS} ] || ! grep -s "^$i" ${DISABLED_TESTS} > /dev/null;
-    then
-        a=`cat $i/Makefile | grep TEST_TIMEOUT | cut -f2 -d'=' | sed 's/m//'` ;
-        [ -z $a ] && a=$DEFAULT_TIMEOUT;
-        echo $a $i;
-    fi
+for i in ` ls -d *.test` ; do 
+    a=`cat $i/Makefile | grep TEST_TIMEOUT | cut -f2 -d'=' | sed 's/m//'` ; 
+    [ -z $a ] && a=$DEFAULT_TIMEOUT; 
+    echo $a $i; 
 done | sort -nr | cut -d' ' -f2
 
