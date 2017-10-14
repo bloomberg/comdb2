bloomberg/comdb2

commit 7c40237f6322c335af8d71ab062c6fa99f953443

fix bad rules in maccparse.y (#410,AZ_macc_y)
This removes a shift/reduce conflict warning, as well as removes ambiguity in grammar.

csc2/maccparse.y
@@ -146,11 +146,9 @@ cnstrtdef:      cnstrtstart cnstrtbllist ctmodifiers cnstrtdef { /*end_constrain
                 ;
 
                 ;
-cnstrtbllist:     cnstrtbllist T_LT string ':' string T_GT  {  add_constraint($3,$5); }
-                | cnstrtbllist string ':' string  {  add_constraint($2,$4); }
-                | cnstrtbllist varname ':' varname  {  add_constraint($2,$4); }
-                | cnstrtbllist cnstrtstart
-                | /* %empty */
+cnstrtbllist:     T_LT string ':' string T_GT  {  add_constraint($2,$4); }
+                | string ':' string  {  add_constraint($1,$3); }
+                | varname ':' varname  {  add_constraint($1,$3); }
                 ;
                
 

commit 2f176024b2d4b021725c71646e078a2ba1fa43aa

allow multiple table entries for foreign key constraints (#421)
* this fixes issue introduced in #410: this will now allow multiple foreign keys per constraint if foreign keys are enclosed in <> :

constraints
{
    "COMDB2_PK" -> <"t1":"COMDB2_PK"> <"t2":"COMDB2_PK"> on update cascade on delete cascade
}

* add testcase with multiple table:index entries as foreign key constraint
    updated docs

csc2/maccparse.y
@@ -113,9 +113,6 @@ comdbg_csc:	structdef
 
 
 
-
-
-
 structdef:	validstruct structdef
 		|	validstruct
 		;
@@ -126,9 +123,11 @@ validstruct:	recstruct
             |   constraintstruct
 			;
 
+
 /* constraintstruct: defines cross-table constraints */
 constraintstruct: T_CONSTRAINTS comment '{' cnstrtdef '}' { end_constraint_list(); }
                 ;
+
 
 ctmodifiers:    T_CON_ON T_CON_UPDATE T_CASCADE ctmodifiers           { set_constraint_mod(0,0,1); }
                 | T_CON_ON T_CON_UPDATE T_RESTRICT ctmodifiers        { set_constraint_mod(0,0,0); }
@@ -137,19 +136,22 @@ ctmodifiers:    T_CON_ON T_CON_UPDATE T_CASCADE ctmodifiers           { set_cons
                 | /* %empty */
                 ;
 
+
 cnstrtstart:      string '-' T_GT { end_constraint_list(); start_constraint_list($1); }
-                | varname '-' T_GT { end_constraint_list(); start_constraint_list($1); }
+		| varname '-' T_GT { end_constraint_list(); start_constraint_list($1); }
+		;
+
+cnstrtdef:        cnstrtdef cnstrtstart cnstrtbllist ctmodifiers { /*end_constraint_list(); */}
+                | /* %empty */
                 ;
 
-cnstrtdef:      cnstrtstart cnstrtbllist ctmodifiers cnstrtdef { /*end_constraint_list(); */}
-                | /* %empty */ 
+
+cnstrtbllist:     cnstrtbllist T_LT string ':' string T_GT  {  add_constraint($3,$5); }
+		| T_LT string ':' string T_GT  {  add_constraint($2,$4); }
+		| string ':' string {  add_constraint($1,$3); }
+		| varname ':' varname {  add_constraint($1,$3); }
                 ;
 
-                ;
-cnstrtbllist:     T_LT string ':' string T_GT  {  add_constraint($2,$4); }
-                | string ':' string  {  add_constraint($1,$3); }
-                | varname ':' varname  {  add_constraint($1,$3); }
-                ;
                
 
 
@@ -335,23 +337,6 @@ comment:	T_COMMENT
 
 	| /* %empty */ {$$=blankchar;}
 		;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 

docs/src/sqlitegen/bubble-generator-data.tcl
@@ -491,13 +491,21 @@ set all_graphs {
       }
   }
 
+
+
   constraint-section {
       loop
       {stack
-          {line /keyname -> < /ref-table-name : /ref-keyname > }
-          {line
-              {opt on update cascade}
-              {opt on delete cascade}
+          {line /keyname -> 
+               {or 
+                    {line /ref-table-name : /ref-keyname }
+                    {line {loop {line < /ref-table-name : /ref-keyname > } } }
+               }
+          }
+          {opt 
+            {loop 
+               {line on {or update delete} {or cascade restrict }}
+            }
           }
       }
   }

tests/constraints.test/t9.req
@@ -1,0 +1,75 @@
+create table t5  { schema
+{
+  int uuid
+  int uuida
+  int uuidb
+  int uuidc
+  int uuidd
+  int uuide
+  int value
+}
+
+keys
+{
+    "UUID1"      = uuid
+    "UUID2"      = uuida
+    "UUID3"      = uuidb
+    "UUID4"      = uuidc
+    "UUID5"      = uuidd
+    "UUID6"      = uuide
+}
+
+constraints
+{
+    "UUID1" -> <"t1":"UUID"> on delete cascade
+    "UUID1" -> "t2":"UUID1"
+    UUID1 -> t2:UUID1
+    "UUID2" -> "t1":"UUID" on update cascade on delete restrict
+    "UUID2" -> <"t2":"UUID1">
+    "UUID3" -> <"t1":"UUID"> <"t2":"UUID1">
+    "UUID4" -> <"t1":"UUID"> <"t2":"UUID1"> on delete cascade
+    //Need to add tbl first then alter with this line : "UUID5" -> "t5":"UUID6"
+}
+} $$
+
+insert into t5(uuid, uuida, uuidb, uuidc, uuidd, uuide, value) values(1,1,1,1,1,1,1)
+insert into t2(uuid, value) values(1,1)
+insert into t5(uuid, uuida, uuidb, uuidc, uuidd, uuide, value) values(1,1,1,1,1,1,1)
+insert into t5(uuid, uuida, uuidb, uuidc, uuidd, uuide, value) values(1,2,1,1,1,1,1)
+delete from t5 where uuid = 1
+insert into t5(uuid, uuida, uuidb, uuidc, uuidd, uuide, value) values(1,2,1,1,1,1,1)
+
+
+create table t6  { schema
+{
+  int uuid
+  int uuida
+  int uuidb
+  int uuidc
+  int uuidd
+  int uuide
+  int value
+}
+
+keys
+{
+    "UUID1"      = uuid
+    "UUID2"      = uuida
+    "UUID3"      = uuidb
+    "UUID4"      = uuidc
+    "UUID5"      = uuidd
+    "UUID6"      = uuide
+}
+
+constraints
+{
+    "UUID1" -> <"t1":"UUID"> on delete cascade
+    "UUID1" -> "t2":"UUID1"
+    UUID1 -> t2:UUID1
+    "UUID2" -> "t1":"UUID" on update cascade on delete restrict
+    "UUID2" -> <"t2":"UUID1">
+    "UUID3" -> "t1":"UUID" "t2":"UUID1" //no angle brackets -- should fail
+}
+} $$
+
+

tests/constraints.test/t9.req.out
@@ -1,0 +1,73 @@
+[create table t5  { schema
+{
+  int uuid
+  int uuida
+  int uuidb
+  int uuidc
+  int uuidd
+  int uuide
+  int value
+}
+
+keys
+{
+    "UUID1"      = uuid
+    "UUID2"      = uuida
+    "UUID3"      = uuidb
+    "UUID4"      = uuidc
+    "UUID5"      = uuidd
+    "UUID6"      = uuide
+}
+
+constraints
+{
+    "UUID1" -> <"t1":"UUID"> on delete cascade
+    "UUID1" -> "t2":"UUID1"
+    UUID1 -> t2:UUID1
+    "UUID2" -> "t1":"UUID" on update cascade on delete restrict
+    "UUID2" -> <"t2":"UUID1">
+    "UUID3" -> <"t1":"UUID"> <"t2":"UUID1">
+    "UUID4" -> <"t1":"UUID"> <"t2":"UUID1"> on delete cascade
+    //Need to add tbl first then alter with this line : "UUID5" -> "t5":"UUID6"
+}
+}] rc 0
+[insert into t5(uuid, uuida, uuidb, uuidc, uuidd, uuide, value) values(1,1,1,1,1,1,1)] failed with rc 3 verify key constraint cannot resolve constraint table 't2' index 'UUID1'
+(rows inserted=1)
+[insert into t2(uuid, value) values(1,1)] rc 0
+(rows inserted=1)
+[insert into t5(uuid, uuida, uuidb, uuidc, uuidd, uuide, value) values(1,1,1,1,1,1,1)] rc 0
+[insert into t5(uuid, uuida, uuidb, uuidc, uuidd, uuide, value) values(1,2,1,1,1,1,1)] failed with rc 299 add key constraint duplicate key 'UUID1' on table 't5' index 0
+(rows deleted=1)
+[delete from t5 where uuid = 1] rc 0
+[insert into t5(uuid, uuida, uuidb, uuidc, uuidd, uuide, value) values(1,2,1,1,1,1,1)] failed with rc 3 verify key constraint cannot resolve constraint table 't2' index 'UUID1'
+[create table t6  { schema
+{
+  int uuid
+  int uuida
+  int uuidb
+  int uuidc
+  int uuidd
+  int uuide
+  int value
+}
+
+keys
+{
+    "UUID1"      = uuid
+    "UUID2"      = uuida
+    "UUID3"      = uuidb
+    "UUID4"      = uuidc
+    "UUID5"      = uuidd
+    "UUID6"      = uuide
+}
+
+constraints
+{
+    "UUID1" -> <"t1":"UUID"> on delete cascade
+    "UUID1" -> "t2":"UUID1"
+    UUID1 -> t2:UUID1
+    "UUID2" -> "t1":"UUID" on update cascade on delete restrict
+    "UUID2" -> <"t2":"UUID1">
+    "UUID3" -> "t1":"UUID" "t2":"UUID1" //no angle brackets -- should fail
+}
+}] failed with rc 304 ERROR at line  28: syntax error: :