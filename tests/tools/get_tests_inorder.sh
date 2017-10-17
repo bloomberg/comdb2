ad7bb403eef9df0363bc08beb990e89964d5d762
Aphyr <aphyr@aphyr.com>
2017-10-17 10:31:39 -0500

    tests/tools/get_tests_inorder.sh: fix missing quotes in test expression

--- tests/tools/get_tests_inorder.sh
+++ tests/tools/get_tests_inorder.sh
@@ -5,7 +5,7 @@ DEFAULT_TIMEOUT=5
 
 for i in ` ls -d *.test` ; do 
     a=`cat $i/Makefile | grep TEST_TIMEOUT | cut -f2 -d'=' | sed 's/m//'` ; 
-    [ -z $a ] && a=$DEFAULT_TIMEOUT; 
+    [ -z "$a" ] && a=$DEFAULT_TIMEOUT; 
     echo $a $i; 
 done | sort -nr | cut -d' ' -f2
 
