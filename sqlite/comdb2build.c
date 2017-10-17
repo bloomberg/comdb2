eee77308ebe82102460339b5ff87889e2a32c0c3
Nirbhay Choubey <nirbhay_c@hotmail.com>
2017-10-17 14:53:17 -0400

    DDL: ALTER TABLE + default 'CURRENT_TIMESTAMP' (#455)

--- sqlite/comdb2build.c
+++ sqlite/comdb2build.c
@@ -1768,8 +1768,8 @@ struct comdb2_ddl_context {
 
 /* Type properties */
 enum {
-    COMDB2_TYPE_FLAG_ALLOW_ARRAY = 1 << 0,
-    COMDB2_TYPE_FLAG_QUOTE_DEFAULT = 1 << 1,
+    FLAG_ALLOW_ARRAY = 1 << 0,
+    FLAG_QUOTE_DEFAULT = 1 << 1,
 };
 
 #define COMDB2_TYPE(A, B, C)                                                   \
@@ -1797,19 +1797,15 @@ struct comdb2_type_mapping {
     COMDB2_TYPE("u_int", "u_int", 0),
     COMDB2_TYPE("int", "int", 0),
     COMDB2_TYPE("longlong", "longlong", 0),
-    COMDB2_TYPE("cstring", "cstring",
-                COMDB2_TYPE_FLAG_ALLOW_ARRAY | COMDB2_TYPE_FLAG_QUOTE_DEFAULT),
-    COMDB2_TYPE("vutf8", "vutf8",
-                COMDB2_TYPE_FLAG_ALLOW_ARRAY | COMDB2_TYPE_FLAG_QUOTE_DEFAULT),
-    COMDB2_TYPE("blob", "blob",
-                COMDB2_TYPE_FLAG_ALLOW_ARRAY | COMDB2_TYPE_FLAG_QUOTE_DEFAULT),
-    COMDB2_TYPE("byte", "byte",
-                COMDB2_TYPE_FLAG_ALLOW_ARRAY | COMDB2_TYPE_FLAG_QUOTE_DEFAULT),
-    COMDB2_TYPE("datetime", "datetime", COMDB2_TYPE_FLAG_QUOTE_DEFAULT),
-    COMDB2_TYPE("datetimeus", "datetimeus", COMDB2_TYPE_FLAG_QUOTE_DEFAULT),
-    COMDB2_TYPE("intervalds", "intervalds", COMDB2_TYPE_FLAG_QUOTE_DEFAULT),
-    COMDB2_TYPE("intervaldsus", "intervaldsus", COMDB2_TYPE_FLAG_QUOTE_DEFAULT),
-    COMDB2_TYPE("intervalym", "intervalym", COMDB2_TYPE_FLAG_QUOTE_DEFAULT),
+    COMDB2_TYPE("cstring", "cstring", FLAG_ALLOW_ARRAY | FLAG_QUOTE_DEFAULT),
+    COMDB2_TYPE("vutf8", "vutf8", FLAG_ALLOW_ARRAY | FLAG_QUOTE_DEFAULT),
+    COMDB2_TYPE("blob", "blob", FLAG_ALLOW_ARRAY | FLAG_QUOTE_DEFAULT),
+    COMDB2_TYPE("byte", "byte", FLAG_ALLOW_ARRAY | FLAG_QUOTE_DEFAULT),
+    COMDB2_TYPE("datetime", "datetime", FLAG_QUOTE_DEFAULT),
+    COMDB2_TYPE("datetimeus", "datetimeus", FLAG_QUOTE_DEFAULT),
+    COMDB2_TYPE("intervalds", "intervalds", FLAG_QUOTE_DEFAULT),
+    COMDB2_TYPE("intervaldsus", "intervaldsus", FLAG_QUOTE_DEFAULT),
+    COMDB2_TYPE("intervalym", "intervalym", FLAG_QUOTE_DEFAULT),
     COMDB2_TYPE("decimal32", "decimal32", 0),
     COMDB2_TYPE("decimal64", "decimal64", 0),
     COMDB2_TYPE("decimal128", "decimal128", 0),
@@ -1817,12 +1813,9 @@ struct comdb2_type_mapping {
     COMDB2_TYPE("double", "double", 0),
 
     /* Additional types mapped to a Comdb2 type. */
-    COMDB2_TYPE("varchar", "cstring",
-                COMDB2_TYPE_FLAG_ALLOW_ARRAY | COMDB2_TYPE_FLAG_QUOTE_DEFAULT),
-    COMDB2_TYPE("char", "cstring",
-                COMDB2_TYPE_FLAG_ALLOW_ARRAY | COMDB2_TYPE_FLAG_QUOTE_DEFAULT),
-    COMDB2_TYPE("text", "vutf8",
-                COMDB2_TYPE_FLAG_ALLOW_ARRAY | COMDB2_TYPE_FLAG_QUOTE_DEFAULT),
+    COMDB2_TYPE("varchar", "cstring", FLAG_ALLOW_ARRAY | FLAG_QUOTE_DEFAULT),
+    COMDB2_TYPE("char", "cstring", FLAG_ALLOW_ARRAY | FLAG_QUOTE_DEFAULT),
+    COMDB2_TYPE("text", "vutf8", FLAG_ALLOW_ARRAY | FLAG_QUOTE_DEFAULT),
     COMDB2_TYPE("integer", "int", 0),
     COMDB2_TYPE("smallint", "short", 0),
     COMDB2_TYPE("bigint", "longlong", 0),
@@ -1902,7 +1895,7 @@ static int comdb2_parse_sql_type(const char *type, int *size)
     for (int i = 0; type_mapping[i].sql_type != NULL; ++i) {
 
         /* Check if current type could accept size. */
-        accepts_size = (type_mapping[i].flag & COMDB2_TYPE_FLAG_ALLOW_ARRAY);
+        accepts_size = (type_mapping[i].flag & FLAG_ALLOW_ARRAY);
 
         if ((accepts_size == 0) && (type_mapping[i].sql_type_len != type_len)) {
             continue;
@@ -2034,139 +2027,6 @@ err:
     return 1;
 }
 
-/*
-  Return the null-terminated string representation of the specified
-  value.
-
-  TODO(Nirbhay): How to detect/represent NULL values?
-
-  @return
-    Success    NULL-terminated string
-    Error      NULL
-*/
-static char *format_val(comdb2ma ma, char *val, int type, int len)
-{
-    char *buf;
-    int buf_len;
-
-    switch (type) {
-    case SERVER_UINT:
-        switch (len) {
-        case 3: {
-            unsigned short v;
-            memcpy(&v, val + 1, len - 1);
-            v = ntohs(v);
-            buf_len = snprintf(NULL, 0, "%hu", v);
-            buf_len++;
-            buf = comdb2_malloc(ma, buf_len);
-            snprintf(buf, buf_len, "%hu", v);
-            return buf;
-        }
-        case 5: {
-            unsigned int v;
-            memcpy(&v, val + 1, len - 1);
-            v = ntohs(v);
-            buf_len = snprintf(NULL, 0, "%u", v);
-            buf_len++;
-            buf = comdb2_malloc(ma, buf_len);
-            snprintf(buf, buf_len, "%hu", v);
-            return buf;
-        }
-        default: goto err;
-        }
-    case SERVER_BINT:
-        switch (len) {
-        case 3: {
-            int2b v;
-            short sval;
-            memcpy(&v, val + 1, len - 1);
-            v = ntohs(v);
-            int2b_to_int2(v, &sval);
-            buf_len = snprintf(NULL, 0, "%hd", sval);
-            buf_len++;
-            buf = comdb2_malloc(ma, buf_len);
-            snprintf(buf, buf_len, "%hd", sval);
-            return buf;
-        }
-        case 5: {
-            int4b v;
-            int ival;
-            memcpy(&v, val + 1, len - 1);
-            v = ntohl(v);
-            int4b_to_int4(v, &ival);
-            buf_len = snprintf(NULL, 0, "%d", ival);
-            buf_len++;
-            buf = comdb2_malloc(ma, buf_len);
-            snprintf(buf, buf_len, "%d", ival);
-            return buf;
-        }
-        case 9: {
-            int8b v;
-            long long lval;
-            memcpy(&v, val + 1, len - 1);
-            v = flibc_ntohll(v);
-            int8b_to_int8(v, &lval);
-            buf_len = snprintf(NULL, 0, "%lld", lval);
-            buf_len++;
-            buf = comdb2_malloc(ma, buf_len);
-            snprintf(buf, buf_len, "%lld", lval);
-            return buf;
-        }
-        default: goto err;
-        }
-    case SERVER_BREAL:
-        switch (len) {
-        case 5: {
-            ieee4b v;
-            float fval;
-            memcpy(&v, val + 1, len - 1);
-            v = ntohl(v);
-            ieee4b_to_ieee4(v, &fval);
-            buf_len = snprintf(NULL, 0, "%f", (double)fval);
-            buf_len++;
-            buf = comdb2_malloc(ma, buf_len);
-            snprintf(buf, buf_len, "%f", (double)fval);
-            return buf;
-        }
-        case 9: {
-            ieee8b v;
-            double dval;
-            memcpy(&v, val + 1, len - 1);
-            v = flibc_ntohll(v);
-            ieee8b_to_ieee8(v, &dval);
-            buf_len = snprintf(NULL, 0, "%f", dval);
-            buf_len++;
-            buf = comdb2_malloc(ma, buf_len);
-            snprintf(buf, buf_len, "%f", dval);
-            return buf;
-        }
-        default: goto err;
-        }
-    case SERVER_BCSTR:
-        buf_len = strlen(val + 1) + 3;
-        buf = comdb2_malloc(ma, buf_len);
-        snprintf(buf, buf_len, "\"%s\"", val + 1);
-        return buf;
-        break;
-    case SERVER_BYTEARRAY:  /* fallthrough */
-    case SERVER_BLOB:       /* fallthrough */
-    case SERVER_DATETIME:   /* fallthrough */
-    case SERVER_INTVYM:     /* fallthrough */
-    case SERVER_INTVDS:     /* fallthrough */
-    case SERVER_VUTF8:      /* fallthrough */
-    case SERVER_DECIMAL:    /* fallthrough */
-    case SERVER_BLOB2:      /* fallthrough */
-    case SERVER_DATETIMEUS: /* fallthrough */
-    case SERVER_INTVDSUS:   /* fallthrough */
-    default: goto err;
-    }
-
-err:
-    logmsg(LOGMSG_ERROR, "%s:%d Invalid value encountered\n", __FILE__,
-           __LINE__);
-    return 0;
-}
-
 struct csc2_constraint {
     char *lclkey;
     int ncnstrts;
@@ -2207,7 +2067,19 @@ static char *format_csc2(struct comdb2_ddl_context *ctx, int nconstraints,
 
         /* Append default. The default is always a null-terminated string. */
         if (field->in_default) {
-            strbuf_appendf(csc2, "dbstore = %s ", field->in_default);
+            assert(field->type < ((sizeof(type_mapping) /
+                                   sizeof(struct comdb2_type_mapping)) -
+                                  1));
+
+            /*
+              Check whether the default value needs to be quoted. Note: CSC2
+              does not allow single quoted value.
+            */
+            if ((type_mapping[field->type].flag & FLAG_QUOTE_DEFAULT) != 0) {
+                strbuf_appendf(csc2, "dbstore = \"%s\" ", field->in_default);
+            } else {
+                strbuf_appendf(csc2, "dbstore = %s ", field->in_default);
+            }
         }
 
         if (field->convopts.dbpad > 0) {
@@ -2654,6 +2526,7 @@ static int retrieve_schema(Parse *pParse, struct comdb2_ddl_context *ctx)
     struct comdb2_field *fentry;
     struct comdb2_schema *sentry;
     struct comdb2_constraint *centry;
+    char *def_str;
 
     assert(ctx != 0);
 
@@ -2683,9 +2556,13 @@ static int retrieve_schema(Parse *pParse, struct comdb2_ddl_context *ctx)
         fentry->field = &schema->member[i];
         /* Convert the default value to string. */
         if (fentry->field->in_default) {
-            fentry->field->in_default = format_val(
-                ctx->mem, fentry->field->in_default,
-                fentry->field->in_default_type, fentry->field->in_default_len);
+            def_str = sql_field_default_trans(fentry->field, 0);
+            /* Remove the quotes around the default value (if any). */
+            sqlite3Dequote(def_str);
+
+            fentry->field->in_default = comdb2_strdup(ctx->mem, def_str);
+            fentry->field->in_default_len = strlen(def_str);
+            sqlite3_free(def_str);
         }
         listc_abl(&ctx->column_list, fentry);
     }
@@ -3094,35 +2971,13 @@ void comdb2AddDefaultValue(Parse *pParse, ExprSpan *pSpan)
     def_len = pSpan->zEnd - pSpan->zStart;
     def = comdb2_strndup(ctx->mem, pSpan->zStart, def_len);
     if (def == 0) goto oom;
+    /* Remove the quotes around the default value (if any). */
+    sqlite3Dequote(def);
 
     field = ((struct comdb2_field *)LISTC_BOT(&ctx->column_list))->field;
 
-    /*
-      Fix the quotes around the supplied default value as CSC2 only accepts
-      double-quotes around string types.
-    */
-    assert(field->type <
-           ((sizeof(type_mapping) / sizeof(struct comdb2_type_mapping)) - 1));
-
-    if ((type_mapping[field->type].flag & COMDB2_TYPE_FLAG_QUOTE_DEFAULT) !=
-        0) {
-        /*
-          In case the supplied default value was unquoted, it would
-          have been a syntax error, and thus, have been caught earlier.
-          We check for single-quoted values and change them to double
-          quotes.
-        */
-        if (def[0] == '\'') {
-            assert(def[def_len - 1] == '\'');
-            def[0] = '"';
-            def[def_len - 1] = '"';
-        }
-    } else {
-        /* Remove the quotes around the default value (if any). */
-        sqlite3Dequote(def);
-    }
-
     field->in_default = def;
+    field->in_default_len = strlen(def);
 
     return;
 
