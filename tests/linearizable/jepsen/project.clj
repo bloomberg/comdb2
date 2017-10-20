2053c89cdcf28c9eae5da7c23684cb51b811dc88
Aphyr <aphyr@aphyr.com>
2017-10-20 15:37:28 -0500

    Jepsen: get rid of spurious test selectors

--- tests/linearizable/jepsen/project.clj
+++ tests/linearizable/jepsen/project.clj
@@ -7,15 +7,4 @@
                  [jepsen "0.1.6"]
                  [org.clojure/java.jdbc "0.6.1"]
                  [slingshot "0.12.2"]
-                 [com.bloomberg.comdb2/cdb2jdbc "2.0.0"]]
-  :test-selectors {:a6 :a6-test
-                   :g2 :g2-test
-                   :register :register-test
-                   :register-nemesis :register-test-nemesis
-                   :dirty-reads :dirty-reads-test
-                   :sets :sets-test
-                   :sets-nemesis :sets-test-nemesis
-                   :bank :test-bank
-                   :bank-nemesis :test-bank-nemesis
-                  }
-  )
+                 [com.bloomberg.comdb2/cdb2jdbc "2.0.0"]])
