979ea2517a1552f3b39a2e622253eb7f8397d51f
Mark Hannum <mhannum72@gmail.com>
2017-10-23 11:28:42 -0400

    Small script changes

---
+++ tests/runt.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# set -x
+export CLUSTER="node1 node2 node3 node4 node5"
+export SETUP_WARN=1
+export testname=${1:-jepsen_register_nemesis}
+export user=$(whoami)
+i=0
+r=0
+
+# Kill the *TEST* after this amount of time
+
+while [[ $r == 0 ]]; do
+    let i=i+1
+    echo "iteration $i $(date)"
+    make $testname 2>&1 | tee out 
+
+    egrep "success" out > /dev/null 2>&1
+    r=$?
+
+    if [[ $r -ne 0 ]]; then
+        egrep "timeout" out > /dev/null 2>&1
+        if [[ $? == 0 ]]; then 
+            echo "Continuing on testcase timeout"
+            r=0
+        fi
+    fi
+done
+
