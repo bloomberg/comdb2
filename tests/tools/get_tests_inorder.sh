64a66d932187653b04e09e15b42cd767bc43d94d
Mark Hannum <mhannum72@gmail.com>
2017-11-01 11:48:28 -0400

    Make get_tests_inorder.sh aware of configurable TEST_TIMEOUT

--- tests/tools/get_tests_inorder.sh
+++ tests/tools/get_tests_inorder.sh
@@ -4,7 +4,7 @@
 DEFAULT_TIMEOUT=5
 
 for i in ` ls -d *.test` ; do 
-    a=`cat $i/Makefile | grep TEST_TIMEOUT | cut -f2 -d'=' | sed 's/m//'` ; 
+    a=`cat $i/Makefile | grep TEST_TIMEOUT | egrep -v "ifeq" | cut -f2 -d'=' | sed 's/m//'` ; 
     [ -z "$a" ] && a=$DEFAULT_TIMEOUT; 
     echo $a $i; 
 done | sort -nr | cut -d' ' -f2
