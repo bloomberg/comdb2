c9a515473336f9cd2f54d84ed7f85d69701728e5
Lingzhi Deng <ldeng33@bloomberg.net>
2018-01-03 15:46:57 -0500

    Sc resume race (#626)
    * fix race between sc_resume and blockprocessor
    
    * fix sc_resume test
    
    * code review

--- db/db_tunables.c
+++ db/db_tunables.c
@@ -107,6 +107,7 @@ extern int gbl_sqlite_sorter_mem;
 extern int gbl_survive_n_master_swings;
 extern int gbl_test_blob_race;
 extern int gbl_test_scindex_deadlock;
+extern int gbl_test_sc_resume_race;
 extern int gbl_berkdb_track_locks;
 extern int gbl_udp;
 extern int gbl_update_delete_limit;
