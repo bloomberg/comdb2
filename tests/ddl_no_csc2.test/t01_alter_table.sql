71d8f851419c6a7b30587cd01c6a39a235631018
Nirbhay Choubey <nchoubey@bloomberg.net>
2018-01-05 15:53:26 -0500

    Test: Add test to ensure sort order is considered in generated constraint name

--- tests/ddl_no_csc2.test/t01_alter_table.sql
+++ tests/ddl_no_csc2.test/t01_alter_table.sql
@@ -133,3 +133,16 @@ SELECT * FROM comdb2_constraints WHERE tablename NOT LIKE 'sqlite_stat%';
 SELECT * FROM sqlite_master WHERE name NOT LIKE 'sqlite_stat%';
 DROP TABLE t1;
 DROP TABLE t2;
+
+CREATE TABLE t1(i INT, j INT, k INT, UNIQUE idx1 (i,j,k), UNIQUE idx2(i DESC, j DESC, k DESC)) $$
+CREATE TABLE t2(i INT, j INT, k INT, FOREIGN KEY (i,j) REFERENCES t1(i,j), FOREIGN KEY (i DESC, j DESC) REFERENCES t1(i DESC, j DESC)) $$
+SELECT * FROM sqlite_master;
+SELECT * FROM comdb2_constraints WHERE tablename NOT LIKE 'sqlite_stat%';
+ALTER TABLE t2 DROP FOREIGN KEY '$CONSTRAINT_9C9BDEA4' $$
+SELECT * FROM comdb2_tables WHERE tablename NOT LIKE 'sqlite_stat%';
+SELECT * FROM comdb2_columns WHERE tablename NOT LIKE 'sqlite_stat%';
+SELECT * FROM comdb2_keys WHERE tablename NOT LIKE 'sqlite_stat%';
+SELECT * FROM comdb2_constraints WHERE tablename NOT LIKE 'sqlite_stat%';
+SELECT * FROM sqlite_master WHERE name NOT LIKE 'sqlite_stat%';
+DROP TABLE t2;
+DROP TABLE t1;
