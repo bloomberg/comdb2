f21f51ad85b815ca9a4bd9341a03546750b16522
Dorin Hogea <dhogea@bloomberg.net>
2018-02-09 16:15:00 -0500

    fix schema change race with backend_open (#703)
    * fix schema change race with backend_open
    
    * formatting spaces on empty lines
    
    * fix typo

--- db/comdb2.c
+++ db/comdb2.c
@@ -755,6 +755,7 @@ int gbl_enable_bulk_import; /* allow this db to bulk import */
 int gbl_enable_bulk_import_different_tables;
 
 char *gbl_dbdir = NULL;
+static int gbl_backend_opened = 0;
 
 extern int gbl_verbose_net;
 
@@ -3857,6 +3858,9 @@ static int init(int argc, char **argv)
 
     /* open db engine */
     logmsg(LOGMSG_INFO, "starting backend db engine\n");
+
+    pthread_rwlock_wrlock(&schema_lk);
+
     if (backend_open(thedb) != 0) {
         logmsg(LOGMSG_FATAL, "failed to open '%s'\n", dbname);
         return -1;
@@ -3868,6 +3872,9 @@ static int init(int argc, char **argv)
     }
 
     load_dbstore_tableversion(thedb);
+
+    gbl_backend_opened = 1;
+    pthread_rwlock_unlock(&schema_lk);
 
     sqlinit();
     rc = create_sqlmaster_records(NULL);
@@ -5588,7 +5595,7 @@ const char *comdb2_get_dbname(void)
 
 int sc_ready(void)
 {
-    return thedb->timepart_views != 0;
+    return gbl_backend_opened;
 }
 
 #define QUOTE_(x) #x
