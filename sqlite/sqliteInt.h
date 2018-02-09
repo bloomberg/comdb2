993fb05e3cb8b0bb60c3f2f7aaecef3541d9476a
Adi Zaimi <azaimi@bloomberg.net>
2018-02-09 15:36:18 -0500

    return error on incorrectly quoted string for user and password (#689)
    * return error on incorrectly quoted string for user and password
    * add testcase which exercises presence of quotes in set user and password

--- sqlite/sqliteInt.h
+++ sqlite/sqliteInt.h
@@ -3645,6 +3645,7 @@ char *sqlite3MAppendf(sqlite3*,char*,const char*,...);
 void sqlite3SetString(char **, sqlite3*, const char*);
 void sqlite3ErrorMsg(Parse*, const char*, ...);
 void sqlite3Dequote(char*);
+int sqlite3IsCorrectlyQuoted(char *);
 void sqlite3TokenInit(Token*,char*);
 int sqlite3KeywordCode(const unsigned char*, int);
 int sqlite3RunParser(Parse*, const char*, char **);
