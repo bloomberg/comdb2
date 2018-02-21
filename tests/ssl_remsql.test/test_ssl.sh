bc75ff2d3162b6cded3cab6411540a059714f6b6
Adi Zaimi <azaimi@bloomberg.net>
2018-02-21 13:21:40 -0500

    fix ssl_remsql to query the same node

--- tests/ssl_remsql.test/test_ssl.sh
+++ tests/ssl_remsql.test/test_ssl.sh
@@ -13,15 +13,17 @@ cdb2cfg2=$4
 
 output=run.out
 
-# populate table on remote
-cdb2sql -s --cdb2cfg $cdb2cfg1 $db1 default - < remdata.req > $output 2>&1
+node=`cdb2sql --tabs --cdb2cfg $cdb2cfg1 $db1 default 'select comdb2_host()'`
 
-cdb2sql -s --cdb2cfg $cdb2cfg2 $db2 default "select * from LOCAL_${db1}.t order by id" >> $output 2>&1
+# populate table on remote
+cdb2sql -s --cdb2cfg $cdb2cfg1 $db1 --host $node - < remdata.req > $output 2>&1
+
+cdb2sql -s --cdb2cfg $cdb2cfg2 $db2 --host $node "select * from LOCAL_${db1}.t order by id" >> $output 2>&1
 
 # get the version V2
 #comdb2sc $a_dbname send fdb info db >> $output 2>&1
-echo cdb2sql --tabs --cdb2cfg $cdb2cfg2 $db2 default "exec procedure sys.cmd.send(\"fdb info db\")" 
-cdb2sql --tabs --cdb2cfg $cdb2cfg2 $db2 default "exec procedure sys.cmd.send(\"fdb info db\")" >> $output 2>&1
+echo cdb2sql --tabs --cdb2cfg $cdb2cfg2 $db2 --host $node "exec procedure sys.cmd.send(\"fdb info db\")" 
+cdb2sql --tabs --cdb2cfg $cdb2cfg2 $db2 --host $node "exec procedure sys.cmd.send(\"fdb info db\")" >> $output 2>&1
 
 # validate results 
 testcase_output=$(cat $output)
