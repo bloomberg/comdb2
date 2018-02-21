1819d7b295c75eb0d8e462946a5ba7625daf52b7
Mark Hannum <mhannum72@gmail.com>
2018-02-20 21:52:00 -0500

    Port logs directory logic to partial

--- tools/comdb2ar/deserialise.cpp
+++ tools/comdb2ar/deserialise.cpp
@@ -1033,10 +1033,29 @@ void restore_partials(
     std::string manifest_sha;
 
     parse_lrl_file(lrlpath, &dbname, &dbdir, &support_files, &table_names, &queue_names, &nonames, &has_cluster_info);
-    nonames = true;  // TODO: need to figure out if really nonames - can't tell from lrl file since
-                     // our defaults changed.
 
-    std::cout << "start restore, datapath " << dbdir << std::endl;
+    if (!dbdir.empty() && !dbname.empty()) {
+        int rc;
+        struct stat st;
+        bool found_usenames = false;
+        bool found_nonames = false;
+
+        rc = stat((dbdir + "/" + dbname + ".txn").c_str(), &st);
+        if (rc == 0 && S_ISDIR(st.st_mode))
+            found_usenames = true;
+
+        rc = stat((dbdir + "/" + "logs").c_str(), &st);
+        if (rc == 0 && S_ISDIR(st.st_mode))
+            found_nonames = true;
+
+        /* bias by parse_lrl_file told us, but use the other if we can find it */
+        if (nonames && found_nonames == false && found_usenames == true)
+            nonames = false;
+
+        if (nonames == false && found_usenames == false && found_nonames == true)
+            nonames = true;
+    }
+
 
     // The first thing we need to do is to delete all the logs - we 
     // don't want recovery run on partial logs
@@ -1045,6 +1064,9 @@ void restore_partials(
         logpath = dbdir + "/logs";
     else
         logpath = dbdir + "/" + dbname + ".txn";
+
+    std::cout << "start restore, datapath " << dbdir << " logpath " << logpath << std::endl;
+
     std::list<std::string> logfiles;
     listdir(logfiles, logpath);
     for (std::list<std::string>::const_iterator it = logfiles.begin();
