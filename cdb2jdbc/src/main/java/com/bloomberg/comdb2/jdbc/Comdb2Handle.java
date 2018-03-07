8a87c2cf43dec0de19afa28ecd8ef6023fa337c6
Mark Hannum <mhannum72@gmail.com>
2018-03-07 13:49:19 -0500

    Send trace to stderr in debug mode

--- cdb2jdbc/src/main/java/com/bloomberg/comdb2/jdbc/Comdb2Handle.java
+++ cdb2jdbc/src/main/java/com/bloomberg/comdb2/jdbc/Comdb2Handle.java
@@ -18,6 +18,7 @@ import java.io.*;
 import java.util.*;
 import java.util.logging.*;
 import javax.net.ssl.*;
+import java.text.MessageFormat;
 
 import com.google.protobuf.*;
 
@@ -389,11 +390,6 @@ public class Comdb2Handle extends AbstractConnection {
             mach = myDbHosts.get(dbHostConnected);
         }
 
-        if (debug) {
-            logger.setLevel(Level.ALL);
-            level = Level.INFO;
-        }
-
         String message = String.format(str, params);
         Object[] messageParams = new Object[] {
             Thread.currentThread().getId(),
@@ -406,8 +402,10 @@ public class Comdb2Handle extends AbstractConnection {
         logger.log(level,
                 "td={0} mach={1} snapshotFile={2} snapshotOffset={3} cnonce={4}: {5}",
                 messageParams);
+
         if (debug) {
-            logger.setLevel(curlevel);
+            MessageFormat form = new MessageFormat("td={0} mach={1} snapshotFile={2} snapshotOffset={3} cnonce={4}: {5}");
+            System.err.println(form.format(messageParams));
         }
     }
 
