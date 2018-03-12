34c7f42a3d84ab40fcf89829d33448505c303f05
Lingzhi Deng <ldeng33@bloomberg.net>
2018-03-12 16:57:29 -0400

    no_wait_rowlock for sc

--- schemachange/sc_records.c
+++ schemachange/sc_records.c
@@ -416,6 +416,7 @@ static int convert_record(struct convert_record_data *data)
     int dtalen = 0, rc, rrn, opfailcode = 0, ixfailnum = 0;
     unsigned long long genid, ngenid, check_genid;
     void *dta = NULL;
+    int no_wait_rowlock = 0;
 
     if (gbl_sc_thd_failed) {
         if (!data->s->retry_bad_genids)
@@ -502,6 +503,7 @@ static int convert_record(struct convert_record_data *data)
                                            genid);
                 if (rc) {
                     rc = RC_INTERNAL_RETRY;
+                    no_wait_rowlock = 1;
                     goto err;
                 }
             }
@@ -867,7 +869,7 @@ err: /*if (is_schema_change_doomed())*/
         data->trans = NULL;
         data->num_retry_errors++;
         data->totnretries++;
-        if (data->cmembers->is_decrease_thrds)
+        if (!no_wait_rowlock && data->cmembers->is_decrease_thrds)
             decrease_max_threads(&data->cmembers->maxthreads);
         else
             poll(0, 0, (rand() % 500 + 10));
