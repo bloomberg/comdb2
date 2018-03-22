bloomberg/comdb2

commit c38eab6484eee0386c3a5f9783c575114e66e8a8

try cleanup some more before exit (#607)
free even more objects on exit

bdb/rep.c
@@ -4886,6 +4886,7 @@ void send_downgrade_and_lose(bdb_state_type *bdb_state)
 extern int gbl_dump_locks_on_repwait;
 extern int gbl_lock_get_list_start;
 int bdb_clean_pglogs_queues(bdb_state_type *bdb_state);
+extern int db_is_stopped();
 
 void *watcher_thread(void *arg)
 {
@@ -4936,7 +4937,7 @@ void *watcher_thread(void *arg)
 
     bdb_state->repinfo->disable_watcher = 0;
 
-    while (1) {
+    while (!db_is_stopped()) {
         time_now = time_epoch();
         time_then = bdb_state->repinfo->disable_watcher;
 

bb/object_pool.c
@@ -262,9 +262,7 @@ int comdb2_objpool_resume(comdb2_objpool_t op)
 
 int comdb2_objpool_destroy(comdb2_objpool_t op)
 {
-    int rc;
-
-    rc = pthread_mutex_lock(&op->data_mutex);
+    int rc = pthread_mutex_lock(&op->data_mutex);
     if (rc != 0)
         return rc;
 
@@ -312,6 +310,7 @@ int comdb2_objpool_destroy(comdb2_objpool_t op)
 
     /* clear access history */
     hash_for(op->history, hash_elem_free_wrapper, NULL);
+    hash_clear(op->history);
     hash_free(op->history);
     free(op->objs);
 

bb/tcputil.c
@@ -182,7 +182,6 @@ int tcpresolve(const char *host, struct in_addr *in, int *port)
     in_addr_t inaddr;
 
     int len;
-    struct hostent *hp;
     char tok[128], *cc;
     cc = strchr(host, (int)':');
     if (cc == 0) {
@@ -203,7 +202,7 @@ int tcpresolve(const char *host, struct in_addr *in, int *port)
         /* it's dotted-decimal */
         memcpy(&in->s_addr, &inaddr, sizeof(inaddr));
     } else {
-        hp = bb_gethostbyname(tok);
+        struct hostent *hp = bb_gethostbyname(tok);
         if (hp == NULL)
             return -1;
         memcpy(&in->s_addr, hp->h_addr, hp->h_length);

bdb/file.c
@@ -1524,10 +1524,26 @@ static int bdb_close_int(bdb_state_type *bdb_state, int envonly)
     free(bdb_state->dir);
     free(bdb_state->txndir);
     free(bdb_state->tmpdir);
+
+    free(bdb_state->seqnum_info->seqnums);
+    free(bdb_state->last_downgrade_time);
+    free(bdb_state->master_lease);
+    free(bdb_state->coherent_state);
+    free(bdb_state->seqnum_info->waitlist);
+    free(bdb_state->seqnum_info->trackpool);
+    free(bdb_state->seqnum_info->time_10seconds);
+    free(bdb_state->seqnum_info->time_minute);
+    free(bdb_state->seqnum_info->expected_udp_count);
+    free(bdb_state->seqnum_info->incomming_udp_count);
+    free(bdb_state->seqnum_info->udp_average_counter);
+    free(bdb_state->seqnum_info->filenum);
+
+    free(bdb_state->repinfo->appseqnum);
+
     /* We can not free bdb_state because other threads get READLOCK
      * and it does not work well doing so on freed memory, so don't:
-     * memset(bdb_state, 0xff, sizeof(bdb_state));
-     * free(bdb_state);
+    memset(bdb_state, 0xff, sizeof(bdb_state));
+    free(bdb_state);
      */
 
     /* DO NOT RELEASE the write lock.  just let it be. */

bdb/threads.c
@@ -373,6 +373,7 @@ void *logdelete_thread(void *arg)
 
 extern int gbl_rowlocks;
 extern unsigned long long osql_log_time(void);
+extern int db_is_stopped();
 
 void *checkpoint_thread(void *arg)
 {
@@ -403,7 +404,7 @@ void *checkpoint_thread(void *arg)
 
     bdb_thread_event(bdb_state, 1);
 
-    while (1) {
+    while (!db_is_stopped()) {
         BDB_READLOCK("checkpoint_thread");
         checkpointtime = bdb_state->attr->checkpointtime;
         checkpointrand = bdb_state->attr->checkpointrand;
@@ -413,14 +414,6 @@ void *checkpoint_thread(void *arg)
         if (broken) {
             logmsg(LOGMSG_ERROR, "%s failed in log_get_last_lsn rc=%d\n", __func__,
                     broken);
-        }
-
-        if (db_is_stopped()) {
-            logmsg(LOGMSG_DEBUG, "checkpoint_thread: exiting\n");
-
-            BDB_RELLOCK();
-            bdb_thread_event(bdb_state, 0);
-            pthread_exit(NULL);
         }
 
         /* can't call checkpoint until llmeta is open if we are using rowlocks
@@ -483,6 +476,10 @@ void *checkpoint_thread(void *arg)
             } while (crt_time_msec < end_sleep_time_msec);
         }
     }
+
+    logmsg(LOGMSG_DEBUG, "checkpoint_thread: exiting\n");
+    bdb_thread_event(bdb_state, 0);
+    return NULL;
 }
 
 int bdb_get_checkpoint_time(bdb_state_type *bdb_state)

db/comdb2.c
@@ -1040,9 +1040,9 @@ static void *purge_old_blkseq_thread(void *arg)
     backend_thread_event(thedb, COMDB2_THR_EVENT_START_RDONLY);
 
     loop = 0;
+    sleep(1);
 
-    while (1) {
-        sleep(1);
+    while (!db_is_stopped()) {
 
         /* Check del unused files progress about twice per threshold  */
         if (!(loop % (gbl_sc_del_unused_files_threshold_ms /
@@ -1053,12 +1053,6 @@ static void *purge_old_blkseq_thread(void *arg)
             loop = 0;
         else
             ++loop;
-
-        if (dbenv->exiting || dbenv->stopped) {
-            dbenv->purge_old_blkseq_is_running = 0;
-            backend_thread_event(thedb, COMDB2_THR_EVENT_DONE_RDONLY);
-            return NULL;
-        }
 
         if (debug_switch_check_for_hung_checkpoint_thread() &&
             dbenv->master == gbl_mynode) {
@@ -1167,7 +1161,13 @@ static void *purge_old_blkseq_thread(void *arg)
 
         if ((loop % 30) == 0 && gbl_verify_dbreg)
             bdb_verify_dbreg(dbenv->bdb_env);
+
+        sleep(1);
     }
+
+    dbenv->purge_old_blkseq_is_running = 0;
+    backend_thread_event(thedb, COMDB2_THR_EVENT_DONE_RDONLY);
+    return NULL;
 }
 
 static void *purge_old_files_thread(void *arg)
@@ -1376,6 +1376,10 @@ void clean_exit(void)
     thedb->exiting = 1;
     stop_threads(thedb);
     logmsg(LOGMSG_INFO, "stopping db engine...\n");
+
+    /* TODO: (NC) Instead of sleep(), maintain a counter of threads and wait for
+      them to quit.
+    */
     sleep(4);
 
     cleanup_q_vars();
@@ -1384,6 +1388,9 @@ void clean_exit(void)
     free_tzdir();
     tz_hash_free();
     bdb_cleanup_private_blkseq(thedb->bdb_env);
+    if (gbl_create_mode) {
+        logmsg(LOGMSG_USER, "Created database %s.\n", thedb->envname);
+    }
 
     rc = backend_close(thedb);
     if (rc != 0) logmsg(LOGMSG_ERROR, "error backend_close() rc %d\n", rc);
@@ -1413,13 +1420,10 @@ void clean_exit(void)
     if (fd != -1) close(fd);
     logmsg(LOGMSG_INFO, "creating %s\n", indicator_file);
     free(indicator_file);
+    extern char *gbl_portmux_unix_socket;
+    free(gbl_portmux_unix_socket);
     cleanup_file_locations();
-
-    /*
-      Wait for other threads to exit by themselves.
-      TODO: (NC) Instead of sleep(), maintain a counter of threads and wait for
-      them to quit.
-    */
+    ctrace_closelog();
 
     backend_cleanup(thedb);
     net_cleanup_subnets();
@@ -1438,10 +1442,9 @@ void clean_exit(void)
     }
     cleanup_interned_strings();
     cleanup_peer_hash();
+    // TODO: would be nice but other threads need to exit first:
+    // comdb2ma_exit();
 
-    if (gbl_create_mode) {
-        logmsg(LOGMSG_USER, "Created database %s.\n", thedb->envname);
-    }
     logmsg(LOGMSG_USER, "goodbye\n");
 
     exit(0);
@@ -4239,7 +4242,7 @@ void *statthd(void *p)
         reqlog_diffstat_init(statlogger);
     }
 
-    while (!dbenv->exiting && !dbenv->stopped) {
+    while (!db_is_stopped()) {
         nqtrap = n_qtrap;
         nfstrap = n_fstrap;
         ncommits = n_commits;

db/reqlog.c
@@ -1092,11 +1092,13 @@ int reqlog_pushprefixv(struct reqlogger *logger, const char *fmt, va_list args)
     nchars = vsnprintf(s, len, fmt, args);
     if (nchars >= len && reqltruncate == 0) {
         len = nchars + 1;
-        s = realloc(s, len);
-        if (!s) {
+        char *news = realloc(s, len);
+        if (!news) {
             fprintf(stderr, "%s:realloc(%d) failed\n", __func__, len);
+            free(s);
             return -1;
         }
+        s = news;
         len = vsnprintf(s, len, fmt, args_c);
     } else {
         len = strlen(s);
@@ -1114,6 +1116,7 @@ int reqlog_pushprefixv(struct reqlogger *logger, const char *fmt, va_list args)
         event = malloc(sizeof(struct push_prefix_event));
         if (!event) {
             logmsg(LOGMSG_ERROR, "%s:malloc failed\n", __func__);
+            free(s);
             return -1;
         }
         event->length = len;
@@ -1200,12 +1203,14 @@ static int reqlog_logv_int(struct reqlogger *logger, unsigned event_flag,
     nchars = vsnprintf(s, len, fmt, args);
     if (nchars >= len && reqltruncate == 0) {
         len = nchars + 1;
-        s = realloc(s, len);
-        if (!s) {
+        char *news = realloc(s, len);
+        if (!news) {
             logmsg(LOGMSG_ERROR, "%s:realloc(%d) failed\n", __func__, len);
             va_end(args_c);
+            free(s);
             return -1;
         }
+        s = news;
         len = vsnprintf(s, len, fmt, args_c);
     } else {
         len = strlen(s);
@@ -1222,6 +1227,7 @@ static int reqlog_logv_int(struct reqlogger *logger, unsigned event_flag,
         event = malloc(sizeof(struct print_event));
         if (!event) {
             logmsg(LOGMSG_ERROR, "%s:malloc failed\n", __func__);
+            free(s);
             return -1;
         }
         event->event_flag = event_flag;

db/resource.c
@@ -62,14 +62,17 @@ char *strdup(char *str1)
 void initresourceman(const char *newlrlname)
 {
     listc_init(&list, offsetof(struct resource, link));
-    if (newlrlname) {
-        lrlname = realpath(newlrlname, NULL);
+    if (!newlrlname)
+        return;
 
-        /* lrl file is always known as "lrl" */
-        if (lrlname) {
-            addresource("lrl", lrlname);
-        }
-    }
+    if (lrlname) // free before assigning new one
+        free(lrlname);
+
+    lrlname = realpath(newlrlname, NULL);
+
+    /* lrl file is always known as "lrl" */
+    if (lrlname)
+        addresource("lrl", lrlname);
 }
 
 /* Gets the path of the child file (usually a .lrl or .csc2 relative to a
@@ -108,24 +111,14 @@ char *getdbrelpath(const char *relpath)
 
 void addresource(const char *name, const char *filepath)
 {
-    char *relpath;
-    struct resource *res;
-    int namelen, pathlen;
-    const char *nameptr;
-
-    relpath = getdbrelpath(filepath);
-    pathlen = strlen(relpath) + 1;
-
     if (!name)
         name = filepath;
 
-    namelen = strlen(name) + 1;
-    nameptr = name;
-
+    struct resource *res;
     /* look for this name and remove it if it is already present */
     LISTC_FOR_EACH(&list, res, link)
     {
-        if (strcmp(res->name, nameptr) == 0) {
+        if (strcmp(res->name, name) == 0) {
             logmsg(LOGMSG_INFO, "removing resource %s -> %s\n", res->name, res->filepath);
             listc_rfl(&list, res);
             free(res);
@@ -133,6 +126,9 @@ void addresource(const char *name, const char *filepath)
         }
     }
 
+    int namelen = strlen(name) + 1;
+    char *relpath = getdbrelpath(filepath);
+    int pathlen = strlen(relpath) + 1;
     res = malloc(sizeof(struct resource) + namelen + pathlen);
 
     bzero(res, sizeof(struct resource));

commit e152192e4bfbc65e7847a881462f4e0e465ba956

bdb: Fix bdb_lock_destructor error on clean exit.
Quick fix to get rid of bdb_lock_destructor error on clean exit. The bug was introduced in c38eab6484eee0386c3a5f9783c575114e66e8a8.

bdb/rep.c
@@ -5303,6 +5303,8 @@ void *watcher_thread(void *arg)
         /* sleep for somewhere between 1-2 seconds */
         poll(NULL, 0, (rand() % 1000) + 1000);
     }
+
+    bdb_thread_event(bdb_state, 0);
 }
 
 int bdb_wait_for_seqnum_from_n(bdb_state_type *bdb_state, seqnum_type *seqnum,