616409bf9073461cb26e95c6bad79c623cc47164
Mohit Khullar <mohit.khullar@gmail.com>
2018-05-01 11:08:38 -0400

    Reset hasql flags on sockpool donate

--- db/sqlinterfaces.c
+++ db/sqlinterfaces.c
@@ -4379,6 +4379,8 @@ void reset_clnt(struct sqlclntstate *clnt, SBUF2 *sb, int initial)
     clnt->ncontext = 0;
     clnt->statement_query_effects = 0;
     clnt->wrong_db = 0;
+    clnt->hasql_on = 0;
+    set_high_availability(clnt, 0);
 }
 
 void reset_clnt_flags(struct sqlclntstate *clnt)
