c33e3322159163ec45ec3b1708e1a002329c8db5
Rivers Zhang <hzhang320@bloomberg.net>
2018-05-02 21:09:28 +0000

    dlmalloc: Minor tweak and cleanup.
    The pull request removes the segment merge logic in sys_alloc.
    The test program is deleted in the pull request as well.

--- dlmalloc/dlmalloc.c
+++ dlmalloc/dlmalloc.c
@@ -442,6 +442,8 @@ DEFAULT_MMAP_THRESHOLD       default: 256K
 
 */
 
+#include "dlmalloc_config.h"
+
 #ifndef WIN32
 #ifdef _WIN32
 #define WIN32 1
@@ -576,8 +578,6 @@ DEFAULT_MMAP_THRESHOLD       default: 256K
 #define MALLINFO_FIELD_TYPE size_t
 #endif  /* MALLINFO_FIELD_TYPE */
 
-#include "dlmalloc_config.h"
-
 #if !defined(__WORDSIZE) || (__WORDSIZE == 32)
 #define DEFAULT_MMAP_THRESHOLD_MAX ((size_t)512U * (size_t)1024U)
 #else
@@ -646,8 +646,6 @@ struct mallinfo {
 
 #endif /* HAVE_USR_INCLUDE_MALLOC_H */
 #endif /* NO_MALLINFO */
-
-#include <math.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -3419,12 +3417,105 @@ static void* sys_alloc(mstate m, size_t nb, int zeroout) {
 
   init_mparams();
 
-  if (m == NULL && allocfunc) {
-      void *mem;
-      mem = allocfunc(nb + SIX_SIZE_T_SIZES + CHUNK_ALIGN_MASK);
-      logmsg(LOGMSG_DEBUG, "+ INITIAL MALLOC %zu\n", nb + SIX_SIZE_T_SIZES + CHUNK_ALIGN_MASK);
-      if (mem != NULL)
-          return mem;
+  if (
+      (zeroout && m->alloc0func) ||
+      (!zeroout && m->allocfunc)
+     ) { /* Obtain memory from user allocation routines first. */
+    size_t rsize = granularity_align(nb + TOP_FOOT_SIZE + SIZE_T_ONE);
+    if (rsize > nb) { /* Check for wrap around 0. */
+      char *mp;
+
+      /*
+       * CONSERVATIVE: Round up the requested size to the nearest multiple of
+       *               page size.
+       *               Example:
+       *               8193B -> 12KB
+       *
+       * MODERATE:     Round up the requested size to the nearest multiple of
+       *               page size, and then grow the value exponentially up to
+       *               1MB. For requested size >= 1MB, fall back to
+       *               CONSERVATIVE. In addition, the exponential factor is
+       *               halved upon a successful sys_trim().
+       *               Example:
+       *               8193B -> 12KB (1st time)
+       *               8193B -> 12KB -> 24KB (2nd)
+       *               8193B -> 12KB -> 48KB (3rd)
+       *               8193B -> 12KB -> 96KB (4th)
+       *               ... successful sys_trim() ...
+       *               8193B -> 12KB -> 48KB (5th)
+       *
+       * AGGRESSIVE:   Round up the requested size to the nearest multiple
+       *               of page size, and then round it up to the nearest
+       *               power of 2, and furthermore grow the value exponentially
+       *               up to 1MB. For requested size >= 1MB, fall back to
+       *               CONSERVATIVE.
+       *               Example:
+       *               8193B -> 12KB -> 16KB (1st time)
+       *               8193B -> 12KB -> 32KB (2nd)
+       *               8193B -> 12KB -> 64KB (3rd)
+       *               8193B -> 12KB -> 128KB (4th)
+       *               ... successful sys_trim() ...
+       *               8193B -> 12KB -> 128KB (5th)
+       */
+
+      /* We may have more policies in the future so let's switch-case here. */
+      switch (mparams.nice) {
+        case NICE_AGGRESSIVE:
+          if (rsize < (1ULL << (20 - m->nallocs))) {
+            unsigned int power = 0;
+            --rsize;
+
+            // unroll the following loop to reduce overhead
+            // while (rsize >>= 1) ++power;
+
+#define ilg2(n) (rsize) & (1ULL << (n))
+            power = ilg2(19) ? 19 :
+              ilg2(18) ? 18 :
+              ilg2(17) ? 17 :
+              ilg2(16) ? 16 :
+              ilg2(15) ? 15 :
+              ilg2(14) ? 14 :
+              ilg2(13) ? 13 :
+              ilg2(12) ? 12 :
+              ilg2(11) ? 11 :
+              ilg2(10) ? 10 :
+              ilg2( 9) ?  9 :
+              ilg2( 8) ?  8 :
+              ilg2( 7) ?  7 :
+              ilg2( 6) ?  6 :
+              ilg2( 5) ?  5 :
+              ilg2( 4) ?  4 :
+              ilg2( 3) ?  3 :
+              ilg2( 2) ?  2 :
+              ilg2( 1) ?  1 : 0;
+            rsize = 1ULL << (power + 1 + m->nallocs);
+          } else if (rsize < (1ULL << 20)) {
+            rsize = (1ULL << 20);
+          }
+          break;
+        case NICE_MODERATE:
+          if (rsize < (1ULL << (20 - m->nallocs))) {
+            rsize *= (1ULL << m->nallocs);
+          } else if (rsize < (1ULL << 20)) {
+            rsize = (1ULL << 20);
+          }
+          break;
+        case NICE_CONSERVATIVE:
+        default:
+          break;
+      }
+
+      mp = zeroout ? m->alloc0func(1, rsize) : m->allocfunc(rsize);
+      if (mp == NULL)
+        tbase = CMFAIL;
+      else {
+        tbase = mp;
+        tsize = rsize;
+        mmap_flag = ALLOCFUNC_BIT;
+        if (mparams.nice < NICE_CONSERVATIVE && m->nallocs < 10)
+          ++(m->nallocs);
+      }
+    }
   }
 
   /* Directly map large chunks */
@@ -3542,106 +3633,6 @@ static void* sys_alloc(mstate m, size_t nb, int zeroout) {
     }
   }
 
-  if (
-       (zeroout && m->alloc0func) || 
-       (!zeroout && m->allocfunc) 
-     ) {
-      size_t rsize = granularity_align(nb + TOP_FOOT_SIZE + SIZE_T_ONE);
-      if (rsize > nb) { /* Check for wrap around 0. */
-        char *mp;
-
-        /* 
-         * CONSERVATIVE: Round up the requested size to the nearest multiple of
-         *               page size.
-         *               Example:
-         *               8193B -> 12KB
-         *      
-         * MODERATE:     Round up the requested size to the nearest multiple of
-         *               page size, and then grow the value exponentially up to
-         *               1MB. For requested size >= 1MB, fall back to
-         *               CONSERVATIVE. In addition, the exponential factor is
-         *               halved upon a successful sys_trim().
-         *               Example:
-         *               8193B -> 12KB (1st time)
-         *               8193B -> 12KB -> 24KB (2nd)
-         *               8193B -> 12KB -> 48KB (3rd)
-         *               8193B -> 12KB -> 96KB (4th)
-         *               ... successful sys_trim() ...
-         *               8193B -> 12KB -> 48KB (5th)
-         *                    
-         * AGGRESSIVE:   Round up the requested size to the nearest multiple
-         *               of page size, and then round it up to the nearest
-         *               power of 2, and furthermore grow the value exponentially
-         *               up to 1MB. For requested size >= 1MB, fall back to
-         *               CONSERVATIVE.
-         *               Example:
-         *               8193B -> 12KB -> 16KB (1st time)
-         *               8193B -> 12KB -> 32KB (2nd)
-         *               8193B -> 12KB -> 64KB (3rd)
-         *               8193B -> 12KB -> 128KB (4th)
-         *               ... successful sys_trim() ...
-         *               8193B -> 12KB -> 128KB (5th)
-         */
-
-        /* We may have more policies in the future so let's switch-case here. */
-        switch (mparams.nice) {
-        case NICE_AGGRESSIVE:
-            if (rsize < (1ULL << (20 - m->nallocs))) {
-                unsigned int power = 0;
-                --rsize;
-
-                // unroll the following loop to reduce overhead
-                // while (rsize >>= 1) ++power;
-
-#define ilg2(n) (rsize) & (1ULL << (n))
-                power = ilg2(19) ? 19 :
-                        ilg2(18) ? 18 :
-                        ilg2(17) ? 17 :
-                        ilg2(16) ? 16 :
-                        ilg2(15) ? 15 :
-                        ilg2(14) ? 14 :
-                        ilg2(13) ? 13 :
-                        ilg2(12) ? 12 :
-                        ilg2(11) ? 11 :
-                        ilg2(10) ? 10 :
-                        ilg2( 9) ?  9 :
-                        ilg2( 8) ?  8 :
-                        ilg2( 7) ?  7 :
-                        ilg2( 6) ?  6 :
-                        ilg2( 5) ?  5 :
-                        ilg2( 4) ?  4 :
-                        ilg2( 3) ?  3 :
-                        ilg2( 2) ?  2 :
-                        ilg2( 1) ?  1 : 0;
-                rsize = 1ULL << (power + 1 + m->nallocs);
-            } else if (rsize < (1ULL << 20)) {
-                rsize = (1ULL << 20);
-            }
-            break;
-        case NICE_MODERATE:
-            if (rsize < (1ULL << (20 - m->nallocs))) {
-                rsize *= (1ULL << m->nallocs);
-            } else if (rsize < (1ULL << 20)) {
-                rsize = (1ULL << 20);
-            }
-            break;
-        case NICE_CONSERVATIVE:
-        default:
-            break;
-        }
-
-        mp = zeroout ? m->alloc0func(1, rsize) : m->allocfunc(rsize);
-        if (mp == NULL)
-            tbase = CMFAIL;
-        else {
-            tbase = mp;
-            tsize = rsize;
-            mmap_flag = ALLOCFUNC_BIT;
-            if (mparams.nice < NICE_CONSERVATIVE && m->nallocs < 10)
-                ++(m->nallocs);
-        }
-      }
-  }
 
   if (tbase != CMFAIL) {
 
@@ -3661,6 +3652,16 @@ static void* sys_alloc(mstate m, size_t nb, int zeroout) {
         mchunkptr mn = next_chunk(mem2chunk(m));
         init_top(m, mn, (size_t)((tbase + tsize) - (char*)mn) -TOP_FOOT_SIZE);
       }
+    }
+
+    else if (mmap_flag == ALLOCFUNC_BIT) {
+      /* Segments obtained via allocfunc (system malloc) are
+         very unlikely to be contiguous. Even if they are,
+         we would leak memory in sys_trim by merging them. */
+      UPD_HIST(tsize, m, tbase);
+      if (tbase < m->least_addr)
+        m->least_addr = tbase;
+      add_segment(m, tbase, tsize, mmap_flag);
     }
 
     else {
@@ -3806,7 +3807,7 @@ static int sys_trim(mstate m, size_t pad, int force) {
             }
           }
         }
-        if (is_mmapped_segment(sp)) {
+        else if (is_mmapped_segment(sp)) {
           if (HAVE_MMAP &&
               sp->size >= extra &&
               !has_segment_link(m, sp)) { /* can't shrink if pinned */
