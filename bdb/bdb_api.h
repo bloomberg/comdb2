23c303c17d7d75c1e5a42fd6a90f12f1ab1c593c
Joe Mistachkin <joe@mistachkin.com>
2018-05-04 15:23:48 -0400

    Fix compilation issue.

--- bdb/bdb_api.h
+++ bdb/bdb_api.h
@@ -732,7 +732,7 @@ int bdb_prim_addkey_genid(bdb_state_type *bdb_handle, tran_type *tran,
                           int isnull, int *bdberr);
 int bdb_prim_delkey_genid(bdb_state_type *bdb_handle, tran_type *tran,
                           void *ixdta, int ixnum, int rrn,
-                          unsigned long long genid, int *bdberr);
+                          unsigned long long genid, int isnull, int *bdberr);
 int bdb_prim_updkey_genid(bdb_state_type *bdb_state, tran_type *tran, void *key,
                           int keylen, int ixnum, unsigned long long oldgenid,
                           unsigned long long genid, void *dta, int dtalen,
