b4ec2902058e193cdd3dc2d9281d2b34a354716a
Lingzhi Deng <ldeng33@bloomberg.net>
2018-05-10 15:14:46 -0400

    format

--- cdb2api/cdb2api.c
+++ cdb2api/cdb2api.c
@@ -2682,8 +2682,9 @@ int cdb2_close(cdb2_hndl_tp *hndl)
         if (hndl->debug_trace) {
             gettimeofday(&tv, NULL);
             fprintf(stderr, "%s: auto consume %d records took %lu ms\n",
-                    __func__, nrec, ((uint64_t)tv.tv_sec) * 1000 +
-                                        tv.tv_usec / 1000 - starttime);
+                    __func__, nrec,
+                    ((uint64_t)tv.tv_sec) * 1000 + tv.tv_usec / 1000 -
+                        starttime);
         }
     }
 
