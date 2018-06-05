8d9405950cf4bdfd6cbfa9b3924155a5945ff72d
Mark Hannum <mhannum72@gmail.com>
2018-06-05 14:25:33 -0400

    Fix jepsen failure for cinsert test- api should automatically changenode on changenode

--- cdb2api/cdb2api.c
+++ cdb2api/cdb2api.c
@@ -3957,10 +3957,27 @@ read_record:
                 PRINT_RETURN(0);
             }
             else if (is_retryable(hndl, err_val) &&
-                    (hndl->snapshot_file || (!hndl->in_trans && !is_commit))) {
+                    (hndl->snapshot_file || (!hndl->in_trans && !is_commit)
+                     || commit_file)) {
                 hndl->error_in_trans = 0;
                 newsql_disconnect(hndl, hndl->sb, __LINE__);
                 hndl->retry_all=1;
+                if (commit_file) {
+                    if (hndl->debug_trace) {
+                        fprintf(stderr,
+                                "td %u:%d: i am retrying, retries_done %d\n",
+                                (uint32_t)pthread_self(), __LINE__, retries_done);
+                        fprintf(stderr, "td %u %s:%d setting in_trans to 1\n",
+                                (uint32_t)pthread_self(), __func__, __LINE__);
+                    }
+                    hndl->in_trans = 1;
+                    hndl->snapshot_file = commit_file;
+                    hndl->snapshot_offset = commit_offset;
+                    hndl->is_retry = commit_is_retry;
+                    hndl->query_list = commit_query_list;
+                    commit_query_list = NULL;
+                    commit_file = 0;
+                }
                 if (hndl->debug_trace) {
                     fprintf(stderr, "td %u %s:%d goto retry_queries "
                                     "err_val=%d\n",
@@ -4061,10 +4078,27 @@ read_record:
             if (is_rollback) {
                 PRINT_RETURN(0);
             } else if (is_retryable(hndl, err_val) &&
-                    (hndl->snapshot_file || (!hndl->in_trans && !is_commit))) {
+                    (hndl->snapshot_file || (!hndl->in_trans && !is_commit) ||
+                     commit_file)) {
                 hndl->error_in_trans = 0;
                 newsql_disconnect(hndl, hndl->sb, __LINE__);
                 hndl->retry_all=1;
+                if (commit_file) {
+                    if (hndl->debug_trace) {
+                        fprintf(stderr,
+                                "td %u:%d: i am retrying, retries_done %d\n",
+                                (uint32_t)pthread_self(), __LINE__, retries_done);
+                        fprintf(stderr, "td %u %s:%d setting in_trans to 1\n",
+                                (uint32_t)pthread_self(), __func__, __LINE__);
+                    }
+                    hndl->in_trans = 1;
+                    hndl->snapshot_file = commit_file;
+                    hndl->snapshot_offset = commit_offset;
+                    hndl->is_retry = commit_is_retry;
+                    hndl->query_list = commit_query_list;
+                    commit_query_list = NULL;
+                    commit_file = 0;
+                }
                 if (hndl->debug_trace) {
                     fprintf(stderr, "td %u %s:%d goto retry_queries "
                                     "err_val=%d\n",
@@ -4135,8 +4169,8 @@ read_record:
     }
 
     if ((hndl->firstresponse->error_code == CDB2__ERROR_CODE__MASTER_TIMEOUT ||
-        hndl->firstresponse->error_code == CDB2ERR_CHANGENODE) && 
-            (hndl->snapshot_file || (!hndl->in_trans && !is_commit))) {
+       hndl->firstresponse->error_code == CDB2ERR_CHANGENODE) && 
+       (hndl->snapshot_file || (!hndl->in_trans && !is_commit) || commit_file)) {
         newsql_disconnect(hndl, hndl->sb, __LINE__);
         hndl->sb = NULL;
         hndl->retry_all = 1;
@@ -4181,7 +4215,8 @@ read_record:
     if (hndl->firstresponse->response_type == RESPONSE_TYPE__COLUMN_NAMES) {
         /* Handle rejects from Server. */
         if (is_retryable(hndl, hndl->firstresponse->error_code) &&
-                (hndl->snapshot_file || (!hndl->in_trans && !is_commit))) {
+                (hndl->snapshot_file || (!hndl->in_trans && !is_commit) ||
+                 commit_file)) {
             newsql_disconnect(hndl, hndl->sb, __LINE__);
             hndl->sb = NULL;
             hndl->retry_all = 1;
--- cdb2jdbc/src/main/java/com/bloomberg/comdb2/jdbc/Comdb2Handle.java
+++ cdb2jdbc/src/main/java/com/bloomberg/comdb2/jdbc/Comdb2Handle.java
@@ -1138,11 +1138,23 @@ public class Comdb2Handle extends AbstractConnection {
                         return 0;
                     }
                     else if (is_retryable(errVal) && (snapshotFile > 0 ||
-                                (!inTxn && !is_commit))) {
+                                (!inTxn && !is_commit) || commitSnapshotFile > 0)) {
                         tdlog(Level.FINER, "continuing on retryable error %d for null readNsh", errVal);
                         errorInTxn = 0;
                         closeNoException();
                         retryAll = true;
+                        if (commitSnapshotFile > 0) {
+                            tdlog(Level.FINER,
+                                    "Resetting txn state info on commit, isHASql=%b lsn=[%d][%d]",
+                                    isHASql, commitSnapshotFile, commitSnapshotOffset);
+                            inTxn = true;
+                            snapshotFile = commitSnapshotFile;
+                            snapshotOffset = commitSnapshotOffset;
+                            isRetry = commitIsRetry;
+                            queryList = commitQueryList;
+                            commitQueryList = null;
+                            commitSnapshotFile = 0;
+                        }
                         continue;
                     }
                     else {
@@ -1265,10 +1277,22 @@ public class Comdb2Handle extends AbstractConnection {
                         return 0;
                     }
                     else if (is_retryable(errVal) && (snapshotFile > 0 ||
-                                (!inTxn && !is_commit))) {
+                                (!inTxn && !is_commit) || commitSnapshotFile > 0)) {
                         errorInTxn = 0;
                         closeNoException();
                         retryAll = true;
+                        if (commitSnapshotFile > 0) {
+                            tdlog(Level.FINER,
+                                    "Resetting txn state info on commit, isHASql=%b lsn=[%d][%d]",
+                                    isHASql, commitSnapshotFile, commitSnapshotOffset);
+                            inTxn = true;
+                            snapshotFile = commitSnapshotFile;
+                            snapshotOffset = commitSnapshotOffset;
+                            isRetry = commitIsRetry;
+                            queryList = commitQueryList;
+                            commitQueryList = null;
+                            commitSnapshotFile = 0;
+                        }
                         tdlog(Level.FINER, "Retrying for failed protocol unpack errval=%d", errVal);
                         continue;
                     }
@@ -1303,9 +1327,9 @@ public class Comdb2Handle extends AbstractConnection {
 
             // no hints ..
 
-            if (firstResp.errCode == Sqlresponse.CDB2_ErrorCode.MASTER_TIMEOUT_VALUE ||
-                    firstResp.errCode == Errors.CDB2ERR_CHANGENODE) {
-
+            if ((firstResp.errCode == Sqlresponse.CDB2_ErrorCode.MASTER_TIMEOUT_VALUE ||
+                firstResp.errCode == Errors.CDB2ERR_CHANGENODE) && (snapshotFile > 0 ||
+                (!inTxn && !is_commit) || commitSnapshotFile > 0)) {
                 closeNoException();
                 retryAll = true;
                 if(commitSnapshotFile > 0) {
@@ -1343,7 +1367,7 @@ public class Comdb2Handle extends AbstractConnection {
                 /* Handle rejects from server. */
                 tdlog(Level.FINEST, "firstResp.respType==1");
                 if (is_retryable(firstResp.errCode) && (snapshotFile > 0 ||
-                            (!inTxn && !is_commit))) {
+                            (!inTxn && !is_commit) || commitSnapshotFile > 0)) {
                     closeNoException();
                     retryAll = true;
                     if (commitSnapshotFile > 0) {
