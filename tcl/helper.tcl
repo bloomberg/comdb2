02d4be8ac0b56aa519b55f1680bd1c9d5dd94e54
Joe Mistachkin <joe@mistachkin.com>
2018-06-13 08:19:29 -0400

    More enhancements to 'bind' tests in preparation for other data types.

--- tcl/helper.tcl
+++ tcl/helper.tcl
@@ -27,7 +27,8 @@ namespace eval ::tclcdb2 {
   #       context of the caller, where the value should be stored.  It will
   #       not be stored when it is a SQL NULL.  If either of the variable
   #       name arguments are an empty string, the associated value will not
-  #       be stored in the context of the caller.
+  #       be stored in the context of the caller.  This procedure may raise
+  #       script errors.
   #
   proc getNullableValue { connection index nullVarName valueVarName } {
     if {[string length $nullVarName] > 0} then {
@@ -42,13 +43,47 @@ namespace eval ::tclcdb2 {
       cdb2 colvalue $connection $index
     } localValue] == 0} then {
       set null false; set value $localValue
-    } elseif {$localValue eq "invalid column value\n"} {
+    } elseif {[isNullValueError $localValue]} then {
       set null true
     } else {
       error $localValue; # FAIL: Unexpected error.
     }
   }
 
+  #
+  # NOTE: This procedure is designed to query the value of a specific column
+  #       and return it.  The connection and index arguments are used to
+  #       specify the column to be queried.  The nullValue argument is the
+  #       value to return in the event that the column value is SQL NULL.
+  #       This procedure may raise script errors.
+  #
+  proc getValue { connection index {nullValue ""} } {
+    if {[catch {
+      cdb2 colvalue $connection $index
+    } value] == 0} then {
+      return $value
+    } elseif {[isNullValueError $value]} then {
+      return $nullValue
+    } else {
+      error $value; # FAIL: Unexpected error.
+    }
+  }
+
+  #
+  # NOTE: This procedure returns non-zero if the specified script error
+  #       message indicates a column value of SQL NULL was encountered.
+  #
+  proc isNullValueError { error } {
+    return [expr {$error eq "invalid column value\n"}]
+  }
+
+  #
+  # NOTE: Invoke the Tcl shared library loader on our associated library
+  #       using the file name appropriate for the platform.  This could
+  #       have been done directly in the package index file; however, by
+  #       doing it here we get the associated script library procedures
+  #       (i.e. in this file) loaded automatically.
+  #
   if {$::tcl_platform(platform) eq "windows"} then {
     load [file join [file dirname \
         [info script]] tclcdb2[info sharedlibextension]] tclcdb2
