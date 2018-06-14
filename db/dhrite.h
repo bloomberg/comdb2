88e57f38cb126a354a720b365d30fe9ae6466cba
Dorin Hogea <dhogea@bloomberg.net>
2018-06-14 16:26:40 -0400

    commit header

---
+++ db/dhrite.h
@@ -0,0 +1,34 @@
+/*
+   Copyright 2015 Bloomberg Finance L.P.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+#ifndef INCLUDED_DHRITE_H
+#define INCLUDED_DHRITE_H
+
+struct ast;
+typedef struct ast ast_t;
+
+enum ast_type {
+    AST_TYPE_INVALID = 0
+    ,AST_TYPE_SELECT = 1
+    ,AST_TYPE_INSERT = 2
+};
+
+ast_t* ast_init(void);
+int ast_push(ast_t *ast, enum ast_type op, void* obj);
+void ast_destroy(ast_t **ast);
+void ast_print(ast_t *ast);
+
+#endif
