e7fcf1eeb8556aa2e5a37878482a82a5c568447b
Mark Hannum <mhannum72@gmail.com>
2018-06-18 16:48:50 -0400

    sp using trylock on the schema-lock can lead to sporadic failures- paper over it with a sleep

--- db/sqlinterfaces.c
+++ db/sqlinterfaces.c
@@ -2646,6 +2646,8 @@ int get_prepared_stmt_try_lock(struct sqlthdstate *thd,
 {
     if (tryrdlock_schema_lk() != 0) {
         // only schemachange will wrlock(schema)
+        sql_debug_logf(clnt, __func__, __LINE__, "Returning SQLITE_SCHEMA on "
+                "tryrdlock failure\n");
         return SQLITE_SCHEMA;
     }
     int rc = get_prepared_stmt_int(thd, clnt, rec, err, initial);
