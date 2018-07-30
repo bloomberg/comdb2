c1b806c28a41bcc912a19d97fa63bdbb02efb6b6
kyleliangus <kyleliangus@gmail.com>
2018-07-30 20:27:23 +0000

    Truncation approach directly from server logs, but not finished.

--- db/truncate_log.h
+++ db/truncate_log.h
@@ -1,6 +1,9 @@
 #ifndef TRUNCATE_LOG_H
 #define TRUNCATE_LOG_H
 
+#include <phys_rep_lsn.h>
+
 int truncate_log(unsigned int file, unsigned int offset);
+LOG_INFO handle_truncation(cdb2_hndl_tp* repl_db, LOG_INFO prev_info);
 
 #endif
