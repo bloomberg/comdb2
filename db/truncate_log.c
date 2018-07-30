3f7ea2c4b7ea15c6aabb814f3b93a9d43eba8df7
kyleliangus <kyleliangus@gmail.com>
2018-07-30 23:51:16 +0000

    Modified replicant to query lsns, but recovering master has invalid reads.

--- db/truncate_log.c
+++ db/truncate_log.c
@@ -16,10 +16,10 @@ LOG_INFO handle_truncation(cdb2_hndl_tp* repl_db, LOG_INFO latest_info)
     if (match_lsn.file == 0)
     {
         logmsg(LOGMSG_WARN, "File is 0, ignoring\n");
-        return match_lsn;
+        return latest_info;
     }
 
-    logmsg(LOGMSG_WARN, "Rewind to lsn: {%u: %u}", match_lsn.file, match_lsn.offset);
+    logmsg(LOGMSG_WARN, "Rewind to lsn: {%u:%u}", match_lsn.file, match_lsn.offset);
 
     /* TODO: Actually call truncation */
     return match_lsn;
@@ -35,26 +35,14 @@ LOG_INFO find_match_lsn(cdb2_hndl_tp* repl_db, LOG_INFO start_info)
     int blob_len;
     unsigned int file, offset;
     LOG_INFO info;
+    info.file = 0;
+    info.offset = 0;
 
-    file = start_info.file;
-    offset = start_info.offset;
-
-    /*TODO: hardcoded rectypes, maybe expose in a nicer way? */
     /* TODO: implement optimization in checking first lsn from the master */
-
-    rc = snprintf(sql_cmd, sql_cmd_len, 
-            "select * from comdb2_transaction_logs('{%u:%u}','{%u:%u}') "
-            "where rectype=15 or rectype=16 or rectype=10", 
-            file, offset, file, offset);
-    if (rc < 0 || rc >= sql_cmd_len)
-        logmsg(LOGMSG_ERROR, "sql_cmd buffer is not long enough!\n");
 
     if ((rc = open_db_cursor(thedb->bdb_env)) != 0)
     {
         logmsg(LOGMSG_ERROR, "Couldn't open a db cursor\n");
-        info.file = 0;
-        info.offset = 0;
-
         close_db_cursor();
         return info;
     }
@@ -64,8 +52,27 @@ LOG_INFO find_match_lsn(cdb2_hndl_tp* repl_db, LOG_INFO start_info)
     {
         unsigned int match_file, match_offset;
 
-        fprintf(stderr, "{%u:%u}\n", start_info.file, start_info.offset);
-        /*
+        /* TODO: hardcoded rectypes, maybe expose in a nicer way? */
+        rc = snprintf(sql_cmd, sql_cmd_len, 
+                "select * from comdb2_transaction_logs('{%u:%u}','{%u:%u}') "
+                "where rectype=15 or rectype=16 or rectype=10", 
+                start_info.file, start_info.offset, 
+                start_info.file, start_info.offset);
+        if (rc < 0 || rc >= sql_cmd_len)
+            logmsg(LOGMSG_ERROR, "sql_cmd buffer is not long enough!\n");
+
+        if ((rc = cdb2_run_statement(repl_db, sql_cmd)) != CDB2_OK)
+        {
+            logmsg(LOGMSG_ERROR, "Couldn't query the database\n");
+            return info;
+        }
+
+        if ((rc = cdb2_next_record(repl_db)) != CDB2_OK)
+        {
+            logmsg(LOGMSG_WARN, "Matchable record doesn't exist, continuing\n");
+            continue;
+        }
+
         lsn = (char *) cdb2_column_value(repl_db, 0);
         blob = cdb2_column_value(repl_db, 4);
         blob_len = cdb2_column_size(repl_db, 4);
@@ -83,12 +90,11 @@ LOG_INFO find_match_lsn(cdb2_hndl_tp* repl_db, LOG_INFO start_info)
             info.size = blob_len;
             return info;
         } 
-        */
+        
     }
 
     logmsg(LOGMSG_WARN, "No matchable lsns in the log\n");
-    info.file = 0;
-    info.offset = 0; 
+    close_db_cursor();
 
     return info;
 }
@@ -97,6 +103,5 @@ LOG_INFO find_match_lsn(cdb2_hndl_tp* repl_db, LOG_INFO start_info)
 
 int truncate_log(unsigned int file, unsigned int offset)
 {
-
     return truncate_log_lock(thedb->bdb_env, file, offset);
 }
