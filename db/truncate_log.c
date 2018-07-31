2dca60a13f2c353a60ab2f42a9383828af434dc1
kyleliangus <kyleliangus@gmail.com>
2018-07-31 21:11:46 +0000

    Removed random queries into log cursor, use merge-style algorithm.

--- db/truncate_log.c
+++ db/truncate_log.c
@@ -22,18 +22,18 @@ LOG_INFO handle_truncation(cdb2_hndl_tp* repl_db, LOG_INFO latest_info)
     logmsg(LOGMSG_WARN, "Rewind to lsn: {%u:%u}", match_lsn.file, match_lsn.offset);
 
     /* TODO: Actually call truncation */
+    truncate_log(match_lsn.file, match_lsn.offset);
     return match_lsn;
 }
 
 LOG_INFO find_match_lsn(cdb2_hndl_tp* repl_db, LOG_INFO start_info)
 {
-    size_t sql_cmd_len = 150;
-    char sql_cmd[sql_cmd_len];
+    char* sql_cmd;
     int rc;
     void* blob;
     char* lsn;
     int blob_len;
-    unsigned int file, offset;
+    unsigned int match_file, match_offset;
     LOG_INFO info;
     info.file = 0;
     info.offset = 0;
@@ -47,40 +47,60 @@ LOG_INFO find_match_lsn(cdb2_hndl_tp* repl_db, LOG_INFO start_info)
         return info;
     }
 
+    /* TODO: hardcoded rectypes, maybe expose in a nicer way? */
+    sql_cmd = "select * from comdb2_transaction_logs(NULL, NULL, 4) "
+            "where rectype=15 or rectype=16 or rectype=10";
+    if ((rc = cdb2_run_statement(repl_db, sql_cmd)) != CDB2_OK)
+    {
+        logmsg(LOGMSG_ERROR, "Couldn't query the database\n");
+        return info;
+    }
+
+    /* Get head ptrs for both our db cursor and remote db cursor */
+
+    if ((rc = cdb2_next_record(repl_db)) != CDB2_OK)
+    {
+        logmsg(LOGMSG_WARN, "Cursor has no more records.\n");
+        return info;
+    }
+    lsn = (char *) cdb2_column_value(repl_db, 0);
+    if ((rc = char_to_lsn(lsn, &match_file, &match_offset)) != 0)
+    {
+        logmsg(LOGMSG_ERROR, "Could not parse lsn? %s\n", lsn);
+    }
+    logmsg(LOGMSG_WARN, "start with lsn{%u:%u}\n", match_file, match_offset);
+
     // keep checking until we have a matching lsn
     while(!(rc = get_next_matchable(&start_info)))
     {
-        unsigned int match_file, match_offset;
-
-        /* TODO: hardcoded rectypes, maybe expose in a nicer way? */
-        rc = snprintf(sql_cmd, sql_cmd_len, 
-                "select * from comdb2_transaction_logs('{%u:%u}','{%u:%u}') "
-                "where rectype=15 or rectype=16 or rectype=10", 
-                start_info.file, start_info.offset, 
-                start_info.file, start_info.offset);
-        if (rc < 0 || rc >= sql_cmd_len)
-            logmsg(LOGMSG_ERROR, "sql_cmd buffer is not long enough!\n");
-
-        if ((rc = cdb2_run_statement(repl_db, sql_cmd)) != CDB2_OK)
+        while(match_file > start_info.file || match_offset > start_info.offset)
         {
-            logmsg(LOGMSG_ERROR, "Couldn't query the database\n");
-            return info;
+            if ((rc = cdb2_next_record(repl_db)) != CDB2_OK)
+            {
+                logmsg(LOGMSG_WARN, "Cursor has no more records.\n");
+                return info;
+            }
+            lsn = (char *) cdb2_column_value(repl_db, 0);
+            if ((rc = char_to_lsn(lsn, &match_file, &match_offset)) != 0)
+            {
+                logmsg(LOGMSG_ERROR, "Could not parse lsn? %s\n", lsn);
+            }
+
+            logmsg(LOGMSG_WARN, "? lsn{%u:%u}\n", match_file, match_offset);
         }
 
-        if ((rc = cdb2_next_record(repl_db)) != CDB2_OK)
+        /* check if lsns match, if not, then get next matchable */
+        if (match_file != start_info.file || 
+                match_offset != start_info.offset)
         {
-            logmsg(LOGMSG_WARN, "Matchable record doesn't exist, continuing\n");
+            logmsg(LOGMSG_WARN, "not same lsn{%u:%u}\n", match_file, match_offset);
             continue;
         }
 
-        lsn = (char *) cdb2_column_value(repl_db, 0);
+        /* here lsns match, thus we can now compare them */
+
         blob = cdb2_column_value(repl_db, 4);
         blob_len = cdb2_column_size(repl_db, 4);
-
-        if ((rc = char_to_lsn(lsn, &match_file, &match_offset)) != 0)
-        {
-            logmsg(LOGMSG_ERROR, "Could not parse lsn? %s\n", lsn);
-        }
 
         if ((rc = compare_log(thedb->bdb_env, match_file, match_offset, 
                         blob, blob_len)) == 0)
