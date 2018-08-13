61c233473803eb2a10b12ed3c79f6734f33625e6
kyleliangus <kyleliangus@gmail.com>
2018-08-13 18:00:43 +0000

    Prep work for more testing.

--- tests/phys_rep.test/generate_tests.sh
+++ tests/phys_rep.test/generate_tests.sh
@@ -15,10 +15,14 @@ for i in $(seq 1 $NRUNS); do
     id=$(($RANDOM % $NRECS))
     case $what in
         0)  echo "insert into t1 (id, a, b, c, d, e, f, g, h, i, j) values ($id, 1, 2, 3, 4, 5, 6.000000, 7.000000, 'eight', x'99', now());"
-    ;;
-    1)  echo "delete from t1 where id = $id limit 1"
-;;
-2)  echo "update t1 set a=a+1, b=b+2 where id=$id"
-;;
-esac
+            ;;
+        1)  echo "delete from t1 where id = $id limit 1"
+            ;;
+        2)  echo "update t1 set a=a+1, b=b+2 where id=$id"
+            ;;
+    esac
 done > 2-update.src.sql
+
+for i in $(seq 1 $NRECS); do
+    echo "delete from t1 where id=$i"
+done > 3-delete.src.sql
