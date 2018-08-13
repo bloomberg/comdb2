504969ec58a8d2ed9cf73ce4a69976ab203174f5
kyleliangus <kyleliangus@gmail.com>
2018-08-13 21:03:37 +0000

    Cleanup my directory.

--- tests/phys_reptest.test/runtest.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-. /home/kliang43/comdb2/tests/phys_reptest.test/setenv.sh 
-cd /home/kliang43/comdb2/tests/phys_reptest.test/
-./runit physreptest50758 $1 
-echo; echo; echo;
-/home/kliang43/comdb2/tests/unsetup
