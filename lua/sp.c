fa2fb7a936f155e841e9fd7393ca06912726bfbd
Mark Hannum <mhannum72@gmail.com>
2018-08-15 14:31:24 -0400

    Return lua error rather than crash if user attempt to emit nothing

--- lua/sp.c
+++ lua/sp.c
@@ -4790,7 +4790,9 @@ static int l_send_back_row(Lua lua, sqlite3_stmt *stmt, int nargs)
 {
     int rc = 0;
     SP sp = getsp(lua);
-    assert(nargs > 0);
+    if (nargs <= 0) {
+        return luabb_error(lua, sp, "no row to send back");
+    }
     if (nargs > MAXCOLUMNS) {
         return luabb_error(lua, sp, "attempt to read %d cols (maxcols:%d)",
                            nargs, MAXCOLUMNS);
