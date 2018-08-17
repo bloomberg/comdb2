45d2d43044945a6364457a174e4a2997ec6286ad
Mark Hannum <mhannum72@gmail.com>
2018-08-17 11:15:16 -0400

    Bug found & fixed

--- db/db_tunables.c
+++ db/db_tunables.c
@@ -193,6 +193,7 @@ extern int gbl_rand_elect_min_ms;
 extern int gbl_rand_elect_max_ms;
 extern int gbl_handle_buf_add_latency_ms;
 extern int gbl_osql_send_startgen;
+extern int gbl_instrument_dblist;
 
 extern long long sampling_threshold;
 
