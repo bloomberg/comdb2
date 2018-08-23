ac66abade47e7aac5798f7d8b24c495d84005fcc
Joe Mistachkin <joe@mistachkin.com>
2018-08-23 15:29:32 -0400

    Remove bad malloc from the token processing code path in the comdb2CreateProcedure function.

--- sqlite/src/comdb2build.c
+++ sqlite/src/comdb2build.c
@@ -853,7 +853,6 @@ void comdb2CreateProcedure(Parse* pParse, Token* nm, Token* ver, Token* proc)
     }
     struct schema_change_type* sc = new_schemachange_type();
     strcpy(sc->table, name);
-    sc->newcsc2 = malloc(proc->n);
     sc->addsp = 1;
     if (ver) {
         TokenStr(version, ver);
