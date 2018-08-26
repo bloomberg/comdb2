d9187fab685bf496ad852145213bf7c7043e6f82
Mark Hannum <mhannum72@gmail.com>
2018-08-25 23:43:00 -0400

    Fix very subtle bug that was corrupting llmeta on physical replicants

--- bdb/ll.c
+++ bdb/ll.c
@@ -1478,6 +1478,7 @@ int ll_rowlocks_bench(bdb_state_type *bdb_state, tran_type *tran, int op,
 
 extern int gbl_rowlocks;
 extern int gbl_fullrecovery;
+extern int gbl_is_physical_replicant;
 
 int ll_checkpoint(bdb_state_type *bdb_state, int force)
 {
@@ -1488,8 +1489,9 @@ int ll_checkpoint(bdb_state_type *bdb_state, int force)
     int bdberr;
 
     if (gbl_rowlocks &&
+        !gbl_is_physical_replicant &&
         (gbl_fullrecovery ||
-         bdb_state->repinfo->master_host == bdb_state->repinfo->myhost)) {
+        (bdb_state->repinfo->master_host == bdb_state->repinfo->myhost))) {
 
         /* Grab current lsn first */
         __log_txn_lsn(bdb_state->dbenv, &curlsn, NULL, NULL);
