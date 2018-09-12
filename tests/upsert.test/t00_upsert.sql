4a1ebd084624dbae0851abfe0652ab9f1a3fbb80
Joe Mistachkin <jmistachkin@bloomberg.net>
2018-09-12 11:21:28 -0400

    Add sections to the first UPSERT test case file.

--- tests/upsert.test/t00_upsert.sql
+++ tests/upsert.test/t00_upsert.sql
@@ -1,14 +1,17 @@
+SELECT '---------------------------------- PART #01 ----------------------------------' AS part;
 # Simple cases
 CREATE TABLE t1(i INT) $$
 INSERT INTO t1 VALUES(1), (1);
 SELECT * FROM t1;
 DROP TABLE t1;
 
+SELECT '---------------------------------- PART #02 ----------------------------------' AS part;
 CREATE TABLE t1(i INT PRIMARY KEY) $$
 INSERT INTO t1 VALUES(1), (1);
 SELECT * FROM t1;
 DROP TABLE t1;
 
+SELECT '---------------------------------- PART #03 ----------------------------------' AS part;
 # Test with non-unique (dup) key
 CREATE TABLE t1(i INT PRIMARY KEY, j INT) $$
 CREATE INDEX t1_j ON t1(j)
@@ -17,6 +20,7 @@ INSERT INTO t1 VALUES(1,1);
 SELECT * FROM t1;
 DROP TABLE t1;
 
+SELECT '---------------------------------- PART #04 ----------------------------------' AS part;
 # ON CONFLICT DO NOTHING
 CREATE TABLE t1(i INT PRIMARY KEY, j INT UNIQUE) $$
 INSERT INTO t1 VALUES(0,1);
@@ -24,12 +28,14 @@ INSERT INTO t1 VALUES(1,1) ON CONFLICT DO NOTHING;
 SELECT * FROM t1;
 DROP TABLE t1;
 
+SELECT '---------------------------------- PART #05 ----------------------------------' AS part;
 # ON CONFLICT DO REPLACE
 CREATE TABLE t1(i INT PRIMARY KEY, j INT UNIQUE) $$
 INSERT INTO t1 VALUES(0,1);
 REPLACE INTO t1 VALUES(1,1);
 SELECT * FROM t1;
 
+SELECT '---------------------------------- PART #06 ----------------------------------' AS part;
 CREATE TABLE t2(i INT PRIMARY KEY, j INT UNIQUE) $$
 INSERT INTO t2 VALUES(0,1);
 INSERT INTO t2 VALUES(1,0);
@@ -38,12 +44,15 @@ INSERT INTO t2 VALUES(1,1);
 REPLACE INTO t2 VALUES(1,1), (2,2), (3,3);
 SELECT * FROM t2;
 
+SELECT '---------------------------------- PART #07 ----------------------------------' AS part;
 CREATE TABLE t3(i INT PRIMARY KEY, j INT, k INT UNIQUE) $$
 INSERT INTO t3 VALUES(0,1,0);
 INSERT INTO t3 VALUES(1,0,1);
 # Another syntax for .. ON CONFLICT DO REPLACE ..
 REPLACE INTO t3 VALUES(1,2,0);
 SELECT * FROM t3;
+
+SELECT '---------------------------------- PART #08 ----------------------------------' AS part;
 # A case where the same record is up for deletion twice.
 REPLACE INTO t3 VALUES(1,3,0);
 SET TRANSACTION READ COMMITTED;
@@ -52,10 +61,12 @@ SET TRANSACTION BLOCKSQL;
 
 SELECT * FROM t3 ORDER BY i;
 
+SELECT '---------------------------------- PART #09 ----------------------------------' AS part;
 DROP TABLE t1;
 DROP TABLE t2;
 DROP TABLE t3;
 
+SELECT '---------------------------------- PART #10 ----------------------------------' AS part;
 # ON CONFLICT DO UPDATE
 CREATE TABLE t1(i INT PRIMARY KEY, j INT, k INT UNIQUE) $$
 INSERT INTO t1 VALUES(0,1,0);
@@ -69,16 +80,20 @@ INSERT INTO t1 VALUEs(2,1,3) ON CONFLICT (i) DO UPDATE SET i=1
 SELECT * FROM t1 ORDER BY i;
 DROP TABLE t1;
 
+SELECT '---------------------------------- PART #11 ----------------------------------' AS part;
 # EXCLUDED (sqlite/test/upsert2.test)
 CREATE TABLE t1(a INTEGER PRIMARY KEY, b int, c DEFAULT 0)$$
 INSERT INTO t1(a,b) VALUES(1,2),(3,4);
 INSERT INTO t1(a,b) VALUES(1,8),(2,11),(3,1) ON CONFLICT(a) DO UPDATE SET b=excluded.b, c=c+1 WHERE t1.b<excluded.b;
 SELECT * FROM t1 ORDER BY a;
 DROP TABLE t1;
+
+SELECT '---------------------------------- PART #12 ----------------------------------' AS part;
 CREATE TABLE t1(x INTEGER PRIMARY KEY, y INT UNIQUE)$$
 INSERT INTO t1(x,y) SELECT 1,2 WHERE 1 ON CONFLICT(x) DO UPDATE SET y=max(t1.y,excluded.y) AND 1;
 DROP TABLE t1;
 
+SELECT '---------------------------------- PART #13 ----------------------------------' AS part;
 CREATE TABLE t1(x INTEGER PRIMARY KEY, y INT UNIQUE) $$
 INSERT INTO t1(x,y) SELECT 1,2 WHERE 1 ON CONFLICT(x) DO UPDATE SET y=max(t1.y,excluded.y) AND 1;
 SELECT * FROM t1
@@ -86,6 +101,7 @@ INSERT INTO t1(x,y) SELECT 1,1 WHERE 1 ON CONFLICT(x) DO UPDATE SET y=max(t1.y,e
 SELECT * FROM t1
 DROP TABLE t1;
 
+SELECT '---------------------------------- PART #14 ----------------------------------' AS part;
 # INDEX ON EXPRESSION (sqlite/test/upsert1.test)
 CREATE TABLE t1 {
 schema
@@ -103,10 +119,13 @@ keys
 INSERT INTO t1(a,b) VALUES(7,8) ON CONFLICT(a+b) DO NOTHING;
 INSERT INTO t1(a,b) VALUES(8,7),(9,6) ON CONFLICT(a+b) DO NOTHING;
 SELECT * FROM t1;
+
+SELECT '---------------------------------- PART #15 ----------------------------------' AS part;
 INSERT INTO t1(a,b) VALUES(8,7),(9,6) ON CONFLICT(a) DO NOTHING;
 SELECT * FROM t1;
 DROP TABLE t1;
 
+SELECT '---------------------------------- PART #16 ----------------------------------' AS part;
 CREATE TABLE t1(i INT PRIMARY KEY, j INT UNIQUE, k INT, l INT UNIQUE)$$
 CREATE INDEX t1_k ON t1(k);
 INSERT INTO t1 VALUES(1,1,1,1);
@@ -118,26 +137,38 @@ INSERT INTO t1 VALUES(1,1,1,1) ON CONFLICT(k) DO NOTHING;
 INSERT INTO t1 VALUES(1,1,1,1) ON CONFLICT(l) DO NOTHING;
 INSERT INTO t1 VALUES(1,1,1,1) ON CONFLICT(m) DO NOTHING;
 SELECT * FROM t1;
+
+SELECT '---------------------------------- PART #17 ----------------------------------' AS part;
 INSERT INTO t1 VALUES(2,1,1,1) ON CONFLICT(j) DO NOTHING;
 INSERT INTO t1 VALUES(2,1,1,2) ON CONFLICT(j) DO NOTHING;
 SELECT * FROM t1;
+
+SELECT '---------------------------------- PART #18 ----------------------------------' AS part;
 INSERT INTO t1 VALUES(2,2,1,1) ON CONFLICT(l) DO NOTHING;
 SELECT * FROM t1;
+
+SELECT '---------------------------------- PART #19 ----------------------------------' AS part;
 INSERT INTO t1 VALUES(99, 99, 1, 99) ON CONFLICT DO NOTHING;
 SELECT * FROM t1;
 DROP TABLE t1;
 
+SELECT '---------------------------------- PART #20 ----------------------------------' AS part;
 CREATE TABLE t1 (i INT, j INT) $$
 CREATE UNIQUE INDEX idx1 ON t1(i) WHERE j > 6;
 INSERT INTO t1 VALUES(1, 10);
 INSERT INTO t1 VALUES(1, 11);
 SELECT * FROM t1;
+
+SELECT '---------------------------------- PART #21 ----------------------------------' AS part;
 INSERT INTO t1 VALUES(1, 4);
 SELECT * FROM t1;
+
+SELECT '---------------------------------- PART #22 ----------------------------------' AS part;
 INSERT INTO t1 VALUES(1, 4) ON CONFLICT(i) WHERE j > 6 DO NOTHING;
 SELECT * FROM t1;
 DROP TABLE t1;
 
+SELECT '---------------------------------- PART #23 ----------------------------------' AS part;
 # Test uniqnulls
 CREATE TABLE t1(i INT UNIQUE)$$
 INSERT INTO t1 VALUES(1);
