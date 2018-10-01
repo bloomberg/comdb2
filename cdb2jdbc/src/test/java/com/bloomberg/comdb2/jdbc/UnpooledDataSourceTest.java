2e01fcefecc2b8c40ecb46e8722d4a4a806fc0ac
Rivers Zhang <hzhang320@bloomberg.net>
2018-10-01 15:25:29 +0000

    Adding a test.

---
+++ cdb2jdbc/src/test/java/com/bloomberg/comdb2/jdbc/UnpooledDataSourceTest.java
@@ -0,0 +1,26 @@
+package com.bloomberg.comdb2.jdbc;
+
+import java.sql.*;
+import java.util.logging.*;
+import org.junit.*;
+import org.junit.Assert.*;
+import java.util.*;
+
+public class UnpooledDataSourceTest {
+
+    @Test public void testDatabaseInitSqls() throws SQLException {
+        String db = System.getProperty("cdb2jdbc.test.database");
+        String cluster = System.getProperty("cdb2jdbc.test.cluster");
+
+        UnpooledDataSource ds = new UnpooledDataSource();
+        ds.setDriver("com.bloomberg.comdb2.jdbc.Driver");
+        ds.setUrl(String.format("jdbc:comdb2://%s/%s", cluster, db));
+        ds.setConnectionInitSqls(Arrays.asList("SET TIMEZONE Zulu"));
+
+        Connection conn = ds.getConnection();
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT CAST(NOW() AS TEXT)");
+        String zulu = rs.getString(1);
+        Assert.assertTrue("Should get back a time in Zulu", zulu.contains("Zulu"));
+    }
+}
