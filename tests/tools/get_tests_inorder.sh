fd9fda5e1f492a5574cfdac2610d925a0124b7d5
Adi Zaimi <azaimi@bloomberg.net>
2018-10-06 01:54:33 -0400

    have generated tests copied on the fly rather than create dir locally

--- tests/tools/get_tests_inorder.sh
+++ tests/tools/get_tests_inorder.sh
@@ -3,8 +3,13 @@
 #get list of tests in order from longest to shortest test to run
 DEFAULT_TIMEOUT=5
 
-for i in ` ls -d *.test` ; do 
+tests=`ls -d *.test | grep -v '_generated.test'`
+for i in $tests ; do 
     a=`cat $i/Makefile | grep TEST_TIMEOUT | egrep -v "ifeq" | cut -f2 -d'=' | sed 's/m//'` ; 
     [ -z "$a" ] && a=$DEFAULT_TIMEOUT; 
     echo $a $i; 
+    for j in `ls $i/*.testopts 2> /dev/null` ; do 
+        gen=`echo $j | sed 's#\.testopts#_generated#g; s#/#_#g; s#\.test##g; s#$#.test#'`
+        echo $a $gen;
+    done
 done | sort -nr | cut -d' ' -f2
