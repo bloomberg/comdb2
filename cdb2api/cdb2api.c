f077c6d7792894f9d3cb675a077cdf075ac0e5eb
Mohit Khullar <mkhullar1@bloomberg.net>
2018-10-08 12:08:09 -0400

    Delay clear_responses until send

--- cdb2api/cdb2api.c
+++ cdb2api/cdb2api.c
@@ -3634,7 +3634,6 @@ static int cdb2_run_statement_typed_int(cdb2_hndl_tp *hndl, const char *sql,
 
 retry_queries:
 
-    clear_responses(hndl);
 
     debugprint(
         "retry_queries: hndl->host=%d (%s)\n", hndl->connected_host,
@@ -3708,6 +3707,8 @@ retry_queries:
             }
         }
     }
+
+    clear_responses(hndl);
 
     hndl->sql = (char *)sql;
     hndl->ntypes = ntypes;
