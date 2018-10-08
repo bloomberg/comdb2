4457603eec9d12a345e5f6ce3773b7cb667bf51f
Mohit Khullar <mkhullar1@bloomberg.net>
2018-10-08 13:11:39 -0400

    clang

--- cdb2api/cdb2api.c
+++ cdb2api/cdb2api.c
@@ -3633,8 +3633,6 @@ static int cdb2_run_statement_typed_int(cdb2_hndl_tp *hndl, const char *sql,
     int run_last = 1;
 
 retry_queries:
-
-
     debugprint(
         "retry_queries: hndl->host=%d (%s)\n", hndl->connected_host,
         (hndl->connected_host >= 0 ? hndl->hosts[hndl->connected_host] : ""));
