ad2bb0e5d7e58f1cd35b6d16d642e503ade67946
Adi Zaimi <azaimi@bloomberg.net>
2018-10-09 00:46:29 -0400

    speedup significantly get_tests_inorder so autocomplete for make is instantaneous now

--- tests/tools/get_tests_inorder.sh
+++ tests/tools/get_tests_inorder.sh
@@ -3,13 +3,19 @@
 #get list of tests in order from longest to shortest test to run
 DEFAULT_TIMEOUT=5
 
-tests=`ls -d *.test | grep -v '_generated.test'`
-for i in $tests ; do 
-    a=`cat $i/Makefile | grep TEST_TIMEOUT | egrep -v "ifeq" | cut -f2 -d'=' | sed 's/m//'` ; 
-    [ -z "$a" ] && a=$DEFAULT_TIMEOUT; 
-    echo $a $i; 
-    for j in `ls $i/*.testopts 2> /dev/null` ; do 
-        gen=`echo $j | sed 's#\.testopts#_generated#g; s#/#_#g; s#\.test##g; s#$#.test#'`
-        echo $a $gen;
-    done
-done | sort -nr | cut -d' ' -f2
+#get the tests with custom times
+custom_times=`grep "TEST_TIMEOUT=" *.test/Makefile | sed 's#export TEST_TIMEOUT=##; s#/Makefile:# #; s#m$##'`
+
+#get the tests with default times
+default_times=`grep -c "TEST_TIMEOUT=" *.test/Makefile  | grep ":0" | sed "s#/Makefile:0# $DEFAULT_TIMEOUT#"`
+IFS=$'\n'; 
+
+#get the generated tests
+generated=$(for j in `ls */*.testopts` ; do 
+    gen=`echo $j | sed 's#\.testopts#_generated#g; s#/#_#g; s#\.test##g; s#$#.test#'`
+    name=`dirname $j`
+    time=`echo -e "${custom_times[*]} \n${default_times[*]}" | grep $name | awk '{print $2}'`
+    echo $gen $time
+done )
+
+echo -e "${custom_times[*]} \n${default_times[*]} \n${generated[*]}" | awk '{print $2,$1}'| sort -nr | awk '{print $2}'
