33afca82610c91c71fb22961521ebba8579ccb55
Adi Zaimi <azaimi@bloomberg.net>
2018-11-01 16:27:56 -0400

    secondary db logic from setup and unsetup rather than from runtestcase

--- tests/testcase.mk
+++ tests/testcase.mk
@@ -40,6 +40,14 @@ export CDB2_OPTIONS=--cdb2cfg $(CDB2_CONFIG)
 export COMDB2_ROOT=$(TESTDIR)
 export COMDB2_UNITTEST?=0
 
+
+ifneq ($(SECONDARY_DB_PREFIX),)
+export SECONDARY_DBNAME=$(SECONDARY_DB_PREFIX)$(DBNAME)
+export SECONDARY_DBDIR=$(TESTDIR)/$(SECONDARY_DBNAME)
+export SECONDARY_CDB2_CONFIG=$(abspath $(SECONDARY_DBDIR)/comdb2db.cfg)
+export SECONDARY_CDB2_OPTIONS=--cdb2cfg $(SECONDARY_CDB2_CONFIG)
+endif
+
 ifneq ($(INSETUP),)
   # we are in setup or running make from within a testdir
   $(shell TESTDIR="${TESTDIR}" CLUSTER="${CLUSTER}" SKIPSSL="${SKIPSSL}" ${TESTSROOTDIR}/tools/keygen.sh )
