b6198adfd0bb9401df245fb0c040790964ccdb1c
Nirbhay Choubey <nchoubey@bloomberg.net>
2018-11-15 12:19:01 -0500

    Update docs

--- docs/src/sqlitegen/bubble-generator-data.tcl
+++ docs/src/sqlitegen/bubble-generator-data.tcl
@@ -696,23 +696,38 @@ set all_graphs {
               {line RENAME TO new-table-name}
               {loop
                   {or
-                      {line ADD column-name column-type
-                          {opt {loop {line column-constraint } { , } } }
+                      {line ADD
+                          {or
+                              {line column-name column-type
+                                  {opt {loop {line column-constraint } { , } } }
+                              }
+                              {line PRIMARY KEY ( index-column-list ) }
+                              {stack
+                                  {line {opt UNIQUE } INDEX index-name
+                                      ( index-column-list ) }
+                                  {line {opt WITH DATACOPY } {opt WHERE expr } }
+                              }
+                              {stack
+                                  {line {opt CONSTRAINT constraint-name } }
+                                  {line FOREIGN KEY ( index-column-list ) foreign-key-def }
+                              }
+                          }
                       }
-                      {line DROP {opt COLUMN} column-name }
-                      {stack
-                          {line ADD {opt UNIQUE } INDEX index-name
-                              ( index-column-list ) }
-                          {line {opt OPTION DATACOPY } {opt WHERE expr } }
+                      {line ALTER
+                          {or
+                              {line column-name column-type
+                                  {opt {loop {line column-constraint } { , } } }
+                              }
+                          }
                       }
-                      {line DROP INDEX index-name }
-                      {line ADD PRIMARY KEY ( index-column-list ) }
-                      {line DROP PRIMARY KEY }
-                      {stack
-                          {line ADD {opt CONSTRAINT constraint-name } }
-                          {line FOREIGN KEY ( index-column-list ) foreign-key-def }
+                      {line DROP
+                          {or
+                              {line {opt COLUMN} column-name }
+                                   {line DROP INDEX index-name }
+                              {line PRIMARY KEY }
+                              {line FOREIGN KEY constraint-name }
+                          }
                       }
-                      {line DROP FOREIGN KEY constraint-name }
                   }
                   { , }
               }
