8c48f7ce3b56d1dbd35390d4923058738eca3183
Joe Mistachkin <joe@mistachkin.com>
2019-01-09 11:48:32 -0500

    Flag the new tunable as 'experimental' and 'internal'.

--- db/db_tunables.h
+++ db/db_tunables.h
@@ -39,8 +39,8 @@ REGISTER_TUNABLE("allow_lua_dynamic_libs",
                  NULL, NULL, NULL, NULL);
 REGISTER_TUNABLE("allow_pragma",
                  "Enable to allow use of the PRAGMA command (Default: off)",
-                 TUNABLE_BOOLEAN, &gbl_allow_pragma, NOARG,
-                 NULL, NULL, NULL, NULL);
+                 TUNABLE_BOOLEAN, &gbl_allow_pragma,
+                 NOARG | EXPERIMENTAL | INTERNAL, NULL, NULL, NULL, NULL);
 REGISTER_TUNABLE("allow_negative_column_size",
                  "Allow negative column size in csc2 schema. Added mostly for "
                  "backwards compatibility. (Default: off)",
