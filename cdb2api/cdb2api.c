8cabcfae4ef98e2c90b7c7a18a2fb6c81eee287d
Adi Zaimi <azaimi@bloomberg.net>
2019-01-22 14:21:34 -0500

    check should be for try_node rather than i-th node

--- cdb2api/cdb2api.c
+++ cdb2api/cdb2api.c
@@ -2204,7 +2204,8 @@ static inline int cdb2_try_on_same_room(cdb2_hndl_tp *hndl)
     for (int i = 0; i < hndl->num_hosts_sameroom; i++) {
         int try_node = (hndl->node_seq + i) % hndl->num_hosts_sameroom;
         if (try_node == hndl->master || hndl->ports[try_node] <= 0 ||
-            try_node == hndl->connected_host || hndl->hosts_connected[i] == 1)
+            try_node == hndl->connected_host || 
+            hndl->hosts_connected[try_node] == 1)
             continue;
         if (newsql_connect(hndl, try_node, 0, 100) == 0)
             return 0;
