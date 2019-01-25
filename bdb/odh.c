b71f1b9d0b1f5eeae728fcbd5659f6b5922095d9
Rivers Zhang <hzhang320@bloomberg.net>
2019-01-25 16:45:59 +0000

    Fix merge error.

--- bdb/odh.c
+++ bdb/odh.c
@@ -1365,24 +1365,6 @@ inline void bdb_cleanup_fld_hints(bdb_state_type *bdb_state)
     }
 }
 
-inline void bdb_set_logical_live_sc(bdb_state_type *bdb_state)
-{
-    if (bdb_state == NULL) {
-        logmsg(LOGMSG_ERROR, "%s(NULL)!!\n", __func__);
-        return;
-    }
-    bdb_state->logical_live_sc = 1;
-}
-
-inline void bdb_clear_logical_live_sc(bdb_state_type *bdb_state)
-{
-    if (bdb_state == NULL) {
-        logmsg(LOGMSG_ERROR, "%s(NULL)!!\n", __func__);
-        return;
-    }
-    bdb_state->logical_live_sc = 0;
-}
-
 int bdb_unpack_heap(bdb_state_type *bdb_state, void *in, size_t inlen,
                     void **out, size_t *outlen, void **freeptr)
 {
