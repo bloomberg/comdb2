5ce6fadeaac6dba8225a5a3da73db9d570ee7e9e
Dorin Hogea <dhogea@bloomberg.net>
2019-01-29 09:46:12 -0500

    put back fingerprinting

--- db/comdb2.c
+++ db/comdb2.c
@@ -709,7 +709,7 @@ int gbl_keycompr = 1;
 int gbl_memstat_freq = 60 * 5;
 int gbl_accept_on_child_nets = 0;
 int gbl_disable_etc_services_lookup = 0;
-int gbl_fingerprint_queries = 0;
+int gbl_fingerprint_queries = 1;
 int gbl_stable_rootpages_test = 0;
 
 /* Only allows the ability to enable: must be enabled on a session via 'set' */
