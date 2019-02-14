bdf820f79e099acf1742f4d77abe5b7dbcc65416
Mark Hannum <mhannum72@gmail.com>
2019-02-13 21:12:39 -0500

    Pg free recovery (#1348)
    * Add more information for LONG rep_process_message
    
    * Disable online recovery for this test
    
    * Remove instrumentation
    
    * Default this to off
    
    * Fix truncatesc testcase for non-online truncates
    
    * Default the truncatesc test to use online recovery
    
    * clang
    
    * fix bad merge
    
    * Default on, disable in legacy defaults
    
    * revert physrep changes (they aren't related to pg_free_recovery bug)
    
    * fix bad rebase
    
    * code review changes

--- bdb/bdb_api.h
+++ bdb/bdb_api.h
@@ -2042,7 +2042,7 @@ void bdb_get_txn_stats(bdb_state_type *bdb_state, int64_t *active,
 
 uint32_t bdb_get_rep_gen(bdb_state_type *bdb_state);
 
-void send_newmaster(bdb_state_type *bdb_state);
+void send_newmaster(bdb_state_type *bdb_state, int online);
 
 typedef struct bias_info bias_info;
 typedef int (*bias_cmp_t)(bias_info *, void *found);
