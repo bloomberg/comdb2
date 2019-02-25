7e1b9d23ac4037e2cfba3706aedf638732c3ee1e
Rivers Zhang <hzhang320@bloomberg.net>
2019-02-25 21:00:54 +0000

    Rebuild our way out of the 0-len blob corruption
    The bug was already fixed in 3d1ed279.

--- db/toblob.c
+++ db/toblob.c
@@ -989,6 +989,15 @@ static int check_one_blob(struct ireq *iq, int isondisk, const char *tag,
             b->bloboffs[cblob] = 0;
             if (repair_mode)
                 outrc = -2;
+        } else if (blob->notnull && blob->length == 0 && b->bloblens[cblob] == 1) {
+            /* There was a bug where a 0-len blob, after compressed using LZ4,
+               would become 1 byte. Repair this too. */
+            free(b->blobptrs[cblob]);
+            b->blobptrs[cblob] = NULL;
+            b->bloblens[cblob] = 0;
+            b->bloboffs[cblob] = 0;
+            if(repair_mode)
+                outrc = -2;
         } else
             return -1;
     }
