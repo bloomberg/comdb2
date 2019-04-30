bfbb2999aa64484171a06ff086af0567e20e6e77
Joe Mistachkin <joe@mistachkin.com>
2019-04-30 15:25:37 -0400

    Fix.

--- bdb/temptable.c
+++ bdb/temptable.c
@@ -486,7 +486,9 @@ done:
 
 void bdb_temp_table_clear_pool(bdb_state_type *bdb_state)
 {
-    comdb2_objpool_clear(bdb_state->temp_table_pool);
+    comdb2_objpool_t op = bdb_state->temp_table_pool;
+    if (op == NULL) return;
+    comdb2_objpool_clear(op);
 }
 
 int bdb_temp_table_create_pool_wrapper(void **tblp, void *bdb_state_arg)
