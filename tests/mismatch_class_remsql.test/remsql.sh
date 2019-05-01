fa325f8044f322b28bb54ab566869ff467dcf687
Dorin Hogea <dhogea@bloomberg.net>
2019-05-01 14:27:40 -0400

    fix also mismatch_class_remsql after fixing testing setup phase

--- tests/mismatch_class_remsql.test/remsql.sh
+++ tests/mismatch_class_remsql.test/remsql.sh
@@ -24,22 +24,24 @@ fi
 cmd_rmt="cdb2sql -s --cdb2cfg ${a_remcdb2config} $a_remdbname --host $mach" 
 cmd="cdb2sql -s --cdb2cfg ${a_cdb2config} $a_dbname --host $mach" 
 
+$cmd_rmt "create table trem (id int, b1 blob)"
+
 echo "Inserting rows"
 $cmd_rmt < inserts.req > ${output} 2>&1
 
 #populate remote schema in the local db
 echo "Select from remote db"
-$cmd "select * from LOCAL_${a_remdbname}.t" >> ${output} 2>&1
+$cmd "select * from LOCAL_${a_remdbname}.trem" >> ${output} 2>&1
 
 # we expect errors here
 set +e
 echo "Trying to skip LOCAL override"
 # this should fail for missing LOCAL
-$cmd "select * from ${a_remdbname}.t" >> ${output} 2>&1
+$cmd "select * from ${a_remdbname}.trem" >> ${output} 2>&1
 
 # this should fail for missing dbname 
 echo "Trying to skip dbname"
-$cmd "select * from t" >> ${output} 2>&1
+$cmd "select * from trem" >> ${output} 2>&1
 
 set -e
 # proper access, test old db with new table
@@ -48,7 +50,7 @@ $cmd "select * from LOCAL_${a_remdbname}.t2" >> ${output} 2>&1
 
 # proper access, test old db and old table
 echo "Accessing again same db, and same old table"
-$cmd "select * from LOCAL_${a_remdbname}.t" >> ${output} 2>&1
+$cmd "select * from LOCAL_${a_remdbname}.trem" >> ${output} 2>&1
 
 
 # get testcase output
