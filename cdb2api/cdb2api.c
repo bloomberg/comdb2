7dc99c7e2b94ec407b30e2fe742893440cd134f4
Mohit Khullar <mohit.khullar@gmail.com>
2019-06-04 21:09:46 -0400

    clang format

--- cdb2api/cdb2api.c
+++ cdb2api/cdb2api.c
@@ -1422,7 +1422,7 @@ static int get_config_file(const char *dbname, char *f, size_t s)
 static void set_cdb2_timeouts(cdb2_hndl_tp *hndl)
 {
     if (!hndl)
-         return;
+        return;
     if (!hndl->api_call_timeout)
         hndl->api_call_timeout = CDB2_API_CALL_TIMEOUT;
     if (!hndl->connect_timeout)
