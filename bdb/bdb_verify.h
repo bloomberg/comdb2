f2a7c3deae09a4042b00a0e51bf75fbe0bb85cd0
Adi Zaimi <azaimi@bloomberg.net>
2019-06-29 01:15:42 -0400

    add new header file

---
+++ bdb/bdb_verify.h
@@ -0,0 +1,50 @@
+/*
+   Copyright 2015 Bloomberg Finance L.P.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+#ifndef __bdb_verify_h
+#define __bdb_verify_h
+
+struct SBUF2;
+struct bdb_state_type;
+
+typedef struct {
+    SBUF2 *sb;
+    bdb_state_type *bdb_state;
+    void *db_table;
+    int (*formkey_callback)(void *parm, void *dta, void *blob_parm, int ix,
+                            void *keyout, int *keysz);
+    int (*get_blob_sizes_callback)(void *parm, void *dta, int blobs[16],
+                                   int bloboffs[16], int *nblobs);
+    int (*vtag_callback)(void *parm, void *dta, int *dtasz, uint8_t ver);
+    int (*add_blob_buffer_callback)(void *parm, void *dta, int dtasz,
+                                    int blobno);
+    void (*free_blob_buffer_callback)(void *parm);
+    unsigned long long (*verify_indexes_callback)(void *parm, void *dta,
+                                                  void *blob_parm);
+    void *callback_parm;
+    int (*lua_callback)(void *, const char *);
+    void *lua_params;
+    void *callback_blob_buf;
+    int progress_report_seconds;
+    int attempt_fix;
+} verify_td_params;
+
+
+
+int bdb_verify(verify_td_params *par);
+
+
+#endif
