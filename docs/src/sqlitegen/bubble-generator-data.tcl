05ce39f39c4276d982b3963a61edc925b22500d8
Michael Ponomarenko <mponomarenko@bloomberg.net>
2019-07-03 23:23:05 -0400

    Start SQL + schema change.

--- docs/src/sqlitegen/bubble-generator-data.tcl
+++ docs/src/sqlitegen/bubble-generator-data.tcl
@@ -356,6 +356,7 @@ set all_graphs {
       {line REBUILD}
       {line REC {or CRLE LZ4 RLE ZLIB}}
       {line BLOBFIELD {or LZ4 RLE ZLIB}}
+      {line DTASTRIPE number}
     } ,} 
   }
 
--- schemachange/schemachange.h
+++ schemachange/schemachange.h
@@ -137,6 +137,8 @@ struct schema_change_type {
     int is_sfunc; /* lua scalar func */
     int is_afunc; /* lua agg func */
 
+    int new_table_dtastripe;
+
     /* ========== runtime members ========== */
     int onstack; /* if 1 don't free */
     int nothrevent;
