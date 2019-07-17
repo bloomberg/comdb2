4f84d1d314959cc2e44d787fad274f97a6275088
Mark Hannum <mhannum72@gmail.com>
2019-07-17 09:24:40 -0400

    Instrument to find verify bug

--- db/osql_srs.c
+++ db/osql_srs.c
@@ -267,8 +267,11 @@ int srs_tran_replay(struct sqlclntstate *clnt, struct thr_handle *thr_self)
             osql_shadtbl_close(clnt); 
         }
 
-        if (clnt->verify_retries == gbl_osql_verify_retries_max + 1)
+        if (clnt->verify_retries == gbl_osql_verify_retries_max + 1) {
+            logmsg(LOGMSG_DEBUG, "%s line %d verify error after %d retries\n",
+                    __func__, __LINE__, clnt->verify_retries);
             osql_set_replay(__FILE__, __LINE__, clnt, OSQL_RETRY_LAST);
+        }
 
         if (0 /*!bdb_am_i_coherent(thedb->bdb_env)*/) {
             fprintf(stderr, "Cannot replay, I am incoherent id=%d retries=%d\n",
