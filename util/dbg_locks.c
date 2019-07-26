098f43d345dc9ab38ce701951b9418700c8486c2
Joe Mistachkin <joe@mistachkin.com>
2019-07-26 17:42:01 -0400

    Stop using mem.h functionality as it cannot be safely used here.  Update tests.

--- util/dbg_locks.c
+++ util/dbg_locks.c
@@ -24,12 +24,6 @@
 #include "plhash.h"
 #include "pthread_wrap_core.h"
 #include "comdb2_atomic.h"
-#include "mem.h"
-
-#ifndef BUILDING_TOOLS
-#include <mem_util.h>
-#include <mem_override.h>
-#endif
 
 enum dbg_lock_pthread_type_t {
   DBG_LOCK_PTHREAD_TYPE_NONE = 0x0,
