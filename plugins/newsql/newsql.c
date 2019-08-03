47a94b5238c94b406503749e007876ab5d6451dc
Nirbhay Choubey <nchoubey@bloomberg.net>
2019-08-03 17:38:22 -0400

    Fix an issue causing a few tests to timeout

--- plugins/newsql/newsql.c
+++ plugins/newsql/newsql.c
@@ -2279,6 +2279,7 @@ static int handle_newsql_request(comdb2_appsock_arg_t *arg)
         if (!clnt.in_client_trans) {
             bzero(&clnt.effects, sizeof(clnt.effects));
             bzero(&clnt.log_effects, sizeof(clnt.log_effects));
+            clnt.had_errors = 0;
         }
         if (clnt.dbtran.mode < TRANLEVEL_SOSQL) {
             clnt.dbtran.mode = TRANLEVEL_SOSQL;
