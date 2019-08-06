ca4221255b12e96137ad8422cac0bd485a1f8cc4
Joe Mistachkin <joe@mistachkin.com>
2019-08-06 14:52:23 -0400

    More testcase cleanup.

--- tests/fingerprints.test/fp.sh
+++ tests/fingerprints.test/fp.sh
@@ -1,67 +1,25 @@
 #!/usr/bin/env bash
 
 cdb2sql $SP_OPTIONS - <<'EOF'
-create procedure test_fp version '1' {
-local function main()
+CREATE PROCEDURE test_fp VERSION 1 {
+local function exec_sql_and_emit(sql)
+  local rc
+  local n, q
+  q, rc = db:exec(sql)
+  if (rc == 0) then
+    n = q:fetch()
+    while n do
+      db:emit(n)
+      n = q:fetch()
+    end
+  else
+    db:emit(db:sqlerror())
+  end
+end
+local function prepare_sql_and_emit(sql)
   local rc
   local n, q, r
-
-  q, rc = db:exec("SELECT 1")
-  if (rc == 0) then
-    n = q:fetch()
-    while n do
-      db:emit(n)
-      n = q:fetch()
-    end
-  else
-    db:emit(db:sqlerror())
-  end
-
-  q, rc = db:exec("INSERT INTO fp1(x) VALUES(0)")
-  if (rc == 0) then
-    n = q:fetch()
-    while n do
-      db:emit(n)
-      n = q:fetch()
-    end
-  else
-    db:emit(db:sqlerror())
-  end
-
-  q, rc = db:exec("SELECT x FROM fp1 ORDER BY x")
-  if (rc == 0) then
-    n = q:fetch()
-    while n do
-      db:emit(n)
-      n = q:fetch()
-    end
-  else
-    db:emit(db:sqlerror())
-  end
-
-  q, rc = db:exec("DELETE FROM fp1 WHERE x = 0")
-  if (rc == 0) then
-    n = q:fetch()
-    while n do
-      db:emit(n)
-      n = q:fetch()
-    end
-  else
-    db:emit(db:sqlerror())
-  end
-
-  q, rc = db:exec("SELECT * FROM fp1 ORDER BY x")
-  if (rc == 0) then
-    n = q:fetch()
-    while n do
-      db:emit(n)
-      n = q:fetch()
-    end
-  else
-    db:emit(db:sqlerror())
-  end
-
-  q, rc = db:prepare("SELECT * FROM fp1 ORDER BY x")
+  q, rc = db:prepare(sql)
   if (rc == 0) then
     r, rc = db:exec(q)
     if (rc == 0) then
@@ -76,6 +34,14 @@ local function main()
   else
     db:emit(db:sqlerror())
   end
+end
+local function main()
+  exec_sql_and_emit("SELECT 1")
+  exec_sql_and_emit("INSERT INTO fp1(x) VALUES(0)")
+  exec_sql_and_emit("SELECT x FROM fp1 ORDER BY x")
+  exec_sql_and_emit("DELETE FROM fp1 WHERE x = 0")
+  exec_sql_and_emit("SELECT * FROM fp1 ORDER BY x")
+  prepare_sql_and_emit("SELECT * FROM fp1 ORDER BY x")
 end}$$
 EOF
 
