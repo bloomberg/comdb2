3946b225b78858ab2a250cfd10a78509c86118ab
Joe Mistachkin <joe@mistachkin.com>
2019-08-06 14:31:05 -0400

    Improve SP query handling in test.

--- tests/fingerprints.test/fp.sh
+++ tests/fingerprints.test/fp.sh
@@ -3,29 +3,85 @@
 cdb2sql $SP_OPTIONS - <<'EOF'
 create procedure test_fp version '1' {
 local function main()
-  db:exec("SELECT 1"):emit()
-  db:exec("INSERT INTO fp1(x) VALUES(0)"):emit()
-  db:exec("SELECT x FROM fp1"):emit()
-  db:exec("DELETE FROM fp1 WHERE x = 0"):emit()
-
   local rc
-  local n, q, r
+  local n, q
+
+  q, rc = db:exec("SELECT 1")
+  if (rc == 0) then
+    n = q:fetch()
+    while n do
+      db:emit(n)
+      n = q:fetch()
+    end
+  else
+    db:emit(db:sqlerror())
+  end
+
+  q, rc = db:exec("INSERT INTO fp1(x) VALUES(0)")
+  if (rc == 0) then
+    n = q:fetch()
+    while n do
+      db:emit(n)
+      n = q:fetch()
+    end
+  else
+    db:emit(db:sqlerror())
+  end
+
+  q, rc = db:exec("SELECT x FROM fp1 ORDER BY x")
+  if (rc == 0) then
+    n = q:fetch()
+    while n do
+      db:emit(n)
+      n = q:fetch()
+    end
+  else
+    db:emit(db:sqlerror())
+  end
+
+  q, rc = db:exec("DELETE FROM fp1 WHERE x = 0")
+  if (rc == 0) then
+    n = q:fetch()
+    while n do
+      db:emit(n)
+      n = q:fetch()
+    end
+  else
+    db:emit(db:sqlerror())
+  end
 
   q, rc = db:exec("SELECT * FROM fp1 ORDER BY x")
   if (rc == 0) then
     n = q:fetch()
-    db:emit(n)
+    while n do
+      db:emit(n)
+      n = q:fetch()
+    end
   else
     db:emit(db:sqlerror())
-  end   
+  end
 
-  r, rc = db:prepare("SELECT * FROM fp1 ORDER BY x")
   if (rc == 0) then
-    r:emit()
+    n = q:fetch()
+    while n do
+      db:emit(n)
+      n = q:fetch()
+    end
   else
     db:emit(db:sqlerror())
-  end   
+  end
+
+  q, rc = db:prepare("SELECT * FROM fp1 ORDER BY x")
+  if (rc == 0) then
+    n = q:fetch()
+    while n do
+      db:emit(n)
+      n = q:fetch()
+    end
+  else
+    db:emit(db:sqlerror())
+  end
 end}$$
 EOF
 
-cdb2sql $SP_OPTIONS "exec procedure test_fp()"
+cdb2sql $SP_OPTIONS "EXEC PROCEDURE test_fp()"
