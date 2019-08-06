5bc31c8279c48540a7aabe20b29dcf268b5e9f6e
Joe Mistachkin <joe@mistachkin.com>
2019-08-06 10:43:26 -0400

    Work on tests.

---
+++ tests/fingerprints.test/fp.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+cdb2sql $SP_OPTIONS - <<'EOF'
+create procedure test_fp version '1' {
+local function main()
+  db:exec("SELECT 1"):emit()
+  db:exec("INSERT INTO fp1(x) VALUES(0)"):emit()
+  db:exec("SELECT x FROM fp1"):emit()
+  db:exec("DELETE FROM fp1 WHERE x = 0"):emit()
+
+  local q, rc1 = db:exec(t)
+  local n = q:fetch()
+  db:emit(n)
+
+  local r, rc2 = db:prepare("SELECT * FROM fp1 ORDER BY x")
+  r:emit()
+end}$$
+EOF
+
+cdb2sql $SP_OPTIONS "exec procedure test_fp()"
