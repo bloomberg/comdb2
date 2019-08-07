70ff013c8ee38a22e798c4871596db78f127b188
mistachkin <joe@mistachkin.com>
2019-08-07 12:35:49 -0400

    Adjustments to the test case.

--- tests/fingerprints.test/fp.sh
+++ tests/fingerprints.test/fp.sh
@@ -17,11 +17,11 @@ local function exec_sql_and_emit(sql)
   end
 end
 local function main()
-  exec_sql_and_emit("SELECT 1 AS xyz")
-  exec_sql_and_emit("INSERT INTO fp1(x) VALUES(0)")
-  exec_sql_and_emit("SELECT x AS w FROM fp1 ORDER BY x")
-  exec_sql_and_emit("SELECT x + x AS y FROM fp1 ORDER BY x")
-  exec_sql_and_emit("SELECT SUM(x) AS z FROM fp1")
+  exec_sql_and_emit("SELECT 1 AS xyz") -- 1 row
+  exec_sql_and_emit("INSERT INTO fp1(x) VALUES(0)") -- 1 row
+  exec_sql_and_emit("SELECT x AS w FROM fp1 ORDER BY x") -- 2 rows
+  exec_sql_and_emit("SELECT x + x AS y FROM fp1 ORDER BY x") -- 2 rows
+  exec_sql_and_emit("SELECT SUM(x) AS z FROM fp1") -- 1 row
 end}$$
 EOF
 
