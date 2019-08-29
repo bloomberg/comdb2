a877325f94e5cc3b8800df9b2b32c570acb50d7a
Joe Mistachkin <joe@mistachkin.com>
2019-08-29 15:58:03 -0400

    Initial draft of tests... needs work.

---
+++ tests/ruleset.test/ruleset.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('free_ruleset')"
+cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('free_ruleset')"
+
+cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('dump_ruleset')"
+
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT * FROM t1;"
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT x FROM t1;"
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT x FROM t1 ORDER BY x;"
+
+cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('load_ruleset t1.ruleset')"
+
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT * FROM t1;"
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT x FROM t1;"
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT x FROM t1 ORDER BY x;"
+
+cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('save_ruleset t1_saved.ruleset')"
+
+if ! diff t1.ruleset t1_saved.ruleset ; then
+  echo output is different from expected
+  exit 1
+fi
+
+cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('evaluate_ruleset')"
+cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('free_ruleset')"
+
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT * FROM t1;"
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT x FROM t1;"
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT x FROM t1 ORDER BY x;"
+
+cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('evaluate_ruleset')"
+
+# debug.random_sql_work_delayed
+# debug.random_sql_work_rejected
---
+++ tests/ruleset.test/t02.req.tool
@@ -0,0 +1,1 @@
+./ruleset.sh
