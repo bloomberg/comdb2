3c03c82cd69c05dbb88dbd0d41dbb332c6bf2b43
Rivers Zhang <hzhang320@bloomberg.net>
2019-08-30 02:51:59 -0400

    Fix a race between time partition rollout and regular schema change
    Time partition rollout locks `views_lk` first and `csc2_subsystem_mtx` (in `csc2_free_all()`) second.
    A regular schema change locks `csc2_subsystem_mtx` first and `views_lk` (in `timepart_is_shard()`) second.
    
    To fix this, in time partition rollout, we invoke `csc2_free_all()` after `views_lk` is released.
    
    (DRQS 147345691)

--- schemachange/sc_logic.c
+++ schemachange/sc_logic.c
@@ -198,9 +198,14 @@ static void free_sc(struct schema_change_type *s)
 {
     free_schema_change_type(s);
     /* free any memory csc2 allocated when parsing schema */
-    Pthread_mutex_lock(&csc2_subsystem_mtx);
+
+    /* Bail out if we're in a time partition rollout otherwise
+       we may deadlock with a regular schema change. The time partition
+       rollout will invoke this function again without holding views_lk. */
+    if (s->views_locked)
+        return;
+
     csc2_free_all();
-    Pthread_mutex_unlock(&csc2_subsystem_mtx);
 }
 
 static void stop_and_free_sc(int rc, struct schema_change_type *s, int do_free)
@@ -371,7 +376,9 @@ static int do_ddl(ddl_t pre, ddl_t post, struct ireq *iq,
         goto end;
     broadcast_sc_start(s->tablename, iq->sc_seed, iq->sc_host,
                        time(NULL));                   // dont care rcode
+
     rc = pre(iq, s, NULL);                            // non-tran ??
+
     if (type == alter && master_downgrading(s)) {
         s->sc_rc = SC_MASTER_DOWNGRADE;
         errstat_set_strf(
