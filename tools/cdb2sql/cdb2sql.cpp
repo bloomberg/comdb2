cd5164cf37d5d93dc5e3ad18e6a1f5667d719b00
levinem <mlevine55@bloomberg.net>
2019-08-30 22:40:45 -0400

    cdb2sql: Replaced while loops for level 1 and charatglyph words usage with range based for loops. Removed NULL from the arrays. (#1777)

--- tools/cdb2sql/cdb2sql.cpp
+++ tools/cdb2sql/cdb2sql.cpp
@@ -174,38 +174,23 @@ void cdb2sql_usage(const int exit_val)
 }
 
 const char *level_one_words[] = {
-  "@",
-  "ALTER", "ANALYZE",
-  "BEGIN",
-  "COMMIT",
-  "CREATE",
-  "DELETE", "DROP", "DRYRUN",
-  "EXEC", "EXPLAIN",
-  "INSERT",
-  "PUT",
-  "REBUILD",
-  "ROLLBACK",
-  "SELECT", "SELECTV", "SET",
-  "TRUNCATE",
-  "UPDATE",
-  "WITH", NULL,  // must be terminated by NULL
+    "@",        "ALTER",  "ANALYZE", "BEGIN",   "COMMIT",   "CREATE", "DELETE",
+    "DROP",     "DRYRUN", "EXEC",    "EXPLAIN", "INSERT",   "PUT",    "REBUILD",
+    "ROLLBACK", "SELECT", "SELECTV", "SET",     "TRUNCATE", "UPDATE", "WITH",
 };
 
 const char *char_atglyph_words[] = {
-    "cdb2_close", "desc",     "hexblobs", "ls", "redirect", "row_sleep",
-    "send",       "strblobs", "time",     NULL, // must be terminated by NULL
+    "cdb2_close", "desc", "hexblobs", "ls",   "redirect",
+    "row_sleep",  "send", "strblobs", "time",
 };
 
 static char *char_atglyph_generator(const char *text, const int state)
 {
-    static int list_index, len;
-    const char *name;
+    static int len;
     if (!state) { // if state is 0 get the length of text
-        list_index = 0;
         len = strlen(text);
     }
-    while ((name = char_atglyph_words[list_index]) != NULL) {
-        list_index++;
+    for (const auto &name : char_atglyph_words) {
         if (len == 0 || strncasecmp(name, text, len) == 0) {
             return strdup(name);
         }
@@ -216,16 +201,13 @@ static char *char_atglyph_generator(const char *text, const int state)
 // Generator function for word completion.
 static char *level_one_generator(const char *text, const int state)
 {
-    static int list_index, len;
-    const char *name;
+    static int len;
     if (!state) { //if state is 0 get the length of text
-        list_index = 0;
         len = strlen (text);
     }
-    while ((name = level_one_words[list_index]) != NULL) {
-        list_index++;
-        if (len == 0 || strncasecmp (name, text, len) == 0) {
-            return strdup (name);
+    for (const auto &name : level_one_words) {
+        if (len == 0 || strncasecmp(name, text, len) == 0) {
+            return strdup(name);
         }
     }
     return (NULL); // If no names matched, then return NULL.
