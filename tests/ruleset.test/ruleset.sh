1da884691fd23ff4b1f64c47e95d453e85c3f0e1
mistachkin <joe@mistachkin.com>
2019-08-30 20:23:02 -0400

    tweak.

--- tests/ruleset.test/ruleset.sh
+++ tests/ruleset.test/ruleset.sh
@@ -18,14 +18,14 @@ cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT * FROM t1;"
 cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT comdb2_ctxinfo('retries'), x FROM t1;"
 cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT x FROM t1 ORDER BY x;"
 
-cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('save_ruleset $DBDIR/t01_saved.ruleset')" | sed 's/file ".*"/file "t01_saved.ruleset"/g' | sed 's/ruleset 0x[0-9A-Fa-f]+/0x00000000/g'
+cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('save_ruleset $DBDIR/t01_saved.ruleset')" | sed 's/file ".*"/file "t01_saved.ruleset"/g'
 
 if ! diff $DBDIR/t01.ruleset $DBDIR/t01_saved.ruleset ; then
   echo output is different from expected
   exit 1
 fi
 
-cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('evaluate_ruleset')"
+cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('evaluate_ruleset')" | sed 's/ruleset 0x[0-9A-Fa-f]+/ruleset 0x00000000/g'
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('free_ruleset')"
 
 cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT 3;"
