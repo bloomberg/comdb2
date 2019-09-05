e1c1b98e26a86502ad1f3641ff7ab3203a6d5330
Adi Zaimi <azaimi@bloomberg.net>
2019-09-05 15:39:29 -0400

    reorder struct reqlogger members

--- db/reqlog_int.h
+++ db/reqlog_int.h
@@ -76,8 +76,6 @@ struct reqlogger {
     unsigned dump_mask;
     unsigned mask; /* bitwise or of the above two masks */
 
-    uint64_t startus;
-
     struct prefix_type prefix;
     char dumpline[1024];
     int dumplinepos;
@@ -105,10 +103,11 @@ struct reqlogger {
     int sqlrows;
     double sqlcost;
 
-    int rc;
+    uint64_t startus;
     uint64_t durationus;
-    int vreplays;
     uint64_t queuetimeus;
+    int rc;
+    int vreplays;
     char fingerprint[FINGERPRINTSZ];
     int have_fingerprint;
     char id[41];
