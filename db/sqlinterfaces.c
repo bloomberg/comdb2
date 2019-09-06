cd3383d0becbcb00578db75ead34844d18f624f7
mistachkin <joe@mistachkin.com>
2019-09-05 22:05:31 -0400

    Fix typo.

--- db/sqlinterfaces.c
+++ db/sqlinterfaces.c
@@ -4904,7 +4904,7 @@ void sqlengine_work_appsock(void *thddata, void *work)
 
     if (gbl_prioritize_queries) {
         if ((gbl_prioritize_max_retries <= 0) ||
-            (clnt->retries < gbl_prioritize_max_retries)) {
+            (clnt->work.retries < gbl_prioritize_max_retries)) {
             if (gbl_fingerprint_queries) {
                 /* IGNORED */
                 preview_and_calc_fingerprint(clnt);
