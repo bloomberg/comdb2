6c71a27dc716608d993e2c019b75a6ca22c4d0af
mistachkin <joe@mistachkin.com>
2019-09-05 23:41:23 -0400

    More work on test.

--- tests/ruleset.test/ruleset.sh
+++ tests/ruleset.test/ruleset.sh
@@ -4,19 +4,19 @@ cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('free_ruleset')
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('free_ruleset')"
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('dump_ruleset')"
 
-cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT 1;"
-cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT * FROM t1;"
-cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT comdb2_ctxinfo('retries'), x FROM t1;"
-cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT x FROM t1 ORDER BY x;"
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT 1;" 2>&1
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT * FROM t1;" 2>&1
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT comdb2_ctxinfo('retries'), x FROM t1;" 2>&1
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT x FROM t1 ORDER BY x;" 2>&1
 
 cdb2sql --host $SP_HOST $SP_OPTIONS "PUT TUNABLE 'debug.thdpool_queue_only' 1"
 cdb2sql --host $SP_HOST $SP_OPTIONS "PUT TUNABLE 'debug.force_thdpool_priority' 1"
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('reload_ruleset $DBDIR/t01.ruleset')" | sed 's/file ".*"/file "t01.ruleset"/g'
 
-cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT 2;"
-cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT * FROM t1;"
-cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT comdb2_ctxinfo('retries'), x FROM t1;"
-cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT x FROM t1 ORDER BY x;"
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT 2;" 2>&1
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT * FROM t1;" 2>&1
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT comdb2_ctxinfo('retries'), x FROM t1;" 2>&1
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT x FROM t1 ORDER BY x;" 2>&1
 
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('save_ruleset $DBDIR/t01_saved.ruleset')" | sed 's/file ".*"/file "t01_saved.ruleset"/g'
 
@@ -28,9 +28,9 @@ fi
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('evaluate_ruleset')" | sed 's/ruleset 0x[0-9A-Fa-f]\+/ruleset 0x00000000/g'
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('free_ruleset')"
 
-cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT 3;"
-cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT * FROM t1;"
-cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT comdb2_ctxinfo('retries'), x FROM t1;"
-cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT x FROM t1 ORDER BY x;"
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT 3;" 2>&1
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT * FROM t1;" 2>&1
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT comdb2_ctxinfo('retries'), x FROM t1;" 2>&1
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT x FROM t1 ORDER BY x;" 2>&1
 
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('evaluate_ruleset')"
