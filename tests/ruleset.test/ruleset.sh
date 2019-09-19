04216d3d6ef6006f275dd043d238ec7dab065aa2
Joe Mistachkin <joe@mistachkin.com>
2019-09-19 12:21:09 -0400

    Work on no_fp test variant.

--- tests/ruleset.test/ruleset.sh
+++ tests/ruleset.test/ruleset.sh
@@ -20,22 +20,24 @@ cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT comdb2_ctxinfo('priority'), x FROM t
 cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT x FROM t1 ORDER BY x;" 2>&1
 cdb2sql --host $SP_HOST $SP_OPTIONS "CREATE TABLE txx(bad TEXT);" 2>&1
 
-cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('save_ruleset $DBDIR/rulesets/t01_saved.ruleset')" | sed 's/file ".*"/file "t01_saved.ruleset"/g'
-cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('save_ruleset $DBDIR/rulesets/t02_saved.ruleset')" | sed 's/file ".*"/file "t02_saved.ruleset"/g'
+if [[ $DBNAME != *"nofpgenerated"* ]] ; then
+  cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('save_ruleset $DBDIR/rulesets/t01_saved.ruleset')" | sed 's/file ".*"/file "t01_saved.ruleset"/g'
+  cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('save_ruleset $DBDIR/rulesets/t02_saved.ruleset')" | sed 's/file ".*"/file "t02_saved.ruleset"/g'
 
-if [ $SP_HOST != `hostname` ]; then
-  scp $SP_HOST:$DBDIR/rulesets/t01_saved.ruleset $DBDIR/rulesets/t01_saved.ruleset
-  scp $SP_HOST:$DBDIR/rulesets/t02_saved.ruleset $DBDIR/rulesets/t02_saved.ruleset
-fi
+  if [ $SP_HOST != `hostname` ]; then
+    scp $SP_HOST:$DBDIR/rulesets/t01_saved.ruleset $DBDIR/rulesets/t01_saved.ruleset
+    scp $SP_HOST:$DBDIR/rulesets/t02_saved.ruleset $DBDIR/rulesets/t02_saved.ruleset
+  fi
 
-if ! diff $DBDIR/rulesets/t01.ruleset $DBDIR/rulesets/t01_saved.ruleset ; then
-  echo output for t01_saved.ruleset is different from expected
-  exit 1
-fi
+  if ! diff $DBDIR/rulesets/t01.ruleset $DBDIR/rulesets/t01_saved.ruleset ; then
+    echo output for t01_saved.ruleset is different from expected
+    exit 1
+  fi
 
-if ! diff $DBDIR/rulesets/t01.ruleset $DBDIR/rulesets/t02_saved.ruleset ; then
-  echo output for t02_saved.ruleset is different from expected
-  exit 1
+  if ! diff $DBDIR/rulesets/t01.ruleset $DBDIR/rulesets/t02_saved.ruleset ; then
+    echo output for t02_saved.ruleset is different from expected
+    exit 1
+  fi
 fi
 
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('evaluate_ruleset')" | sed 's/ruleset 0x[0-9A-Fa-f]\+/ruleset 0x00000000/g'
