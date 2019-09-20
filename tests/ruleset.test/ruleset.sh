cc305e2ae9585825ca104518ad70d7fbe511f4a7
mistachkin <joe@mistachkin.com>
2019-09-20 16:26:32 -0400

    Test enhancements.

--- tests/ruleset.test/ruleset.sh
+++ tests/ruleset.test/ruleset.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT 'phase 0' AS z;" 2>&1
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('free_ruleset')"
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('free_ruleset')"
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('dump_ruleset')"
 
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT 'phase 1' AS z;" 2>&1
 cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT 1;" 2>&1
 cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT * FROM t1 ORDER BY x;" 2>&1
 cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT comdb2_ctxinfo('priority'), x FROM t1 ORDER BY x;" 2>&1 | sed 's/=[0-9][0-9]/=xx/g'
 cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT x FROM t1 ORDER BY x;" 2>&1
 
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT 'phase 2' AS z;" 2>&1
 cdb2sql --host $SP_HOST $SP_OPTIONS "PUT TUNABLE 'debug.thdpool_queue_only' 1"
 cdb2sql --host $SP_HOST $SP_OPTIONS "PUT TUNABLE 'debug.force_thdpool_priority' 1"
 
@@ -20,6 +23,7 @@ fi
 
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('dump_ruleset')" | sed 's/ruleset 0x[0-9A-Fa-f]\+/ruleset 0x00000000/g'
 
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT 'phase 3' AS z;" 2>&1
 cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT 2;" 2>&1
 cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT * FROM t1 ORDER BY x;" 2>&1
 
@@ -31,6 +35,8 @@ fi
 
 cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT x FROM t1 ORDER BY x;" 2>&1
 cdb2sql --host $SP_HOST $SP_OPTIONS "CREATE TABLE txx(bad TEXT);" 2>&1
+
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT 'phase 4' AS z;" 2>&1
 
 if [[ $DBNAME != *"nofpgenerated"* ]] ; then
   cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('save_ruleset $DBDIR/rulesets/t01_saved.ruleset')" | sed 's/file ".*"/file "t01_saved.ruleset"/g'
@@ -53,16 +59,21 @@ if [[ $DBNAME != *"nofpgenerated"* ]] ; then
 fi
 
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('evaluate_ruleset')" | sed 's/ruleset 0x[0-9A-Fa-f]\+/ruleset 0x00000000/g'
+
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT 'phase 5' AS z;" 2>&1
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('reload_ruleset $DBDIR/rulesets/t00.ruleset')" | sed 's/file ".*"/file "t00.ruleset"/g'
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('dump_ruleset')" | sed 's/ruleset 0x[0-9A-Fa-f]\+/ruleset 0x00000000/g'
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('free_ruleset')"
 
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT 'phase 6' AS z;" 2>&1
 cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT 3;" 2>&1
 cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT * FROM t1 ORDER BY x;" 2>&1
 cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT comdb2_ctxinfo('priority'), x FROM t1 ORDER BY x;" 2>&1 | sed 's/=[0-9][0-9]/=zz/g'
 cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT x FROM t1 ORDER BY x;" 2>&1
 
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('evaluate_ruleset')"
+
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT 'phase 7' AS z;" 2>&1
 
 if [[ $DBNAME == *"nofpgenerated"* ]] ; then
   cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('reload_ruleset $DBDIR/rulesets/t01.ruleset')" 2>&1 | sed 's/\[ERROR\] .*t01.ruleset/\[ERROR\] t01.ruleset/g'
@@ -80,9 +91,12 @@ cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('evaluate_rules
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('dump_ruleset')" | sed 's/ruleset 0x[0-9A-Fa-f]\+/ruleset 0x00000000/g'
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('free_ruleset')"
 
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT 'phase 8' AS z;" 2>&1
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('reload_ruleset $DBDIR/rulesets/t03.ruleset')" | sed 's/file ".*"/file "t03.ruleset"/g'
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('dump_ruleset')" | sed 's/ruleset 0x[0-9A-Fa-f]\+/ruleset 0x00000000/g'
 cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('free_ruleset')"
+
+cdb2sql --host $SP_HOST $SP_OPTIONS "SELECT 'phase 9' AS z;" 2>&1
 
 if [[ $DBNAME == *"nofpgenerated"* ]] ; then
   cdb2sql --host $SP_HOST $SP_OPTIONS "EXEC PROCEDURE sys.cmd.send('reload_ruleset $DBDIR/rulesets/t01.ruleset')" 2>&1 | sed 's/\[ERROR\] .*t01.ruleset/\[ERROR\] t01.ruleset/g'
