3630dcecf421e6ac20b48f8c39cfa6d491bf960a
Pavlos Rontidis <prontidis@bloomberg.net>
2019-09-24 12:26:32 -0400

    Fix Apple/Darwin build

--- cmake/FindReadline.cmake
+++ cmake/FindReadline.cmake
@@ -10,13 +10,25 @@ find_library(NCURSES_LIBRARY
   NAMES ncurses
   HINTS ${NCURSES_ROOT_DIR}
 )
+
+if (NOT APPLE)
 find_library(TINFO_LIBRARY
   NAMES tinfo
   HINTS ${TINFO_ROOT_DIR}
 )
+endif()
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Readline DEFAULT_MSG READLINE_INCLUDE_DIR)
 find_package_handle_standard_args(libreadline DEFAULT_MSG READLINE_LIBRARY)
 find_package_handle_standard_args(libncurses  DEFAULT_MSG NCURSES_LIBRARY)
+
+if (NOT APPLE)
 find_package_handle_standard_args(libtinfo DEFAULT_MSG TINFO_LIBRARY)
-list(APPEND READLINE_LIBRARIES ${READLINE_LIBRARY} ${NCURSES_LIBRARY} ${TINFO_LIBRARY})
+endif()
+
+
+list(APPEND READLINE_LIBRARIES ${READLINE_LIBRARY} ${NCURSES_LIBRARY})
+if (NOT APPLE)
+list(APPEND READLINE_LIBRARIES ${TINFO_LIBRARY})
+endif()
