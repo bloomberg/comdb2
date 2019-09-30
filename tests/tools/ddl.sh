d6867460d4f60bb72255ca05470cfa2668aad1ce
Mark Hannum <mhannum72@gmail.com>
2019-09-30 14:39:25 -0400

    Sc async constraint (#1816)
    * Fix a bug in new snapshot: we previously neglected to remove a lock from the tracked-lock list on lock_put.

---
+++ tests/tools/ddl.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+. ${TESTSROOTDIR}/tools/write_prompt.sh
+
+function drop_table
+{
+    [[ $debug == "1" ]] && set -x
+    typeset func="create_table"
+    write_prompt $func "Running $func"
+    typeset table=${1:-t1}
+    $CDB2SQL_EXE -tabs $CDB2_OPTIONS $DBNAME default "drop table $table"
+}
+
+function create_table
+{
+    [[ $debug == "1" ]] && set -x
+    typeset func="create_table"
+    write_prompt $func "Running $func"
+    typeset table=${1:-t1}
+    $CDB2SQL_EXE -tabs $CDB2_OPTIONS $DBNAME default "create table $table(a int)" 
+}
+
+function create_index
+{
+    [[ $debug == "1" ]] && set -x
+    typeset func="create_index"
+    write_prompt $func "Running $func"
+    typeset table=${1:-t1}
+    typeset column=${2:-a}
+    typeset ixname=${2:-ix1}
+    $CDB2SQL_EXE -tabs $CDB2_OPTIONS $DBNAME default "create index $ixname on $table($column)" 
+}
+
+function create_unique_index
+{
+    [[ $debug == "1" ]] && set -x
+    typeset func="create_index"
+    write_prompt $func "Running $func"
+    typeset table=${1:-t1}
+    typeset column=${2:-a}
+    typeset ixname=${2:-uniqix1}
+    $CDB2SQL_EXE -tabs $CDB2_OPTIONS $DBNAME default "create unique index $ixname on $table($column)" 
+}
