559bc3bdb6d27b48267d0e9e065114dfcf74ee50
Joe Mistachkin <joe@mistachkin.com>
2019-10-01 15:51:00 -0400

    Even more surgical.

--- cdb2api/cdb2api.c
+++ cdb2api/cdb2api.c
@@ -1472,7 +1472,7 @@ static void set_cdb2_timeouts(cdb2_hndl_tp *hndl)
 static int read_available_comdb2db_configs(
     cdb2_hndl_tp *hndl, char comdb2db_hosts[][64], const char *comdb2db_name,
     int *num_hosts, int *comdb2db_num, const char *dbname, char db_hosts[][64],
-    int *num_db_hosts, int *dbnum, int noLock, int noBuffer)
+    int *num_db_hosts, int *dbnum, int noLock, int defaultOnly)
 {
     char filename[PATH_MAX];
     SBUF2 *s;
@@ -1495,13 +1495,13 @@ static int read_available_comdb2db_configs(
         *num_db_hosts = 0;
     int *send_stack = hndl ? (&hndl->send_stack) : NULL;
 
-    if (!noBuffer && CDB2DBCONFIG_BUF != NULL) {
+    if (!defaultOnly && CDB2DBCONFIG_BUF != NULL) {
         read_comdb2db_cfg(NULL, NULL, comdb2db_name, CDB2DBCONFIG_BUF,
                           comdb2db_hosts, num_hosts, comdb2db_num, dbname,
                           db_hosts, num_db_hosts, dbnum, send_stack);
         fallback_on_bb_bin = 0;
     } else {
-        if (*CDB2DBCONFIG_NOBBENV != '\0') {
+        if (!defaultOnly && *CDB2DBCONFIG_NOBBENV != '\0') {
             s = sbuf2openread(CDB2DBCONFIG_NOBBENV);
             if (s != NULL) {
                 read_comdb2db_cfg(NULL, s, comdb2db_name, NULL, comdb2db_hosts,
@@ -5443,10 +5443,10 @@ after_callback:
 }
 
 static inline void only_read_config(cdb2_hndl_tp *hndl, int noLock,
-                                    int noBuffer)
+                                    int defaultOnly)
 {
     read_available_comdb2db_configs(NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-                                    NULL, NULL, noLock, noBuffer);
+                                    NULL, NULL, noLock, defaultOnly);
     set_cdb2_timeouts(hndl);
 }
 
