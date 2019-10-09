b5732febf61d6510a8b639d10dbd4c9456c1a115
Dorin Hogea <dhogea@bloomberg.net>
2019-10-08 20:32:00 -0400

    make sure we don't overcount decimal in indexes (#1854)

--- db/tag.c
+++ db/tag.c
@@ -7400,7 +7400,8 @@ int extract_decimal_quantum(struct dbtable *db, int ix, char *inbuf, char *poutb
 
     decimals = 0;
     for (i = 0; i < s->nmembers; i++) {
-        decimals++;
+        if (s->member[i].type == SERVER_DECIMAL)
+            decimals++;
     }
 
     if (outbuf && outlen && (outbuf_max < 4 * decimals)) {
