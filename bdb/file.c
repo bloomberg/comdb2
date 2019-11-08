4cdfbdff6ce0b4d87a8da4b47d05547b7182f04b
Mark Hannum <mhannum72@gmail.com>
2019-11-07 22:22:37 -0500

    Add tid to txn object, assert that we dont hold a tran outside of the block processor

--- bdb/file.c
+++ bdb/file.c
@@ -8479,3 +8479,9 @@ int bdb_list_all_fileids_for_newsi(bdb_state_type *bdb_state,
     free(buf);
     return 0;
 }
+
+void __txn_assert_none(void *dbenv);
+void bdb_assert_notran(bdb_state_type *bdb_state)
+{
+    __txn_assert_none(bdb_state->dbenv);
+}
