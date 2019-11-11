22932fbc5b5a6fa46101ebbbd5832a8a4b1b700d
Nirbhay Choubey <nchoubey@bloomberg.net>
2019-11-11 17:10:54 -0500

    Fix code format

--- util/ssl_support.c
+++ util/ssl_support.c
@@ -304,16 +304,15 @@ int SBUF2_FUNC(ssl_new_ctx)(SSL_CTX **pctx, ssl_mode mode, const char *dir,
     /* Test read permission on certificate. */
     if (cert != NULL && (rc = access(cert, R_OK)) != 0) {
         ssl_sfeprint(err, n, my_ssl_eprintln,
-                     "Failed to read certificate %s: %s.",
-                     cert, strerror(errno));
+                     "Failed to read certificate %s: %s.", cert,
+                     strerror(errno));
         goto error;
     }
 
     if (key != NULL) {
         if ((rc = stat(key, &buf)) != 0) {
             ssl_sfeprint(err, n, my_ssl_eprintln,
-                         "Failed to access key %s: %s.",
-                         key, strerror(errno));
+                         "Failed to access key %s: %s.", key, strerror(errno));
             goto error;
         }
 
@@ -349,8 +348,7 @@ int SBUF2_FUNC(ssl_new_ctx)(SSL_CTX **pctx, ssl_mode mode, const char *dir,
     /* Test read permission on cacert. */
     if (ca != NULL && (rc = access(ca, R_OK)) != 0) {
         /* User has provided us with root CA. */
-        ssl_sfeprint(err, n, my_ssl_eprintln,
-                     "Could not read cacert %s: %s.",
+        ssl_sfeprint(err, n, my_ssl_eprintln, "Could not read cacert %s: %s.",
                      ca, strerror(errno));
         goto error;
     }
