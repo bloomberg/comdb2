f3193a848a5660c3f57e915e80ec2306c4dc6f6b
Nirbhay Choubey <nchoubey@bloomberg.net>
2019-11-12 15:24:21 -0500

    Legacy behavior: For compound SELECT column names were picked up from the command instead of schema

--- sqlite/src/select.c
+++ sqlite/src/select.c
@@ -1934,7 +1934,12 @@ static void generateColumnNames(
       /* An AS clause always takes first priority */
       char *zName = pEList->a[i].zName;
       sqlite3VdbeSetColName(v, i, COLNAME_NAME, zName, SQLITE_TRANSIENT);
+#if !defined(SQLITE_BUILDING_FOR_COMDB2)
     }else if( srcName && p->op==TK_COLUMN ){
+#else /* defined(SQLITE_BUILDING_FOR_COMDB2) */
+    /* TODO: introduce a tunable */
+    }else if( srcName && p->op==TK_COLUMN && isCompound==0 ){
+#endif /* !defined(SQLITE_BUILDING_FOR_COMDB2) */
       char *zCol;
       int iCol = p->iColumn;
       pTab = p->y.pTab;
