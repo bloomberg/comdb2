15820b61f194e7b1fa90ee1bea99aaecfc948641
Mark Hannum <mhannum72@gmail.com>
2019-11-15 10:24:58 -0500

    This HAS to use a transaction

--- db/tag.c
+++ db/tag.c
@@ -7075,12 +7075,12 @@ static int load_new_ondisk(dbtable *db, tran_type *tran)
     newdb->meta = db->meta;
     newdb->dtastripe = gbl_dtastripe;
 
-    /* reopen db no tran - i.e. auto commit */
-    newdb->handle = bdb_open_more(
+    /* Must use tran or this can cause deadlocks */
+    newdb->handle = bdb_open_more_tran(
         db->tablename, thedb->basedir, newdb->lrl, newdb->nix,
         (short *)newdb->ix_keylen, newdb->ix_dupes, newdb->ix_recnums,
         newdb->ix_datacopy, newdb->ix_collattr, newdb->ix_nullsallowed,
-        newdb->numblobs + 1, thedb->bdb_env, &bdberr);
+        newdb->numblobs + 1, thedb->bdb_env, tran, 0, &bdberr);
 
     if (bdberr != 0 || newdb->handle == NULL) {
         logmsg(LOGMSG_ERROR, "reload_schema handle %p bdberr %d\n",
