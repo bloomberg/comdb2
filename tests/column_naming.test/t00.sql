2b0df3e86117bafe09dbbeefd8982b6535939ed2
Nirbhay Choubey <nchoubey@bloomberg.net>
2019-11-20 16:24:01 -0500

    More tests

--- tests/column_naming.test/t00.sql
+++ tests/column_naming.test/t00.sql
@@ -1,8 +1,17 @@
+select '==== test 1 ====' as test;
 create table t1(i int)$$
 insert into t1 values(1);
 select * from t1;
 select i from t1;
 select I from t1;
 select i from t1 union select 1;
+# Should get "I" with legacy_column_name enabled
 select I from t1 union select 1;
+drop table t1;
+
+select '==== test 2 ====' as test;
+create table t1(i int)$$
+insert into t1 values(1);
+# Should get "t1.i" with legacy_column_name enabled
+select t1.i from t1 join (select i from t1);
 drop table t1;
