f301334923b65964a4c42ceb4f28c855d494a113
Rivers Zhang <riverszhang89@users.noreply.github.com>
2019-11-21 10:49:41 -0500

    Fix unportable usage of realpath (#1929)
    Some systems (e.g., AIX) do not like `resolved_path` being NULL: POSIX.1-2001
    states that if `resolved_name` is a null pointer, the behavior of `realpath()` is
    implementation-defined. So use `realpath()` in a portable way.

--- lua/sp.c
+++ lua/sp.c
@@ -63,6 +63,7 @@
 #include <logmsg.h>
 #include <tohex.h>
 #include <ctrace.h>
+#include <bb_oscompat.h>
 
 #ifdef WITH_RDKAFKA    
 
@@ -3233,7 +3234,7 @@ int db_csvcopy(Lua lua)
     strbuf *columns, *params, *sql;
 
     char path[PATH_MAX + 1];
-    char *fname = realpath(fname_orig, path);
+    char *fname = comdb2_realpath(fname_orig, path);
 
     int basedir_len = strlen(thedb->basedir);
 
