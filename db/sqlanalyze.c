4e9a0cfb8b6b52e84db8985f188bac66cc77be58
Mohit Khullar <mohit.khullar@gmail.com>
2019-12-04 13:03:25 -0500

    clang format

--- db/sqlanalyze.c
+++ db/sqlanalyze.c
@@ -131,8 +131,10 @@ int backout_stats_frm_tbl(struct sqlclntstate *clnt, const char *table,
                           int stattbl)
 {
     char *sql = NULL;
-    sql = sqlite3_mprintf("delete from sqlite_stat%d where tbl='%q' and exists (select tbl from sqlite_stat%d where tbl='cdb2.%q.sav')",
-                          stattbl, table, stattbl, table);
+    sql = sqlite3_mprintf(
+        "delete from sqlite_stat%d where tbl='%q' and exists (select tbl from "
+        "sqlite_stat%d where tbl='cdb2.%q.sav')",
+        stattbl, table, stattbl, table);
     assert(sql != NULL);
     int rc = run_internal_sql_clnt(clnt, sql);
     sqlite3_free(sql); sql = NULL;
