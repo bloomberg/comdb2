e7c3605d556cc7546a5345c50228125b2e36c207
Joe Mistachkin <joe@mistachkin.com>
2019-12-17 15:35:53 -0500

    Fix compilation issue.

--- schemachange/sc_logic.c
+++ schemachange/sc_logic.c
@@ -722,8 +722,6 @@ int finalize_schema_change_thd(struct ireq *iq, tran_type *trans)
         rc = do_finalize(finalize_add_view, iq, s, trans, user_view);
     else if (s->drop_view)
         rc = do_finalize(finalize_drop_view, iq, s, trans, user_view);
-    else if (s->del_qdb_file)
-        rc = do_finalize(finalize_del_qdb_file, iq, s, trans, queue_db);
 
     reset_sc_thread(oldtype, s);
     Pthread_mutex_unlock(&s->mtx);
