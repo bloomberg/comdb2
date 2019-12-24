a92095150ed17138377353ff341848c46aaa6f64
mistachkin <joe@mistachkin.com>
2019-12-24 15:06:37 -0500

    Attempt to fix some Lua type issues.

--- tests/queuedb_rollover.test/log_consumer.lua
+++ tests/queuedb_rollover.test/log_consumer.lua
@@ -1,11 +1,17 @@
 local function main()
 	local consumer = db:consumer()
 	local event = consumer:get()
+	local oldi = "<nil>"
+	local newi = "<nil>"
+	if event.old ~= nil then
+		oldi = tostring(event.old.i)
+	end
+	if event.new ~= nil then
+		newi = tostring(event.new.i)
+	end
 	db:begin()
 	db:exec("SELECT comdb2_test_log("
-		.. event.type .. ", "
-		.. tostring(event.old.i) .. ", "
-		.. tostring(event.new.i) ..
+		.. event.type .. ", " .. oldi .. ", " .. newi ..
 	")")
 	consumer:consume()
 	db:commit()
