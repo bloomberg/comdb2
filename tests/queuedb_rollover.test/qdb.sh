3c8d243f780e33b633252ce0966f32d9d073a79d
Joe Mistachkin <joe@mistachkin.com>
2019-12-27 10:05:39 -0500

    More test work.

--- tests/queuedb_rollover.test/qdb.sh
+++ tests/queuedb_rollover.test/qdb.sh
@@ -5,3 +5,12 @@ create procedure log1 version 'logtest' {$(cat log_consumer.lua)}\$\$
 create lua consumer nop0 on (table foraudit for insert and update and delete)
 create lua consumer log1 on (table foraudit for insert and update and delete)
 EOF
+
+for ((i=1;i<9600;++i)); do
+    echo "insert into foraudit values(${i})"
+done | cdb2sql $SP_OPTIONS - > /dev/null
+
+for ((i=1;i<9600;++i)); do
+    echo "exec procedure nop0()"
+    echo "exec procedure log1()"
+done | cdb2sql $SP_OPTIONS - > /dev/null
