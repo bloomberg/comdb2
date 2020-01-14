480005f6abe96e94c98135ed7e04041278d41469
Joe Mistachkin <jmistachkin@bloomberg.net>
2020-01-14 11:39:04 -0500

    test adjustments.

--- tests/queuedb_rollover.test/qdb2.sh
+++ tests/queuedb_rollover.test/qdb2.sh
@@ -7,12 +7,12 @@ create procedure nop1 version 'noptest' {$(cat nop_consumer.lua)}\$\$
 create lua consumer nop1 on (table foraudit2 for insert and update and delete)
 EOF
 
-./qdb2_adds.sh 96000 &
+./qdb2_adds.sh 9600 &
 sleep 2
 
 cdb2sql $SP_OPTIONS "select depth > 0 from comdb2_queues where queuename = '__qnop1';"
 
-./qdb2_cons.sh 96000 &
+./qdb2_cons.sh 9600 &
 sleep 2
 
 cdb2sql $SP_OPTIONS "select depth > 0 from comdb2_queues where queuename = '__qnop1';"
