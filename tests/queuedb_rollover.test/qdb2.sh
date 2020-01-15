472fe48d7a27cb25e8e6d8ff952997f0c1b3b019
Joe Mistachkin <joe@mistachkin.com>
2020-01-15 14:29:24 -0500

    Simplify test case.

--- tests/queuedb_rollover.test/qdb2.sh
+++ tests/queuedb_rollover.test/qdb2.sh
@@ -7,16 +7,6 @@ create procedure nop1 version 'noptest' {$(cat nop_consumer.lua)}\$\$
 create lua consumer nop1 on (table foraudit2 for insert and update and delete)
 EOF
 
-./qdb2_adds.sh 9600 &
-sleep 10
-
-cdb2sql $SP_OPTIONS "select (depth > 0) as hasDepth from comdb2_queues where queuename = '__qnop1';"
-
-./qdb2_cons.sh 9600 &
-sleep 10
-
-cdb2sql $SP_OPTIONS "select (depth > 0) as hasDepth from comdb2_queues where queuename = '__qnop1';"
-
 for ((i=0;i<10;++i)); do
     for ((j=0;j<3;++j)); do
         ./qdb2_adds.sh 96 &
