e75f7d058598abb4abab926817ea55ee3ca3b894
Joe Mistachkin <joe@mistachkin.com>
2020-01-16 16:49:02 -0500

    Refine test.

--- tests/queuedb_rollover.test/qdb3.sh
+++ tests/queuedb_rollover.test/qdb3.sh
@@ -8,9 +8,9 @@ create lua consumer const3 on (table foraudit3 for insert)
 EOF
 
 for ((i=0;i<5;++i)); do
-    ./qdb3_dml_adds.sh 1000 &
-    ./qdb3_dml_cons.sh 1000 &
-    ./qdb3_ddl_cons.sh 10 &
-    ./qdb3_ddl_proc.sh 10 &
+    ./qdb3_dml_adds.sh 1000 2>&1 >/dev/null &
+    ./qdb3_dml_cons.sh 1000 2>&1 >/dev/null &
+    ./qdb3_ddl_cons.sh 10 2>&1 >/dev/null &
+    ./qdb3_ddl_proc.sh 10 2>&1 >/dev/null &
 done
 wait
--- tests/queuedb_rollover.test/qdb3_ddl_proc.sh
+++ tests/queuedb_rollover.test/qdb3_ddl_proc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 for ((k=0;k<$1;++k)); do
-    cdb2sql $SP_OPTIONS "drop procedure const3 version 'const_test'" 2>&1 >/dev/null
-    cdb2sql $SP_OPTIONS "create procedure const3 version 'const_test' {$(cat const_consumer.lua)}" 2>&1 >/dev/null
+    cdb2sql $SP_OPTIONS "drop procedure const3 version 'const_test'" > /dev/null
+    cdb2sql $SP_OPTIONS "create procedure const3 version 'const_test' {$(cat const_consumer.lua)}" > /dev/null
 done
