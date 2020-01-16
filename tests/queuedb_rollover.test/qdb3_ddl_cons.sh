1df53b0d3b3bfc55a575f06d3867f8dfd8b144ca
Joe Mistachkin <joe@mistachkin.com>
2020-01-16 15:14:09 -0500

    changes

--- tests/queuedb_rollover.test/qdb3_ddl_cons.sh
+++ tests/queuedb_rollover.test/qdb3_ddl_cons.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 for ((k=0;k<$1;++k)); do
-    cdb2sql $SP_OPTIONS "drop lua consumer const3" > /dev/null
-    cdb2sql $SP_OPTIONS "create lua consumer const3 on (table foraudit3 for insert)" > /dev/null
+    cdb2sql $SP_OPTIONS "drop lua consumer const3" 2>&1 >/dev/null
+    cdb2sql $SP_OPTIONS "create lua consumer const3 on (table foraudit3 for insert)" 2>&1 >/dev/null
 done
