08f7a48d47d182fd0d16f424083e56737aff610b
Joe Mistachkin <joe@mistachkin.com>
2020-01-24 15:29:17 -0500

    Fine tuning of test.

--- tests/queuedb_rollover.test/qdb3_dml_adds.sh
+++ tests/queuedb_rollover.test/qdb3_dml_adds.sh
@@ -2,4 +2,5 @@
 
 for ((k=0;k<$1;++k)); do
     cdb2sql $SP_OPTIONS "insert into foraudit3 values(${k})" &>/dev/null
+    sleep 0.1
 done
--- tests/queuedb_rollover.test/qdb3_dml_cons.sh
+++ tests/queuedb_rollover.test/qdb3_dml_cons.sh
@@ -2,4 +2,5 @@
 
 for ((k=0;k<$1;++k)); do
     cdb2sql $SP_OPTIONS "exec procedure const3($2)" &>/dev/null
+    sleep 0.1
 done
