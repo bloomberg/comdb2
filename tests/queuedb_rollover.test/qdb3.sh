9645f5c5fc59eeb3dd6ea3863c43fa94a8956ac9
Joe Mistachkin <joe@mistachkin.com>
2020-01-28 14:18:55 -0500

    Simplify the qdb3 test case for now.

--- tests/queuedb_rollover.test/qdb3.sh
+++ tests/queuedb_rollover.test/qdb3.sh
@@ -7,42 +7,12 @@ create procedure const3 version 'const_test' {$(cat const_consumer.lua)}\$\$
 create lua consumer const3 on (table foraudit3 for insert)
 EOF
 
-something_was_enqueued=0
-something_was_dequeued=0
-
 for ((i=0;i<5;++i)); do
-    if [[ "$something_was_enqueued" == "0" && "$i" == "3" ]]; then
-        wait
-    fi
-
-    if [[ "$something_was_dequeued" == "0" && "$i" == "3" ]]; then
-        wait
-    fi
-
     ./qdb3_dml_adds.sh 1000 &
     ./qdb3_dml_cons.sh 1000 $i &
-
-    if [[ "$something_was_enqueued" == "0" || "$something_was_dequeued" == "0" ]]; then
-        sleep 5
-    fi
-
-    had_enq=$(cdb2sql $SP_OPTIONS "select (total_enqueued > 0) as hadEnq from comdb2_queues where queuename = '__qconst3'")
-
-    if [[ "$had_enq" == "(hadEnq=1)" ]]; then
-        something_was_enqueued=1
-    fi
-
-    had_deq=$(cdb2sql $SP_OPTIONS "select (total_dequeued > 0) as hadDeq from comdb2_queues where queuename = '__qconst3'")
-
-    if [[ "$had_deq" == "(hadDeq=1)" ]]; then
-        something_was_dequeued=1
-    fi
-
     ./qdb3_ddl_cons.sh 10 &
     ./qdb3_ddl_proc.sh 10 &
 done
 wait
 
-echo something_was_enqueued=$something_was_enqueued
-echo something_was_dequeued=$something_was_dequeued
 cdb2sql $SP_OPTIONS "select (count(*) > 0) as hasRows from foraudit3;"
