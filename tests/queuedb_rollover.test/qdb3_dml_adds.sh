6306d3b82b6200a53d68d4ca6599879c87df4b29
Joe Mistachkin <joe@mistachkin.com>
2020-01-28 14:49:51 -0500

    Remove sleeps.

--- tests/queuedb_rollover.test/qdb3_dml_adds.sh
+++ tests/queuedb_rollover.test/qdb3_dml_adds.sh
@@ -2,5 +2,4 @@
 
 for ((k=0;k<$1;++k)); do
     cdb2sql $SP_OPTIONS "insert into foraudit3 values(${k})" &>/dev/null
-    sleep 0.1
 done
--- tests/queuedb_rollover.test/qdb3_dml_cons.sh
+++ tests/queuedb_rollover.test/qdb3_dml_cons.sh
@@ -2,5 +2,4 @@
 
 for ((k=0;k<$1;++k)); do
     cdb2sql $SP_OPTIONS "exec procedure const3($2)" &>/dev/null
-    sleep 0.1
 done
