1bdb9c4646f6db48865404b5564b64045cbabbda
Joe Mistachkin <joe@mistachkin.com>
2020-01-29 13:11:23 -0500

    Robustify the DML INSERT portion of the qdb3 sub-test.

--- tests/queuedb_rollover.test/qdb3_dml_adds.sh
+++ tests/queuedb_rollover.test/qdb3_dml_adds.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 for ((k=0;k<$1;++k)); do
-    cdb2sql $SP_OPTIONS "insert into foraudit3 values($2)" &>/dev/null
+    did_insert=$(cdb2sql $SP_OPTIONS "insert into foraudit3 values($2)")
+
+    if [[ "$did_insert" != "(rows inserted=1)" ]]; then
+        echo "failed INSERT for round $2 at iteration $1"
+    fi
 done
