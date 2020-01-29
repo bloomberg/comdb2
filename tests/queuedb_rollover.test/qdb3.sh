092b6a87a2f1de20e41fde14f28163a64aa55845
Joe Mistachkin <joe@mistachkin.com>
2020-01-29 11:50:39 -0500

    Fix test case.

--- tests/queuedb_rollover.test/qdb3.sh
+++ tests/queuedb_rollover.test/qdb3.sh
@@ -8,7 +8,7 @@ create lua consumer const3 on (table foraudit3 for insert)
 EOF
 
 for ((i=0;i<5;++i)); do
-    ./qdb3_dml_adds.sh 1000 &
+    ./qdb3_dml_adds.sh 1000 $i &
     ./qdb3_dml_cons.sh 1000 $i &
     ./qdb3_ddl_cons.sh 10 &
     ./qdb3_ddl_proc.sh 10 &
