e7168f99fa9af953589780a7fe412ade215f02af
Joe Mistachkin <joe@mistachkin.com>
2020-01-29 15:01:39 -0500

    fix typo in test.

--- tests/queuedb_rollover.test/qdb3_dml_adds.sh
+++ tests/queuedb_rollover.test/qdb3_dml_adds.sh
@@ -4,6 +4,6 @@ for ((k=0;k<$1;++k)); do
     did_insert=$(cdb2sql $SP_OPTIONS "insert into foraudit3 values($2)")
 
     if [[ "$did_insert" != "(rows inserted=1)" ]]; then
-        echo "failed INSERT for round $2 at iteration $1"
+        echo "failed INSERT for round $2 at iteration $k"
     fi
 done
