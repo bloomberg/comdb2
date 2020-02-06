77a4069a6e4c60afddbf315c6e6cc9ec6c1efeef
Nirbhay Choubey <nchoubey@bloomberg.net>
2020-02-06 16:23:57 -0500

    F'ix some compiler warnings

--- util/portmuxusr.c
+++ util/portmuxusr.c
@@ -545,7 +545,7 @@ static int portmux_get_unix_socket(const char *unix_bind_path)
                __func__, __LINE__);
         return -1;
     } else {
-        strncpy(addr.sun_path, gbl_portmux_unix_socket, uslen);
+        strncpy(addr.sun_path, gbl_portmux_unix_socket, sizeof(addr.sun_path));
     }
 
     len = offsetof(struct sockaddr_un, sun_path) + uslen;
