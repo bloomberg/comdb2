40cf0babdfd22d14fb01094562fb334400b79a31
Joe Mistachkin <joe@mistachkin.com>
2020-02-10 17:32:41 -0500

    test case fix.

--- tests/queuedb_rollover.test/qdb1.sh
+++ tests/queuedb_rollover.test/qdb1.sh
@@ -19,11 +19,17 @@ done | cdb2sql $SP_OPTIONS - >/dev/null
 
 for ((i=1;i<9600;++i)); do
     echo "exec procedure log1()"
-done | cdb2sql $SP_OPTIONS - >/dev/null
+done | cdb2sql --host $SP_HOST $SP_OPTIONS - >/dev/null
+
+if [ $SP_HOST == `hostname` ]; then
+    cp ${TESTDIR}/logs/${DBNAME}.db qdb1-log1.log
+else
+    scp ${SP_HOST}:${TESTDIR}/logs/${DBNAME}.db qdb1-log1.log
+fi
 
 cdb2sql $SP_OPTIONS "select queuename, depth from comdb2_queues order by queuename;"
 
-added_to_log=$(cat ${TESTDIR}/logs/${DBNAME}.db | egrep "^add, <nil>, [0123456789]{1,4}$" | uniq | wc -l)
+added_to_log=$(cat qdb1-log1.log | egrep "^add, <nil>, [0123456789]{1,4}$" | uniq | wc -l)
 
 if [ $added_to_log -ne 9599 ] ; then
     echo "bad queuedb side-effects, need 9599 entries, got $added_to_log"
