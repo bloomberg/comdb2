f81e1a8f7c3ddacd680831a2837986ca62f78174
Joe Mistachkin <jmistachkin@bloomberg.net>
2020-02-10 11:58:27 -0500

    Remove DDL stress test, which was moved to a different PR.

--- tests/queuedb_rollover.test/qdb3_ddl_proc.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-for ((k=0;k<$1;++k)); do
-    cdb2sql $SP_OPTIONS "drop procedure const3 version 'const_test'" &>/dev/null
-    cdb2sql $SP_OPTIONS "create procedure const3 version 'const_test' {$(cat const_consumer.lua)}" &>/dev/null
-done
--- tests/queuedb_rollover.test/qdb3_dml_adds.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-for ((k=0;k<$1;++k)); do
-    did_insert=$(cdb2sql $SP_OPTIONS "insert into foraudit3 values($2)")
-
-    if [[ "$did_insert" != "(rows inserted=1)" ]]; then
-        echo "failed INSERT for round $2 at iteration $k"
-    fi
-done
