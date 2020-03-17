9035c3af04e756757f9d81926f261a822cbda689
Nirbhay Choubey <nchoubey@bloomberg.net>
2020-03-17 16:19:56 -0400

    sqlitex: Fix columns names returned from procedures (7.0)
    Signed-off-by: Nirbhay Choubey <nchoubey@bloomberg.net>

--- lua/sp.c
+++ lua/sp.c
@@ -640,7 +640,8 @@ char *sp_column_name(struct response_data *arg, int col)
     if (parent->clntname[col] == NULL) {
         sqlite3_stmt *stmt = arg->stmt;
         if (stmt) {
-            parent->clntname[col] = strdup(sqlite3_column_name(stmt, col));
+            parent->clntname[col] = strdup(comdb2_column_name(arg->sp->clnt,
+                                                              stmt, col));
         } else {
             size_t n = snprintf(NULL, 0, "$%d", col);
             char *name = malloc(n + 1);
@@ -1328,9 +1329,9 @@ static int lua_sql_step(Lua lua, sqlite3_stmt *stmt)
         }
         default:
             return luaL_error(lua, "unknown field type:%d for col:%s",
-                              type, sqlite3_column_name(stmt, col));
+                              type, comdb2_column_name(clnt, stmt, col));
         }
-        lua_setfield(lua, -2, sqlite3_column_name(stmt, col));
+        lua_setfield(lua, -2, comdb2_column_name(clnt, stmt, col));
     }
     return rc;
 }
