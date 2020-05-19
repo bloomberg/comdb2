61fdb4370a0e9dee94a3d6424fb5f6e15308ffed
Michael Ponomarenko <mponomar@bloomberg.net>
2020-05-19 16:23:17 -0400

    tests: Test control chars in json output
    Signed-off-by: Akshat Sikarwar <asikarwar1@bloomberg.net>

--- tests/sp.test/sp.sh
+++ tests/sp.test/sp.sh
@@ -1376,5 +1376,13 @@ local function main(emoji)
     db:emit(json)
 end}$$
 EOF
-cdb2sql $SP_OPTIONS "exec procedure json_emoji('hello world 😁')"
-cdb2sql $SP_OPTIONS "execute procedure json_emoji('hello world 😁')"
+
+cdb2sql $SP_OPTIONS - > /dev/null <<'EOF'
+create procedure escape_controls version 'sptest' {
+local function main()
+    local tbl = {control=""}
+    local json = db:table_to_json(tbl)
+    db:emit(json)
+end}$$
+EOF
+cdb2sql $SP_OPTIONS "exec procedure escape_controls()"
