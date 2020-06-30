8cc2216f75927889e9daf9d2e8978d6d62814445
Akshat Sikarwar <asikarwar1@bloomberg.net>
2020-06-30 19:40:34 -0400

    newsql: Use non-fatal type-conversion for sp's write_response
    Signed-off-by: Akshat Sikarwar <asikarwar1@bloomberg.net>

--- plugins/newsql/newsql.c
+++ plugins/newsql/newsql.c
@@ -898,32 +898,42 @@ static int newsql_row_lua(struct sqlclntstate *clnt, struct response_data *arg)
         switch (type) {
         case SQLITE_INTEGER: {
             int64_t i64;
-            sp_column_val(arg, i, type, &i64);
+            if (sp_column_val(arg, i, type, &i64)) {
+                return -1;
+            }
             newsql_integer(cols, i, i64, flip);
             break;
         }
         case SQLITE_FLOAT: {
             double d;
-            sp_column_val(arg, i, type, &d);
+            if (sp_column_val(arg, i, type, &d)) {
+                return -1;
+            }
             newsql_double(cols, i, d, flip);
             break;
         }
         case SQLITE_TEXT: {
             size_t l;
-            cols[i].value.data = sp_column_ptr(arg, i, type, &l);
+            if ((cols[i].value.data = sp_column_ptr(arg, i, type, &l)) == NULL) {
+                return -1;
+            }
             cols[i].value.len = l + 1;
             break;
         }
         case SQLITE_BLOB: {
             size_t l;
-            cols[i].value.data = sp_column_ptr(arg, i, type, &l);
+            if ((cols[i].value.data = sp_column_ptr(arg, i, type, &l)) == NULL) {
+                return -1;
+            }
             cols[i].value.len = l;
             break;
         }
         case SQLITE_DATETIME:
         case SQLITE_DATETIMEUS: {
             datetime_t d;
-            sp_column_val(arg, i, type, &d);
+            if (sp_column_val(arg, i, type, &d)) {
+                return -1;
+            }
             if (d.prec == DTTZ_PREC_MSEC && type == SQLITE_DATETIMEUS)
                 d.frac *= 1000;
             else if (d.prec == DTTZ_PREC_USEC && type == SQLITE_DATETIME)
@@ -959,7 +969,9 @@ static int newsql_row_lua(struct sqlclntstate *clnt, struct response_data *arg)
         }
         case SQLITE_INTERVAL_YM: {
             intv_t in, *val = &in;
-            sp_column_val(arg, i, type, val);
+            if (sp_column_val(arg, i, type, val)) {
+                return -1;
+            }
             cdb2_client_intv_ym_t *c = alloca(sizeof(*c));
             newsql_ym(cols, i, val, flip);
             break;
@@ -967,7 +979,9 @@ static int newsql_row_lua(struct sqlclntstate *clnt, struct response_data *arg)
         case SQLITE_INTERVAL_DS:
         case SQLITE_INTERVAL_DSUS: {
             intv_t in, *val = &in;
-            sp_column_val(arg, i, type, &in);
+            if (sp_column_val(arg, i, type, &in)) {
+                return -1;
+            }
             newsql_ds(cols, i, val, flip);
             break;
         }
@@ -980,8 +994,7 @@ static int newsql_row_lua(struct sqlclntstate *clnt, struct response_data *arg)
     r.n_value = ncols;
     r.value = value;
     if (arg->pingpong) {
-        return newsql_response_int(clnt, &r, RESPONSE_HEADER__SQL_RESPONSE_PING,
-                                   1);
+        return newsql_response_int(clnt, &r, RESPONSE_HEADER__SQL_RESPONSE_PING, 1);
     }
     return newsql_response(clnt, &r, 0);
 }
