88213cc44b7f847b4db5da9f46c6753751d1d60e
Akshat Sikarwar <asikarwar1@bloomberg.net>
2020-08-05 11:54:58 -0400

    cmake: Use FindSQLite3
    Signed-off-by: Akshat Sikarwar <asikarwar1@bloomberg.net>

--- cmake/FindSQLite3.cmake
+++
@@ -1,5 +0,0 @@
-find_path(SQLITE3_INCLUDE_DIR NAMES sqlite3.h)
-find_library(SQLITE3_LIBRARY NAMES sqlite3)
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SQLITE3 DEFAULT_MSG SQLITE3_INCLUDE_DIR)
-find_package_handle_standard_args(libsqlite3 DEFAULT_MSG SQLITE3_LIBRARY)
