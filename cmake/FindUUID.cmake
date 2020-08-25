39e2b3ba8c61deeb8f0f037c9b6fc9a5a180ff92
Nirbhay Choubey <nchoubey@bloomberg.net>
2020-08-25 08:39:20 -0400

    Fix cmake warnings
    Also add a switch to find tinfo library.
    
    Signed-off-by: Nirbhay Choubey <nchoubey@bloomberg.net>

--- cmake/FindUUID.cmake
+++ cmake/FindUUID.cmake
@@ -1,5 +1,5 @@
 find_path(UUID_INCLUDE_DIR NAMES uuid/uuid.h)
 find_library(UUID_LIBRARY NAMES uuid)
+
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(UUID DEFAULT_MSG UUID_INCLUDE_DIR)
-find_package_handle_standard_args(libuuid DEFAULT_MSG UUID_LIBRARY)
+find_package_handle_standard_args(UUID DEFAULT_MSG UUID_INCLUDE_DIR UUID_LIBRARY)
--- cmake/FindUnwind.cmake
+++ cmake/FindUnwind.cmake
@@ -1,5 +1,5 @@
 find_path(UNWIND_INCLUDE_DIR NAMES libunwind.h)
 find_library(UNWIND_LIBRARY NAMES unwind System)
+
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(UNWIND DEFAULT_MSG UNWIND_INCLUDE_DIR)
-find_package_handle_standard_args(libunwind DEFAULT_MSG UNWIND_LIBRARY)
+find_package_handle_standard_args(Unwind DEFAULT_MSG UNWIND_INCLUDE_DIR UNWIND_LIBRARY)
