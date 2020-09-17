8c265d0150c226237b8564a777293d6e631d5b69
Aakash Arayambeth <aarayambeth@bloomberg.net>
2020-09-17 10:13:05 -0400

    One line change to initialize Mem before usage
    Signed-off-by: Aakash Arayambeth <aarayambeth@bloomberg.net>

--- db/sqlglue.c
+++ db/sqlglue.c
@@ -831,6 +831,7 @@ static int ondisk_to_sqlite_tz(struct dbtable *db, struct schema *s, void *inp,
         genid = rrn;
 
     if (genid) {
+        memset(&m[fnum], 0, sizeof(Mem));
         m[fnum].u.i = genid;
         m[fnum].flags = MEM_Int;
 
