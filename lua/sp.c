671f248a7959862bcbe6b1b47c008c752dc975f8
Nirbhay Choubey <nchoubey@bloomberg.net>
2020-09-28 08:06:47 -0400

    Some SP statememts may not get fingerprinted
    In a simple case where a SELECT statement is prepared, fetched
    and emitted only once - its fingerprint is never never logged.
    
    ```
    local function main()
        local t, rc = db:prepare([[SELECT 1,2]])
        if rc ~=0 then
    	return rc
        end
        local row = t:fetch()
        db:emit(row)
        return 0
    end
    ```
    
    Signed-off-by: Nirbhay Choubey <nchoubey@bloomberg.net>

--- lua/sp.c
+++ lua/sp.c
@@ -2149,16 +2149,21 @@ static void lua_another_step(struct sqlclntstate *clnt,
 static void lua_end_step(struct sqlclntstate *clnt, SP sp,
                          sqlite3_stmt *pStmt)
 {
-    int64_t time = comdb2_time_epochms();
     Vdbe *pVdbe = (Vdbe*)pStmt;
 
-    if ((sp != NULL) && (pVdbe != NULL) && (pVdbe->luaStartTime != 0)) {
+    /* Check whether fingerprint has already been computed. */
+    if ((pVdbe == NULL) || (pVdbe->fingerprint_added == 1)) {
+        return;
+    }
+
+    if ((sp != NULL) && (pVdbe->luaStartTime != 0)) {
         const char *zNormSql = sqlite3_normalized_sql(pStmt);
 
         if (zNormSql != NULL) {
             double cost = 0.0;
             int64_t prepMs = 0;
             int64_t timeMs;
+            int64_t time = comdb2_time_epochms();
 
             clnt_query_cost(sp->thd, &cost, &prepMs);
             timeMs = time - pVdbe->luaStartTime + prepMs;
@@ -2176,6 +2181,8 @@ static void lua_end_step(struct sqlclntstate *clnt, SP sp,
             clnt->spcost.time += timeMs;
             clnt->spcost.prepTime += prepMs;
             clnt->spcost.rows += pVdbe->luaRows;
+
+            pVdbe->fingerprint_added = 1;
         }
 
         restore_thd_cost_and_reset(sp->thd, pVdbe);
@@ -2279,6 +2286,8 @@ static int luatable_emit(Lua L)
     } else {
         return luaL_error(L, "attempt to emit row without defining columns");
     }
+    /* NC: Should we iterate over all the rows here and call lua_end_step()
+       (like dbstmt_emit()) ? */
     int rc = l_send_back_row(L, stmt, cols);
     lua_pushinteger(L, rc);
     return 1;
@@ -3242,7 +3251,13 @@ static int join_threads(SP sp)
 static int dbstmt_free(Lua L)
 {
     dbstmt_t *stmt = lua_touserdata(L, -1);
-    donate_stmt(getsp(L), stmt);
+    SP sp = getsp(L);
+
+    /* Compute and add fingerprint for this statement in case it wasn't
+       done already. */
+    lua_end_step(sp->clnt, sp, stmt->stmt);
+
+    donate_stmt(sp, stmt);
     return 0;
 }
 
