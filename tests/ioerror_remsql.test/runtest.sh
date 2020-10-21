83de3f74f2b7366652a3395c85bacbf4b0b3c2c3
Dorin Hogea <dhogea@bloomberg.net>
2020-10-21 10:22:43 -0400

    fix case when single rows take out dohsql quota
    Signed-off-by: Dorin Hogea <dhogea@bloomberg.net>

--- tests/ioerror_remsql.test/runtest.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-. /bb/bigstorn/systems/dhogea/comdb2/OPEN/c112/tests/ioerror_remsql.test/setenv.sh
-cd /bb/bigstorn/systems/dhogea/comdb2/OPEN/c112/tests/ioerror_remsql.test/
-./runit ioerrorremsql36629 
-echo; echo; echo;
-/bb/bigstorn/systems/dhogea/comdb2/OPEN/c112/tests/unsetup
