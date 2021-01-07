470b000323cf353b3b29aa3cacc5270815f67a2b
Dorin Hogea <dhogea@bloomberg.net>
2021-01-07 14:16:42 -0500

    reuse the malloced sql string in sockbplog
    Signed-off-by: Dorin Hogea <dhogea@bloomberg.net>

--- db/osqlsession.h
+++ db/osqlsession.h
@@ -33,6 +33,13 @@ typedef struct osql_uuid_req osql_uuid_req_t;
 osql_sess_t *osql_sess_create(const char *sql, int sqlen, char *tzname,
                               int type, unsigned long long rqid, uuid_t uuid,
                               const char *host, bool is_reorder_on);
+/**
+ * Same as osql_sess_create, but sql is already a malloced cstr
+ *
+ */
+osql_sess_t *osql_sess_create_socket(const char *sql, char *tzname, int type,
+                                     unsigned long long rqid, uuid_t uuid,
+                                     const char *host, bool is_reorder_on);
 
 /**
  * Terminates an in-use osql session (for which we could potentially
