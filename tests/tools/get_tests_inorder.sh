a8936b502c05a5bd012a93210e57800811903647
Adi Zaimi <azaimi@bloomberg.net>
2021-05-18 14:56:05 -0400

    Tests: Exclude long running tests by default
    Exclude by default tests that run for more than a given limit (120min).
    In order to run with such tests, we can run `make $INCLUDE_LONG_RUNNING`
    
    Signed-off-by: Adi Zaimi <azaimi@bloomberg.net>

--- tests/tools/get_tests_inorder.sh
+++ tests/tools/get_tests_inorder.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 # Get list of tests in order from longest to shortest duration
 
+SKIPLONGRUNNING=0
+SHOWTIMEOUT=
+LONGESTFIRST=1
+
+while true ; do
+  case "$1" in
+  --skip-long-running) SKIPLONGRUNNING=1; shift;;
+  --show-timeout) SHOWTIMEOUT=1; shift;;
+  --shortest-first) LONGESTFIRST=0; shift;;
+  * ) break;;
+  esac
+done
+
 DEFAULT_TIMEOUT=5  # default timeout for the tests is 5 seconds
 
 #get the tests with custom times
@@ -13,8 +26,13 @@ IFS=$'\n';
 #get the generated tests
 generated=$(for j in */*.testopts ; do 
     basedir=${j%%/*}
-    time=`echo -e "${custom_times} \n${default_times}" | grep $basedir | awk '{print $2}'`
+    time=`echo -e "${custom_times} \n${default_times}" | grep "^$basedir" | awk '{print $2}'`
     echo $j $time
 done | sed 's#.test/#_#g; s#\.testopts#_generated.test#g;' )
 
-echo -e "${custom_times} \n${default_times} \n${generated}" | sort -k2 -t' ' -nr | cut -f1 -d' '
+F=${SHOWTIMEOUT:+,\$2}
+if [ "$LONGESTFIRST" -eq 1 ] ; then
+    SORTORDER="r"
+fi
+
+echo -e "${custom_times} \n${default_times} \n${generated}" | sort -k2 -t' ' -n${SORTORDER} | awk '{ if(!'$SKIPLONGRUNNING' || $2 < 120) { print $1'$F' } }'
