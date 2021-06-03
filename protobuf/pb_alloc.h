efa9ed6c44ca500543b2353a1eb4b5ded5b87db2
Akshat Sikarwar <asikarwar1@bloomberg.net>
2021-06-03 12:44:48 -0400

    bbcmake
    Signed-off-by: Akshat Sikarwar <asikarwar1@bloomberg.net>

--- protobuf/pb_alloc.h
+++ protobuf/pb_alloc.h
@@ -1,7 +1,7 @@
 #ifndef INCLUDED_PB_ALLOC_H
 #define INCLUDED_PB_ALLOC_H
 
-#include <google/protobuf-c/protobuf-c.h> /* for ProtobufCAllocator */
+#include <protobuf-c/protobuf-c.h> /* for ProtobufCAllocator */
 #include "mem_protobuf.h" /* for comdb2_malloc_protobuf */
 
 extern comdb2bma blobmem;
@@ -9,7 +9,7 @@ extern unsigned gbl_blob_sz_thresh_bytes;
 
 static inline void* malloc_wrap(void *allocator_data, size_t n)
 {
-    return comdb2_malloc_protobuf (n);
+    return comdb2_malloc_protobuf(n);
 }
 
 static inline void free_wrap(void *allocator_data, void *p)
