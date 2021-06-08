c04bf3ada715946500876bb6a4cb20e18f26a327
Rivers Zhang <hzhang320@bloomberg.net>
2021-06-08 21:12:14 +0000

    Correctly handle unix time of -1 (#2873)
    db_struct2time() returns -1 on error, as well as for "1969-12-31T23:59:59".
    There's no way to tell the two cases apart and we wind up treating "1969-12-31T23:59:59" as an error.
    
    To fix it, errno is set if there's an actual failure. When db_struct2time() returns -1, if errno is 0,
    it's "1969-12-31T23:59:59"; otherwise it's an error.
    
    (DRQS 166367318)
    
    Signed-off-by: Rivers Zhang <hzhang320@bloomberg.net>

--- db/types.c
+++ db/types.c
@@ -7605,7 +7605,7 @@ int datetime_check_range(long long secs, int fracs)
                                                                                \
     /* convert */                                                              \
     memcpy(&tm, &cdt.tm, sizeof(cdt.tm));                                      \
-    if ((sdt.sec = db_struct2time(actualtzname, &tm)) == -1) {                 \
+    if ((sdt.sec = db_struct2time(actualtzname, &tm)) == -1 && errno) {        \
         return -1;                                                             \
     }                                                                          \
                                                                                \
@@ -7615,7 +7615,7 @@ int datetime_check_range(long long secs, int fracs)
         memcpy(&tm, &cdt.tm, sizeof(struct tm));                               \
         tm.tm_isdst = (tm.tm_isdst) ? 0 : 1;                                   \
                                                                                \
-        if ((sdt.sec = db_struct2time(actualtzname, &tm)) == -1) {             \
+        if ((sdt.sec = db_struct2time(actualtzname, &tm)) == -1 && errno) {    \
             return -1;                                                         \
         }                                                                      \
     }                                                                          \
