af712ea611bb73e3fdd2106cd7fde828024368c3
Akshat Sikarwar <asikarwar1@bloomberg.net>
2021-11-11 14:30:39 +0000

    plhash: One declaration for comdb2 modifications
    Signed-off-by: Akshat Sikarwar <asikarwar1@bloomberg.net>

--- bbinc/plhash_glue.h
+++ bbinc/plhash_glue.h
@@ -1,10 +1,8 @@
 #ifndef INCLUDED_PLHASH_GLUE_H
 #define INCLUDED_PLHASH_GLUE_H
 #include <plhash.h>
-#ifdef COMDB2_BBCMAKE
 /* Following are not available in sysutil */
-hash_t *hash_init_ptr(void);
-hash_t *hash_init_strcase(int);
-hash_t *hash_init_strcaseptr(int);
-#endif /*COMDB2_BBCMAKE*/
+hash_t *hash_init_ptr(void); /* hash of pointers (addresses) */
+hash_t *hash_init_strcase(int keyoff); /* string starts at keyoff (case-insensitive) */
+hash_t *hash_init_strcaseptr(int keyoff); /* like above, case insensitive */
 #endif /*INCLUDED_PLHASH_GLUE_H*/
