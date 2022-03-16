bloomberg/comdb2

commit 43173a9306f32ba086e0aa3b040cbd0968e3e07e

Reduce libevent overhead
The patch gets rid of a context switch from appsock_timer to appsock_rd,
and reduces the number of event_add/del calls.

Signed-off-by: Rivers Zhang <hzhang320@bloomberg.net>

db/sqlinterfaces.c
@@ -613,6 +613,8 @@ static pthread_mutex_t clnt_lk = PTHREAD_MUTEX_INITIALIZER;
 static LISTC_T(struct sqlclntstate) clntlist;
 static int64_t connid = 0;
 
+/* lru_evbuffers may be accessed by multiple sql threads, hence we need to protect it with a mutex. */
+static pthread_mutex_t lru_evbuffers_mtx = PTHREAD_MUTEX_INITIALIZER;
 static TAILQ_HEAD(lru_evbuffers, sqlclntstate) lru_evbuffers = TAILQ_HEAD_INITIALIZER(lru_evbuffers);
 static TAILQ_HEAD(sql_evbuffers, sqlclntstate) sql_evbuffers = TAILQ_HEAD_INITIALIZER(sql_evbuffers);
 
@@ -6563,29 +6565,32 @@ void rem_sql_evbuffer(struct sqlclntstate *clnt)
 
 void add_lru_evbuffer(struct sqlclntstate *clnt)
 {
-    check_appsock_rd_thd();
+    Pthread_mutex_lock(&lru_evbuffers_mtx);
     if (in_client_trans(clnt)) {
         /* Point to self -> not in lru_evbuffers list */
         TAILQ_NEXT(clnt, lru_entry) = clnt;
-        return;
+    } else {
+        TAILQ_INSERT_HEAD(&lru_evbuffers, clnt, lru_entry);
     }
-    TAILQ_INSERT_HEAD(&lru_evbuffers, clnt, lru_entry);
+    Pthread_mutex_unlock(&lru_evbuffers_mtx);
 }
 
 void rem_lru_evbuffer(struct sqlclntstate *clnt)
 {
-    check_appsock_rd_thd();
-    if (TAILQ_NEXT(clnt, lru_entry) == clnt) {
-        return;
+    Pthread_mutex_lock(&lru_evbuffers_mtx);
+    if (TAILQ_NEXT(clnt, lru_entry) != clnt) {
+        TAILQ_REMOVE(&lru_evbuffers, clnt, lru_entry);
+        TAILQ_NEXT(clnt, lru_entry) = clnt;
     }
-    TAILQ_REMOVE(&lru_evbuffers, clnt, lru_entry);
-    TAILQ_NEXT(clnt, lru_entry) = clnt;
+    Pthread_mutex_unlock(&lru_evbuffers_mtx);
 }
 
 static int close_lru_evbuffer(struct sqlclntstate *self)
 {
     check_appsock_rd_thd();
+    Pthread_mutex_lock(&lru_evbuffers_mtx);
     struct sqlclntstate *clnt = TAILQ_LAST(&lru_evbuffers, lru_evbuffers);
+    Pthread_mutex_unlock(&lru_evbuffers_mtx);
     if (clnt && clnt != self) {
         rem_lru_evbuffer(clnt);
         clnt->plugin.close(clnt);

net/net_appsock.h
@@ -43,14 +43,13 @@ extern int gbl_libevent_appsock;
 #define check_appsock_rd_thd() check_thd(appsock_rd_thd)
 #define check_appsock_timer_thd() check_thd(appsock_timer_thd)
 
-#define event_once(a, b, c)                                                    \
-    ({                                                                         \
-        int erc;                                                               \
-        if ((erc = event_base_once(a, -1, EV_TIMEOUT, b, c, NULL)) != 0) {     \
-            logmsg(LOGMSG_ERROR, "%s:%d event_base_once failed\n", __func__,   \
-                   __LINE__);                                                  \
-        }                                                                      \
-        erc;                                                                   \
+#define evtimer_once(a, b, c)                                                                                          \
+    ({                                                                                                                 \
+        int erc;                                                                                                       \
+        if ((erc = event_base_once(a, -1, EV_TIMEOUT, b, c, NULL)) != 0) {                                             \
+            logmsg(LOGMSG_ERROR, "%s:%d event_base_once failed\n", __func__, __LINE__);                                \
+        }                                                                                                              \
+        erc;                                                                                                           \
     })
 
 #endif

net/net_evbuffer.c
@@ -829,7 +829,7 @@ static void disable_write(int dummyfd, short what, void *data)
         e->fd = -1;
     }
     Pthread_mutex_unlock(&e->wr_lk);
-    event_once(base, d->func, e);
+    evtimer_once(base, d->func, e);
     free(d);
 }
 
@@ -856,7 +856,7 @@ static void disable_read(int dummyfd, short what, void *data)
     struct disable_info *d = data;
     struct event_info *e = d->e;
     do_disable_read(e);
-    event_once(wr_base, disable_write, d);
+    evtimer_once(wr_base, disable_write, d);
 }
 
 static void do_disable_heartbeats(struct event_info *e)
@@ -877,7 +877,7 @@ static void disable_heartbeats(int dummyfd, short what, void *data)
     struct disable_info *d = data;
     struct event_info *e = d->e;
     do_disable_heartbeats(e);
-    event_once(rd_base, disable_read, d);
+    evtimer_once(rd_base, disable_read, d);
 }
 
 static void do_host_close(int dummyfd, short what, void *data)
@@ -885,7 +885,7 @@ static void do_host_close(int dummyfd, short what, void *data)
     struct disable_info *d = data;
     struct event_info *e = d->e;
     host_node_close(e->host_node_ptr);
-    event_once(timer_base, disable_heartbeats, d);
+    evtimer_once(timer_base, disable_heartbeats, d);
 }
 
 static void do_close(struct event_info *e, event_callback_fn func)
@@ -897,7 +897,7 @@ static void do_close(struct event_info *e, event_callback_fn func)
     struct disable_info *i = malloc(sizeof(struct disable_info));
     i->e = e;
     i->func = func;
-    event_once(base, do_host_close, i);
+    evtimer_once(base, do_host_close, i);
 }
 
 static int skip_connect(struct event_info *e)
@@ -1052,7 +1052,7 @@ static void user_msg_callback(void *work)
     if (e->akq_full && outstanding < (max_bytes * resume_lvl)) {
         //hprintf("RESUMING RD outstanding:%zumb (max:%zumb )\n", outstanding / MB(1), max_bytes / MB(1));
         e->akq_full = 0;
-        event_once(rd_base, resume_read, e);
+        evtimer_once(rd_base, resume_read, e);
     }
     Pthread_mutex_unlock(&e->akq_lk);
     if (evbuffer_get_length(e->payload_buf) != datalen) {
@@ -1572,7 +1572,7 @@ static void do_queued(int dummyfd, short what, void *data)
 {
     struct event_info *e = data;
     struct host_connected_info *info = LIST_FIRST(&e->host_connected_list);
-    event_once(base, do_open, info);
+    evtimer_once(base, do_open, info);
 }
 
 static void finish_host_setup(int dummyfd, short what, void *data)
@@ -1607,7 +1607,7 @@ static void enable_heartbeats(int dummyfd, short what, void *data)
     e->hb_send_ev = event_new(timer_base, -1, EV_PERSIST, heartbeat_send, e);
     event_add(e->hb_check_ev, &one_sec);
     event_add(e->hb_send_ev, &one_sec);
-    event_once(base, finish_host_setup, i);
+    evtimer_once(base, finish_host_setup, i);
 }
 
 static void enable_read(int dummyfd, short what, void *data)
@@ -1624,7 +1624,7 @@ static void enable_read(int dummyfd, short what, void *data)
     e->rd_buf = evbuffer_new();
     e->rd_ev = event_new(rd_base, e->fd, EV_READ | EV_PERSIST, readcb, e);
     event_add(e->rd_ev, NULL);
-    event_once(timer_base, enable_heartbeats, i);
+    evtimer_once(timer_base, enable_heartbeats, i);
 }
 
 static void enable_write(int dummyfd, short what, void *data)
@@ -1643,7 +1643,7 @@ static void enable_write(int dummyfd, short what, void *data)
     e->wr_full = 0;
     e->decomissioned = 0;
     Pthread_mutex_unlock(&e->wr_lk);
-    event_once(rd_base, enable_read, i);
+    evtimer_once(rd_base, enable_read, i);
 }
 
 static void do_open(int dummyfd, short what, void *data)
@@ -1652,7 +1652,7 @@ static void do_open(int dummyfd, short what, void *data)
     struct event_info *e = i->e;
     check_base_thd();
     host_node_open(e->host_node_ptr, i->fd);
-    event_once(wr_base, enable_write, i);
+    evtimer_once(wr_base, enable_write, i);
 }
 
 static void host_connected(struct event_info *e, int fd, int connect_msg)
@@ -2170,7 +2170,7 @@ static void do_read(int fd, short what, void *data)
         arg->fd = fd;
         arg->addr = ss;
         arg->rd_buf = buf;
-        event_once(appsock_rd_base, info->cb, arg); /* handle_newsql_request_evbuffer */
+        evtimer_once(appsock_rd_base, info->cb, arg); /* handle_newsql_request_evbuffer */
         return;
     }
     handle_appsock(netinfo_ptr, &ss, first_byte, buf, fd);
@@ -2752,7 +2752,7 @@ void add_host(host_node_type *host_node_ptr)
         return;
     }
     init_event_net(netinfo_ptr);
-    event_once(base, do_add_host, host_node_ptr);
+    evtimer_once(base, do_add_host, host_node_ptr);
 }
 
 void decom(char *host)

net/sqlwriter.c
@@ -32,7 +32,7 @@
 #define MSEC(x) (x * 1000) /* millisecond -> microsecond */
 
 //heartbeat will tick every
-static struct timeval heartbeat_time = {.tv_sec = 0, .tv_usec = MSEC(10)};
+static struct timeval heartbeat_time = {.tv_sec = 0, .tv_usec = MSEC(100)};
 
 //send heartbeat if no data every (seconds)
 #define min_hb_time 1
@@ -66,21 +66,10 @@ struct sqlwriter {
     unsigned wr_continue : 1;
 };
 
-static void sql_enable_trickle(struct sqlwriter *writer)
-{
-    event_del(writer->heartbeat_ev);
-    event_add(writer->heartbeat_trickle_ev, NULL);
-}
-
-static void sql_disable_trickle(struct sqlwriter *writer)
-{
-    event_add(writer->heartbeat_ev, &heartbeat_time);
-    event_del(writer->heartbeat_trickle_ev);
-}
+static void sql_trickle_cb(int fd, short what, void *arg);
 
 static void sql_enable_flush(struct sqlwriter *writer)
 {
-    sql_disable_heartbeat(writer);
     struct timeval recover_ddlk_timeout = {.tv_sec = 1};
     event_add(writer->flush_ev, &recover_ddlk_timeout);
 }
@@ -102,15 +91,14 @@ static void sql_timeout_cb(int fd, short what, void *arg)
         writer->do_timeout = 0;
         writer->timed_out = 1;
     }
-    event_del(writer->timeout_ev);
-    sql_enable_trickle(writer);
     Pthread_mutex_unlock(&writer->wr_lock);
 }
+
+static void sql_heartbeat_cb(int fd, short what, void *arg);
 
 void sql_enable_heartbeat(struct sqlwriter *writer)
 {
     event_add(writer->heartbeat_ev, &heartbeat_time);
-    event_del(writer->heartbeat_trickle_ev);
 }
 
 void sql_disable_heartbeat(struct sqlwriter *writer)
@@ -122,7 +110,7 @@ void sql_disable_heartbeat(struct sqlwriter *writer)
 void sql_enable_timeout(struct sqlwriter *writer, int timeout_sec)
 {
     if (!writer->timeout_ev) {
-        writer->timeout_ev = event_new(appsock_timer_base, -1, EV_TIMEOUT, sql_timeout_cb, writer);
+        writer->timeout_ev = event_new(appsock_timer_base, -1, 0, sql_timeout_cb, writer);
     }
     struct timeval timeout = {.tv_sec = timeout_sec};
     event_add(writer->timeout_ev, &timeout);
@@ -177,16 +165,8 @@ static void sql_flush_cb(int fd, short what, void *arg)
 static int sql_flush_int(struct sqlwriter *writer)
 {
     sql_enable_flush(writer);
-    while (!writer->wr_continue && !writer->bad) {
-        event_base_dispatch(writer->wr_base);
-    }
-    if (writer->wr_continue && !writer->bad) {
-        if (!writer->done && !writer->timed_out) {
-            sql_enable_heartbeat(writer);
-        }
-        return 0;
-    }
-    return -1;
+    event_base_dispatch(writer->wr_base);
+    return (writer->wr_continue && !writer->bad) ? 0 : -1;
 }
 
 int sql_flush(struct sqlwriter *writer)
@@ -286,7 +266,6 @@ static void sql_trickle_int(struct sqlwriter *writer, int fd)
         if (difftime(time(NULL), writer->sent_at) >= min_hb_time && !writer->timed_out) {
             sql_pack_heartbeat(writer);
         } else {
-            sql_disable_trickle(writer);
             return;
         }
     }
@@ -300,35 +279,35 @@ static void sql_trickle_int(struct sqlwriter *writer, int fd)
     }
     writer->sent_at = time(NULL);
     int left = outstanding - n;
-    if (left) {
-        sql_disable_trickle(writer);
-    } else if (evbuffer_get_contiguous_space(writer->wr_buf) < KB(1)) {
+    if (left == 0 && evbuffer_get_contiguous_space(writer->wr_buf) < KB(1)) {
         evbuffer_pullup(writer->wr_buf, KB(4));
     }
 }
 
-void sql_trickle_cb(int fd, short what, void *arg)
+static void sql_trickle_cb(int fd, short what, void *arg)
 {
     if (!(what & EV_WRITE)) {
         abort();
     }
     struct sqlwriter *writer = arg;
-    Pthread_mutex_lock(&writer->wr_lock);
-    sql_trickle_int(writer, fd);
-    Pthread_mutex_unlock(&writer->wr_lock);
+    if (pthread_mutex_trylock(&writer->wr_lock) == 0) {
+        sql_trickle_int(writer, fd);
+        Pthread_mutex_unlock(&writer->wr_lock);
+    }
 }
 
-void sql_heartbeat_cb(int fd, short what, void *arg)
+static void sql_heartbeat_cb(int fd, short what, void *arg)
 {
     check_appsock_timer_thd();
     struct sqlwriter *writer = arg;
-    Pthread_mutex_lock(&writer->wr_lock);
-    int len = evbuffer_get_length(writer->wr_buf);
-    time_t now = time(NULL);
-    if (len || difftime(now, writer->sent_at) >= min_hb_time) {
-        sql_enable_trickle(writer);
+    if (pthread_mutex_trylock(&writer->wr_lock) == 0) {
+        int len = evbuffer_get_length(writer->wr_buf);
+        time_t now = time(NULL);
+        if (len || difftime(now, writer->sent_at) >= min_hb_time) {
+            event_add(writer->heartbeat_trickle_ev, NULL);
+        }
+        Pthread_mutex_unlock(&writer->wr_lock);
     }
-    Pthread_mutex_unlock(&writer->wr_lock);
 }
 
 void sql_reset(struct sqlwriter *writer)
@@ -418,14 +397,9 @@ struct sqlwriter *sqlwriter_new(struct sqlwriter_arg *arg)
     writer->wr_base = event_base_new_with_config(cfg);
     event_config_free(cfg);
 
-    int flush_flags = EV_WRITE | EV_TIMEOUT | EV_PERSIST;
-    writer->flush_ev = event_new(writer->wr_base, arg->fd, flush_flags, sql_flush_cb, writer);
-
-    int hb_flags = EV_TIMEOUT | EV_PERSIST;
-    writer->heartbeat_ev = event_new(appsock_timer_base, -1, hb_flags, sql_heartbeat_cb, writer);
-
-    int trickle_flags = EV_WRITE | EV_PERSIST;
-    writer->heartbeat_trickle_ev = event_new(appsock_timer_base, arg->fd, trickle_flags, sql_trickle_cb, writer);
+    writer->flush_ev = event_new(writer->wr_base, arg->fd, EV_WRITE | EV_PERSIST, sql_flush_cb, writer);
+    writer->heartbeat_ev = event_new(appsock_timer_base, arg->fd, EV_PERSIST, sql_heartbeat_cb, writer);
+    writer->heartbeat_trickle_ev = event_new(appsock_timer_base, arg->fd, EV_WRITE, sql_trickle_cb, writer);
 
     return writer;
 }

plugins/newsql/newsql_evbuffer.c
@@ -94,7 +94,7 @@ static void newsql_cleanup(int dummyfd, short what, void *arg)
     struct newsql_appdata_evbuffer *appdata = arg;
     sql_disable_heartbeat(appdata->writer);
     sql_disable_timeout(appdata->writer);
-    event_once(appsock_rd_base, free_newsql_appdata_evbuffer, appdata);
+    evtimer_once(appsock_rd_base, free_newsql_appdata_evbuffer, appdata);
 }
 
 static int newsql_flush_evbuffer(struct sqlclntstate *clnt)
@@ -125,7 +125,7 @@ static void newsql_read_again(int dummyfd, short what, void *arg)
             reset_clnt_flags(clnt);
         }
     }
-    event_once(appsock_rd_base, newsql_read_hdr, appdata);
+    newsql_read_hdr(-1, 0, appdata);
 }
 
 static void newsql_reset_evbuffer(struct newsql_appdata_evbuffer *appdata)
@@ -138,7 +138,7 @@ static int newsql_done_cb(struct sqlclntstate *clnt)
 {
     struct newsql_appdata_evbuffer *appdata = clnt->appdata;
     if (clnt->query_rc == CDB2ERR_IO_ERROR) { /* dispatch timed out */
-        return event_once(appsock_timer_base, newsql_cleanup, appdata);
+        return evtimer_once(appsock_timer_base, newsql_cleanup, appdata);
     }
     if (clnt->osql.replay == OSQL_RETRY_DO) {
         clnt->done_cb = NULL;
@@ -153,9 +153,9 @@ static int newsql_done_cb(struct sqlclntstate *clnt)
         appdata->query = NULL;
     }
     if (sql_done(appdata->writer) == 0) {
-        event_once(appsock_timer_base, newsql_read_again, appdata);
+        newsql_read_again(-1, 0, appdata);
     } else {
-        event_once(appsock_timer_base, newsql_cleanup, appdata);
+        evtimer_once(appsock_timer_base, newsql_cleanup, appdata);
     }
     return 0;
 }
@@ -252,7 +252,7 @@ static void write_dbinfo(int dummyfd, short what, void *arg)
     } else if (evbuffer_get_length(wrbuf)) {
         event_base_once(appsock_timer_base, appdata->fd, EV_WRITE, write_dbinfo, appdata, NULL);
     } else {
-        event_once(appsock_rd_base, rd_newsql_hdr, appdata);
+        evtimer_once(appsock_rd_base, rd_newsql_hdr, appdata);
     }
 }
 
@@ -346,8 +346,10 @@ static void process_query(struct newsql_appdata_evbuffer *appdata, CDB2QUERY *qu
     int do_read = 0;
     int commit_rollback;
     appdata->query = query;
-    appdata->sqlquery = query->sqlquery;
+    CDB2SQLQUERY *sqlquery = appdata->sqlquery = query->sqlquery;
     struct sqlclntstate *clnt = &appdata->clnt;
+    if (sqlquery == NULL)
+        goto out;
     if (!appdata->active) {
         if (add_appsock_connection_evbuffer(clnt) != 0) {
             add_lru_evbuffer(clnt);
@@ -357,12 +359,12 @@ static void process_query(struct newsql_appdata_evbuffer *appdata, CDB2QUERY *qu
         appdata->active = 1;
     }
     if (appdata->initial) {
-        if (newsql_first_run(clnt, query->sqlquery) != 0) {
+        if (newsql_first_run(clnt, sqlquery) != 0) {
             goto out;
         }
         appdata->initial = 0;
     }
-    if (newsql_loop(clnt, query->sqlquery) != 0) {
+    if (newsql_loop(clnt, sqlquery) != 0) {
         goto out;
     }
     if (newsql_should_dispatch(clnt, &commit_rollback) != 0) {
@@ -378,7 +380,7 @@ static void process_query(struct newsql_appdata_evbuffer *appdata, CDB2QUERY *qu
         return;
     }
 out:cdb2__query__free_unpacked(query, NULL);
-    event_once(appsock_timer_base, do_read ? newsql_read_again : newsql_cleanup, appdata);
+    evtimer_once(appsock_timer_base, do_read ? newsql_read_again : newsql_cleanup, appdata);
 }
 
 static void process_cdb2query(struct newsql_appdata_evbuffer *appdata, CDB2QUERY *query)
@@ -410,7 +412,7 @@ static void process_newsql_payload(struct newsql_appdata_evbuffer *appdata, CDB2
     case CDB2_REQUEST_TYPE__SSLCONN:
         /* not implemented - disable us for now */
         gbl_libevent_appsock = 0;
-        event_once(appsock_timer_base, newsql_cleanup, appdata);
+        evtimer_once(appsock_timer_base, newsql_cleanup, appdata);
         break;
     default:
         logmsg(LOGMSG_ERROR, "%s bad type:%d fd:%d\n", __func__, appdata->hdr.type, appdata->fd);
@@ -423,7 +425,7 @@ static void rd_payload(int dummyfd, short what, void *arg)
     struct newsql_appdata_evbuffer *appdata = arg;
     if (what & EV_READ) {
         if (evbuffer_read(appdata->rd_buf, appdata->fd, -1) <= 0) {
-            event_once(appsock_timer_base, newsql_cleanup, appdata);
+            evtimer_once(appsock_timer_base, newsql_cleanup, appdata);
             return;
         }
     }
@@ -435,7 +437,7 @@ static void rd_payload(int dummyfd, short what, void *arg)
     int len = appdata->hdr.length;
     void *data = evbuffer_pullup(appdata->rd_buf, len);
     if (!len || (query = cdb2__query__unpack(NULL, len, data)) == NULL) {
-        event_once(appsock_timer_base, newsql_cleanup, appdata);
+        evtimer_once(appsock_timer_base, newsql_cleanup, appdata);
     } else {
         evbuffer_drain(appdata->rd_buf, len);
         process_newsql_payload(appdata, query);
@@ -444,11 +446,10 @@ static void rd_payload(int dummyfd, short what, void *arg)
 
 static void rd_newsql_hdr(int dummyfd, short what, void *arg)
 {
-    check_appsock_rd_thd();
     struct newsql_appdata_evbuffer *appdata = arg;
     if (what & EV_READ) {
         if (evbuffer_read(appdata->rd_buf, appdata->fd, -1) <= 0) {
-            event_once(appsock_timer_base, newsql_cleanup, appdata);
+            evtimer_once(appsock_timer_base, newsql_cleanup, appdata);
             return;
         }
     }

commit 8fdef5e7567d40c144c11f677a0cc07300e59884

newsql: Fix thread-dispatch assert
This regression was introduced in (#3067)
>> 12:48:25 akdb:FATAL ERROR: add_appsock_connection_evbuffer EVENT NOT DISPATCHED on appsock_rd_thd

Fix this by locking access around the active connection list and
removing the assert.

Signed-off-by: Akshat Sikarwar <asikarwar1@bloomberg.net>

db/sqlinterfaces.c
@@ -6553,45 +6553,58 @@ void rem_sql_evbuffer(struct sqlclntstate *clnt)
     TAILQ_REMOVE(&sql_evbuffers, clnt, sql_entry);
 }
 
-void add_lru_evbuffer(struct sqlclntstate *clnt)
+static void add_lru_evbuffer_int(struct sqlclntstate *clnt)
 {
-    Pthread_mutex_lock(&lru_evbuffers_mtx);
     if (in_client_trans(clnt)) {
         /* Point to self -> not in lru_evbuffers list */
         TAILQ_NEXT(clnt, lru_entry) = clnt;
     } else {
         TAILQ_INSERT_HEAD(&lru_evbuffers, clnt, lru_entry);
     }
+}
+
+void add_lru_evbuffer(struct sqlclntstate *clnt)
+{
+    Pthread_mutex_lock(&lru_evbuffers_mtx);
+    add_lru_evbuffer_int(clnt);
     Pthread_mutex_unlock(&lru_evbuffers_mtx);
 }
 
-void rem_lru_evbuffer(struct sqlclntstate *clnt)
+static void rem_lru_evbuffer_int(struct sqlclntstate *clnt)
 {
-    Pthread_mutex_lock(&lru_evbuffers_mtx);
     if (TAILQ_NEXT(clnt, lru_entry) != clnt) {
         TAILQ_REMOVE(&lru_evbuffers, clnt, lru_entry);
         TAILQ_NEXT(clnt, lru_entry) = clnt;
     }
+}
+void rem_lru_evbuffer(struct sqlclntstate *clnt)
+{
+    Pthread_mutex_lock(&lru_evbuffers_mtx);
+    rem_lru_evbuffer_int(clnt);
     Pthread_mutex_unlock(&lru_evbuffers_mtx);
 }
 
-static int close_lru_evbuffer(struct sqlclntstate *self)
+static int close_lru_evbuffer_int(struct sqlclntstate *self)
 {
-    check_appsock_rd_thd();
-    Pthread_mutex_lock(&lru_evbuffers_mtx);
     struct sqlclntstate *clnt = TAILQ_LAST(&lru_evbuffers, lru_evbuffers);
-    Pthread_mutex_unlock(&lru_evbuffers_mtx);
     if (clnt && clnt != self) {
-        rem_lru_evbuffer(clnt);
+        rem_lru_evbuffer_int(clnt);
         clnt->plugin.close(clnt);
         return 0;
     }
     return -1;
 }
 
+static int close_lru_evbuffer(struct sqlclntstate *self)
+{
+    Pthread_mutex_lock(&lru_evbuffers_mtx);
+    int ret = close_lru_evbuffer_int(self);
+    Pthread_mutex_unlock(&lru_evbuffers_mtx);
+    return ret;
+}
+
 int add_appsock_connection_evbuffer(struct sqlclntstate *clnt)
 {
-    check_appsock_rd_thd();
     if (clnt->admin) {
        return 0;
     }
@@ -6605,7 +6618,6 @@ int add_appsock_connection_evbuffer(struct sqlclntstate *clnt)
 
 void rem_appsock_connection_evbuffer(struct sqlclntstate *clnt)
 {
-    check_appsock_rd_thd();
     if (clnt->admin) {
         return;
     }