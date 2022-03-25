81b75a667ea76e84af5ef721dad12b7cecbc7be6
Rivers Zhang <hzhang320@bloomberg.net>
2022-03-25 09:35:37 -0400

    Implementing comdb2_memory system table
    This new system table shows heap memory usage (number of used/unused/total/peak bytes) of all sub-systems.
    
    Signed-off-by: Rivers Zhang <hzhang320@bloomberg.net>

--- sqlite/ext/comdb2/tables.c
+++ sqlite/ext/comdb2/tables.c
@@ -330,6 +330,8 @@ int comdb2SystblInit(
     rc = systblFdbInfoInit(db);
   if (rc == SQLITE_OK)
     rc = sqlite3_carray_init(db, 0, 0);
+  if (rc == SQLITE_OK)
+    rc = systblMemstatsInit(db);
 #endif
   return rc;
 }
