ca9372bbf4b4f33dddc40375fb7411b296fef45f
Nirbhay Choubey <nchoubey@bloomberg.net>
2022-03-28 19:07:55 +0000

    Fix memory corruptions in reqlogger
    Due to a recent changes, reqlogger instances can now be accessed/modified
    by two threads (appsock & watchdog). Add mutex to protect critical sections.
    
    Signed-off-by: Nirbhay Choubey <nchoubey@bloomberg.net>

--- db/reqlog_int.h
+++ db/reqlog_int.h
@@ -63,6 +63,8 @@ struct tablelist {
 struct reqlogger {
     char origin[128];
 
+    pthread_mutex_t mtx;
+
     /* Everything from here onwards is transient and can be reset
      * with a bzero. */
     int start_transient;
