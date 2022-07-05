8b639cd372d44edd2424e58ac9f17f3d6794ec41
Rivers Zhang <hzhang320@bloomberg.net>
2022-07-05 11:37:10 -0400

    Use temptable for sqlite's rowset
    SQLite's rowset is implemented using binary search trees. These
    binary trees are kept in memory. When a large amount of rowid's
    are recorded, this implementation ends up using quite a lot memory.
    
    This patch provides an alternative implementation using temptable,
    which spills content to disk when the amount of entries is over a
    threshold to reduce memory footprint. It also runs faster than stock
    SQLite, thanks to its more compact and efficient data structure.
    
    Signed-off-by: Rivers Zhang <hzhang320@bloomberg.net>

--- db/db_tunables.c
+++ db/db_tunables.c
@@ -103,6 +103,7 @@ extern int skip_clear_queue_extents;
 extern int gbl_slow_rep_process_txn_freq;
 extern int gbl_slow_rep_process_txn_maxms;
 extern int gbl_sqlite_sorter_mem;
+extern int gbl_sqlite_use_temptable_for_rowset;
 extern int gbl_allow_bplog_restarts;
 extern int gbl_test_blob_race;
 extern int gbl_test_scindex_deadlock;
