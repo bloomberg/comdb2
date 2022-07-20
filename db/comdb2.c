28f1dbc63e0f169c7363f5ccaea6f19b35ec3b9c
mohitkhullar <mkhullar1@bloomberg.net>
2022-07-19 22:21:18 -0400

    Change default cache size for iam identities 7.0
    Signed-off-by: mohitkhullar <mkhullar1@bloomberg.net>

--- db/comdb2.c
+++ db/comdb2.c
@@ -207,6 +207,7 @@ int gbl_move_deadlk_max_attempt = 500;
 
 int gbl_uses_password;
 int gbl_uses_externalauth;
+int gbl_identity_cache_max = 500;
 int gbl_uses_accesscontrol_tableXnode;
 int gbl_blocksql_grace =
     10; /* how many seconds we wait for a blocksql during downgrade */
