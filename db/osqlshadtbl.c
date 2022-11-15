240ab61123e9c7e931ca891dc73eebe9d77880f1
Salil Chandra <schandra107@bloomberg.net>
2022-11-15 19:50:49 +0000

    Make tzname NULL for partial datacopy
    Signed-off-by: Salil Chandra <schandra107@bloomberg.net>

--- db/osqlshadtbl.c
+++ db/osqlshadtbl.c
@@ -2210,7 +2210,7 @@ static int insert_record_indexes(BtCursor *pCur, struct sql_thread *thd,
         if (pCur->db->ix_datacopy[ix]) {
             if (pCur->db->ix_datacopylen[ix] > 0) { // partial datacopy
                 datacopy = alloca(pCur->db->ix_datacopylen[ix]);
-                rc = stag_to_stag_buf_schemas(get_schema(pCur->db, -1), get_schema(pCur->db, ix)->partial_datacopy, pCur->ondisk_buf, datacopy, pCur->db->tablename);
+                rc = stag_to_stag_buf_schemas(get_schema(pCur->db, -1), get_schema(pCur->db, ix)->partial_datacopy, pCur->ondisk_buf, datacopy, NULL);
                 if (rc == -1) {
                     logmsg(LOGMSG_ERROR, "insert_record:partial datacopy conversion ix %d\n", ix);
                     return SQLITE_INTERNAL;
