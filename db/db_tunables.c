ff110d810b420b5749108e8c13a52a38916125d7
Mark Hannum <mhannum72@gmail.com>
2023-04-24 12:02:57 -0400

    Recover_blkseq fix & blkseq_race test
    Clang fixes
    
    NET_OSQL_SNAP_UID_REQ isn't used, remove that code
    
    Change tunable to INTERNAL from review- thank you Mike!
    
    Signed-off-by: Mark Hannum <mhannum72@gmail.com>

--- db/db_tunables.c
+++ db/db_tunables.c
@@ -82,6 +82,7 @@ extern int gbl_largepages;
 extern int gbl_loghist;
 extern int gbl_loghist_verbose;
 extern int gbl_master_retry_poll_ms;
+extern int gbl_debug_blkseq_race;
 extern int gbl_master_swing_osql_verbose;
 extern int gbl_master_swing_sock_restart_sleep;
 extern int gbl_max_lua_instructions;
