af9c38c991a6d1af4c42c1bdfeba19be917857ec
Rivers Zhang <hzhang320@bloomberg.net>
2023-07-25 13:57:32 -0400

    {172590922}: dumping tables on exit
    Quick fix: have database dump tables on exit, so that `copycomdb2 -m` won't miss any newly created tables since startup.
    
    Signed-off-by: Rivers Zhang <hzhang320@bloomberg.net>

--- db/comdb2.c
+++ db/comdb2.c
@@ -1660,6 +1660,8 @@ void clean_exit(void)
 
     stop_threads(thedb);
     flush_db();
+    if (gbl_backend_opened)
+        llmeta_dump_mapping(thedb);
 
 #   if 0
     /* TODO: (NC) Instead of sleep(), maintain a counter of threads and wait for
