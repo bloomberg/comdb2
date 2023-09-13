0d96f51a08b5b85738949b32fc2a8ba65354a0b6
Rivers Zhang <hzhang320@bloomberg.net>
2023-09-13 14:56:15 -0400

    {171377006}: Fixing spurious 402 error during leader-swing
    The osql repo is cleared in bdb_upgrade(). However currently bdb_upgrade() may be called more than once, for instance,
    when a machine has more votes than needed to become a majority. This would lead to the following race condition:
    
    ```
    machine-1                                      machine-2
    ----------------------------------------------------------------------------------
    received a vote and has majority (N/2+1)
    |
    bdb_upgrade()
     \
      clean up osql-repo
      boardcast new leader
    ----------------------------------------------------------------------------------
                                                   session restarted against machine-1
    ----------------------------------------------------------------------------------
    received 1 more vote past majority (N/2+2)
    |
    bdb_upgrade()
     \
      clean up osql-repo
      boardcast new leader
    ----------------------------------------------------------------------------------
                                                   session lost
    ```
    
    To fix this, a new `HAS_MAJORITY` message type is introduced. The type is returned when a node
    has more than enough votes to become a leader, and is treated as a no-op subsequently. The leader
    would then call `bdb_upgrade()` only once during a single generation of election.
    
    Signed-off-by: Rivers Zhang <hzhang320@bloomberg.net>

--- berkdb/build/db.h
+++ berkdb/build/db.h
@@ -1402,6 +1402,7 @@ typedef enum {
 #define	DB_VERIFY_BAD		(-30976)/* Verify failed; bad format. */
 #define	DB_CUR_STALE		(-30975)/* Cursor deserialization failed since 
 					 * something had changed. comdb2 add */
+#define	DB_HAS_MAJORITY		(-30974)/* have enough votes to become a majority */
 #define DB_LOCK_DESIRED		(-31000)/* BDB needs  a long running operation
 					 *  out of the library */
 #define DB_LOCK_DEADLOCK_CUSTOM	(-31001)/* Deadlock on custom log record.  */
