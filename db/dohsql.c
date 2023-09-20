bloomberg/comdb2

commit 457b834543446a4b1befce7d2ffb284842c1fa47

{171755293}: Fixing race on clnt
After being freed by an appsock thread or a libevent event-loop, clnt
may still be read from another thread of control. This patch fixes it.

The original patch that attempted to address this issue, unfortunately,
triggered a different race condition between sql-thread-exiting and
newsql-cleanup. This patch adds a test to verify that both of these
2 races are addressed.

Signed-off-by: Rivers Zhang <hzhang320@bloomberg.net>

db/dohsql.c
@@ -228,8 +228,6 @@ static void sqlengine_work_shard(struct thdpool *pool, void *work,
         handle_child_error(clnt, clnt->query_rc);
     }
 
-    sql_reset_sqlthread(thd->sqlthd);
-
     if (put_curtran(thedb->bdb_env, clnt)) {
         logmsg(LOGMSG_ERROR, "%s: unable to destroy a CURSOR transaction!\n",
                __func__);

db/sqlglue.c
@@ -9476,6 +9476,7 @@ void cancel_sql_statement_with_cnonce(const char *cnonce)
 void sql_dump_running_statements(void)
 {
     struct sql_thread *thd;
+    struct sqlclntstate *clnt;
     BtCursor *cur;
     struct tm tm;
     char rqid[50];
@@ -9487,24 +9488,24 @@ void sql_dump_running_statements(void)
         localtime_r((time_t *)&t, &tm);
         Pthread_mutex_lock(&thd->lk);
 
-        if (thd->clnt && thd->clnt->sql) {
-            if (thd->clnt->osql.rqid) {
+        if ((clnt = thd->clnt) != NULL && clnt->sql) {
+            if (clnt->osql.rqid) {
                 uuidstr_t us;
-                snprintf(rqid, sizeof(rqid), "txn %016llx %s",
-                         thd->clnt->osql.rqid,
-                         comdb2uuidstr(thd->clnt->osql.uuid, us));
+                snprintf(rqid, sizeof(rqid), "txn %016llx %s", clnt->osql.rqid, comdb2uuidstr(clnt->osql.uuid, us));
             } else
                 rqid[0] = 0;
 
-            logmsg(LOGMSG_USER, "id %d %02d/%02d/%02d %02d:%02d:%02d %s%s pid %d task %s ", thd->id,
-                   tm.tm_mon + 1, tm.tm_mday, 1900 + tm.tm_year, tm.tm_hour,
-                   tm.tm_min, tm.tm_sec, rqid, thd->clnt->origin, thd->clnt->conninfo.pid, thd->clnt->argv0 ? thd->clnt->argv0 : "???");
-            logmsg(LOGMSG_USER, "%s\n", thd->clnt->sql);
+            logmsg(LOGMSG_USER, "id %d %02d/%02d/%02d %02d:%02d:%02d %s%s pid %d task %s ", thd->id, tm.tm_mon + 1,
+                   tm.tm_mday, 1900 + tm.tm_year, tm.tm_hour, tm.tm_min, tm.tm_sec, rqid, clnt->origin,
+                   clnt->conninfo.pid, clnt->argv0 ? clnt->argv0 : "???");
+            if (clnt->osql.replay != OSQL_RETRY_NONE)
+                logmsg(LOGMSG_USER, "[replay] ");
+            logmsg(LOGMSG_USER, "%s\n", clnt->sql);
 
-            int nparams = thd->clnt->plugin.param_count(thd->clnt);
+            int nparams = clnt->plugin.param_count(clnt);
             char param[255];
             for (int i = 0; i < nparams; i++) {
-                char *value = param_string_value(thd->clnt, i, param, sizeof(param));
+                char *value = param_string_value(clnt, i, param, sizeof(param));
                 if (value)
                     logmsg(LOGMSG_USER, "    %s\n", value);
             }

db/sqlinterfaces.c
@@ -4423,6 +4423,15 @@ int done_cb_evbuffer(struct sqlclntstate *clnt)
 
 void signal_clnt_as_done(struct sqlclntstate *clnt)
 {
+    struct sql_thread *thd = (clnt->thd && clnt->thd->sqlthd) ? clnt->thd->sqlthd : NULL;
+
+    /* Clear the client from the sql thread, so that sql-dump won't see it. */
+    if (thd) {
+        Pthread_mutex_lock(&gbl_sql_lock);
+        thd->clnt = NULL;
+        Pthread_mutex_unlock(&gbl_sql_lock);
+    }
+
     if (clnt->done_cb) {
         clnt->done_cb(clnt); /* newsql_done_cb */
     } else {
@@ -4489,8 +4498,6 @@ static void sqlengine_work_lua_thread(void *thddata, void *work)
 
     debug_close_clnt(clnt);
     signal_clnt_as_done(clnt);
-
-    sql_reset_sqlthread(thd->sqlthd);
 
     thrman_setid(thrman_self(), "[done]");
 }
@@ -4807,7 +4814,6 @@ void sqlengine_work_appsock(struct sqlthdstate *thd, struct sqlclntstate *clnt)
     clnt_change_state(clnt, CONNECTION_IDLE);
     debug_close_clnt(clnt);
     signal_clnt_as_done(clnt);
-    sql_reset_sqlthread(sqlthd);
 
     thrman_setid(thrman_self(), "[done]");
 }
@@ -6163,17 +6169,6 @@ void clnt_change_state(struct sqlclntstate *clnt, enum connection_state state) {
     Pthread_mutex_lock(&clnt->state_lk);
     clnt->state = state;
     Pthread_mutex_unlock(&clnt->state_lk);
-}
-
-/* we have to clear
-      - sqlclntstate (key, pointers in Bt, thd)
-      - thd->tran and mode (this is actually done in Commit/Rollback)
- */
-void sql_reset_sqlthread(struct sql_thread *thd)
-{
-    if (thd) {
-        thd->clnt = NULL;
-    }
 }
 
 /**

tests/sql_dump_race.test/Makefile
@@ -1,0 +1,5 @@
+ifeq ($(TESTSROOTDIR),)
+  include ../testcase.mk
+else
+  include $(TESTSROOTDIR)/testcase.mk
+endif

tests/sql_dump_race.test/lrl.options
@@ -1,0 +1,9 @@
+# don't let watchdogs' SELECT-1 intervene
+nowatch
+
+# these settings below make the only sql thread to exit immediately and
+# gets respawned. It makes sure that we don't race with done_sql_thread()
+
+sqlenginepool maxt 1
+sqlenginepool mint 0
+sqlenginepool linger 0

tests/sql_dump_race.test/runit
@@ -1,0 +1,48 @@
+#!/usr/bin/env bash
+bash -n "$0" | exit 1
+
+dbnm=$1
+mach=`cdb2sql --tabs ${CDB2_OPTIONS} $dbnm default 'SELECT comdb2_host()'`
+
+# this triggers cleanup_clnt code path and verifies that it doesn't race with done_sql_thread()
+while true; do cdb2sql $dbnm --host $mach 'SYNTAX-ERROR'; done >/dev/null 2>&1 &
+pid_sql=$!
+
+# this verifies that sql-dump does not race with cleanup_clnt
+while true; do cdb2sql $dbnm --host $mach 'exec procedure sys.cmd.send("sql dump")'; done >/dev/null 2>&1 &
+pid_sql_dump=$!
+
+sleep 60
+
+kill -9 $pid_sql
+kill -9 $pid_sql_dump
+
+cdb2sql $dbnm --host $mach 'SELECT 1'
+if [ $? != 0 ]; then
+    echo "did not get a response from $mach???" >&2
+    exit 1
+fi
+
+# also verifies that sql dump shows replay
+cdb2sql $dbnm --host $mach "EXEC PROCEDURE sys.cmd.send('sqlenginepool maxt 8')"
+cdb2sql $dbnm --host $mach "CREATE TABLE t (i INTEGER)"
+cdb2sql $dbnm --host $mach "INSERT INTO t VALUES (1)"
+
+for i in `seq 1 8`; do
+    while true; do cdb2sql $dbnm --host $mach "UPDATE t SET i = 1 WHERE 1"; done &
+done
+
+while true; do cdb2sql $dbnm --host $mach 'exec procedure sys.cmd.send("sql dump")'; done >sql_dump.out 2>&1 &
+
+sleep 30
+
+jobs -p | xargs kill -9 >/dev/null 2>&1
+
+grep '[replay]' sql_dump.out
+if [ $? = 0 ]; then
+  echo success
+  exit 0
+else
+  echo 'Not seeing any replay in sql-dump???' >&2
+  exit 1
+fi

commit 56beae937aebf1933db87b5dc5d220bd99d2d86a

{173038130}: Fixing clnt race
This is a regression from #3778. The idea of that pull request was to clear
`sqlthd->clnt` in `signal_clnt_as_done()`, while holding the global `gbl_sql_lock`
mutex. But dohsql child worker does not call `signal_clnt_as_done()`.

This patch adds back the code to clear `sqlthd->clnt`, in `sqlengine_work_shard()`.

Signed-off-by: Rivers Zhang <hzhang320@bloomberg.net>

db/dohsql.c
@@ -228,6 +228,13 @@ static void sqlengine_work_shard(struct thdpool *pool, void *work,
         handle_child_error(clnt, clnt->query_rc);
     }
 
+    /* clear the child clnt from this sql thread */
+    if (thd->sqlthd) {
+        Pthread_mutex_lock(&gbl_sql_lock);
+        thd->sqlthd->clnt = NULL;
+        Pthread_mutex_unlock(&gbl_sql_lock);
+    }
+
     if (put_curtran(thedb->bdb_env, clnt)) {
         logmsg(LOGMSG_ERROR, "%s: unable to destroy a CURSOR transaction!\n",
                __func__);