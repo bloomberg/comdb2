b9ae436325d12a94016b0072223f6151906e4d54
Akshat Sikarwar <asikarwar1@bloomberg.net>
2023-11-06 19:27:47 +0000

    {173434718} comdb2_triggers: Check if found qdb
    Signed-off-by: Akshat Sikarwar <asikarwar1@bloomberg.net>

--- db/translistener.c
+++ db/translistener.c
@@ -1422,6 +1422,7 @@ int gather_triggers(struct gather_triggers_arg *arg)
         e.name = sp->name;
         if (strncmp(e.name, "__q", 3) == 0) e.name += 3;
         struct dbtable *qdb = getqueuebyname(sp->name);
+        if (!qdb) continue;
         switch (dbqueue_consumer_type(qdb->consumers[0])) {
         case CONSUMER_TYPE_LUA: e.type = "trigger"; break;
         case CONSUMER_TYPE_DYNLUA: e.type = "consumer"; break;
