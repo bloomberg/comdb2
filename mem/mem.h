23106327ac51641ad8faf38a4dc03697fd419f71
Rivers Zhang <hzhang320@bloomberg.net>
2024-03-06 11:10:12 -0500

    {174371954}: Fixing memory leaks
    An unterminated transaction (e.g., client executes "BEGIN", and disconnects without issuing a "COMMIT" or "ROLLBACK")
    may leak memory in a few places. This patch fixes it.
    
    Signed-off-by: Rivers Zhang <hzhang320@bloomberg.net>

--- mem/mem.h
+++ mem/mem.h
@@ -41,6 +41,7 @@ XMACRO_COMDB2MA(COMDB2MA_STATIC_DFP_DECNUMBER,  "dfp_decNumber",    0, 0) \
 XMACRO_COMDB2MA(COMDB2MA_STATIC_PROTOBUF,       "protobuf",         0, 0) \
 XMACRO_COMDB2MA(COMDB2MA_STATIC_SCHEMACHANGE,   "schemachange",     0, 0) \
 XMACRO_COMDB2MA(COMDB2MA_STATIC_LUA,            "lua",              0, 0) \
+XMACRO_COMDB2MA(COMDB2MA_STATIC_PLUGINS,        "plugins",          0, 0) \
 XMACRO_COMDB2MA(COMDB2MA_COUNT,                 NULL,               0, 0)
 
 #define XMACRO_COMDB2MA(idx, name, size, cap) idx,
