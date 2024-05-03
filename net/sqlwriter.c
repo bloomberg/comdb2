2bfe01da997f82ec2d9e462ecb97271920b6e470
Akshat Sikarwar <asikarwar1@bloomberg.net>
2024-05-03 13:45:56 +0000

    {174794643} sqlwriter: Don't disable heartbeat during flush
    Signed-off-by: Akshat Sikarwar <asikarwar1@bloomberg.net>

--- net/sqlwriter.c
+++ net/sqlwriter.c
@@ -349,8 +349,11 @@ static int sql_pack_heartbeat(struct sqlwriter *writer)
 
 static void sql_trickle_int(struct sqlwriter *writer, int fd)
 {
-    if (!writer->wr_continue || writer->bad || writer->done) {
+    if (writer->bad || writer->done) {
         sql_disable_heartbeat(writer);
+        return;
+    }
+    if (!writer->wr_continue) {
         return;
     }
     const int outstanding = evbuffer_get_length(writer->wr_buf);
@@ -386,14 +389,15 @@ static void sql_trickle_cb(int fd, short what, void *arg)
 static void sql_heartbeat_cb(int fd, short what, void *arg)
 {
     struct sqlwriter *writer = arg;
-    if (pthread_mutex_trylock(&writer->wr_lock) == 0) {
+    if (pthread_mutex_trylock(&writer->wr_lock)) return;
+    if (writer->wr_continue) {
         int len = evbuffer_get_length(writer->wr_buf);
         time_t now = time(NULL);
         if (len || difftime(now, writer->sent_at) >= min_hb_time) {
             event_add(writer->heartbeat_trickle_ev, NULL);
         }
-        Pthread_mutex_unlock(&writer->wr_lock);
     }
+    Pthread_mutex_unlock(&writer->wr_lock);
 }
 
 void sql_reset(struct sqlwriter *writer)
