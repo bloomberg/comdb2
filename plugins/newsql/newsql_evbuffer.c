7dc1d05ca12c7aca7b1321220c7d16f5a87b0e4c
Akshat Sikarwar <asikarwar1@bloomberg.net>
2024-05-30 20:24:29 +0000

    {174286343} Enqueue heartbeat before dispatching request
    Also bump sqltick, so smartbeats doesn't generate a false positive.
    
    Signed-off-by: Akshat Sikarwar <asikarwar1@bloomberg.net>

--- plugins/newsql/newsql_evbuffer.c
+++ plugins/newsql/newsql_evbuffer.c
@@ -585,6 +585,7 @@ static void process_query(struct newsql_appdata_evbuffer *appdata)
         }
     }
     clnt->sqlite_row_format = have_sqlite_fmt;
+    ++clnt->sqltick;
 
     /* If the connection is forwarded from a secure pmux port,
      * both IAM and TLS must be enabled on the database. */
