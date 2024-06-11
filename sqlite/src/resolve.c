535dc56102bd0f1d481ff5a75d534e22eb18abb7
Rivers Zhang <hzhang320@bloomberg.net>
2024-06-11 09:41:49 -0400

    Cherrypick of SQLite planner improvement on LIMIT
    https://sqlite.org/src/info/c4a9dda2809c6e0e
    
    Signed-off-by: Rivers Zhang <hzhang320@bloomberg.net>

--- sqlite/src/resolve.c
+++ sqlite/src/resolve.c
@@ -1199,7 +1199,7 @@ static int resolveCompoundOrderBy(
       Expr *pE, *pDup;
       if( pItem->done ) continue;
       pE = sqlite3ExprSkipCollate(pItem->pExpr);
-      if( sqlite3ExprIsInteger(pE, &iCol) ){
+      if( sqlite3ExprIsInteger(pE, &iCol, 0) ){
         if( iCol<=0 || iCol>pEList->nExpr ){
           resolveOutOfRangeError(pParse, "ORDER", i+1, pEList->nExpr);
           return 1;
@@ -1411,7 +1411,7 @@ static int resolveOrderGroupBy(
         continue;
       }
     }
-    if( sqlite3ExprIsInteger(pE2, &iCol) ){
+    if( sqlite3ExprIsInteger(pE2, &iCol, 0) ){
       /* The ORDER BY term is an integer constant.  Again, set the column
       ** number so that sqlite3ResolveOrderGroupBy() will convert the
       ** order-by term to a copy of the result-set expression */
