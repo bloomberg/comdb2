e2f457a070928c506f89155acd3e66232ed21f63
Morgan Douglas <mdouglas47@bloomberg.net>
2024-07-16 17:27:38 -0400

    Update tests
    Signed-off-by: Morgan Douglas <mdouglas47@bloomberg.net>

---
+++ tests/bulkimport.test/fixtures.sh
@@ -0,0 +1,55 @@
+source util.sh
+
+function fixture_src_tbl_and_dst_tbl_are_the_same() {
+	local -n _src_tbl="$1" _dst_tbl="$2"
+	_src_tbl=foo _dst_tbl=foo
+	query_src_db "create table $_src_tbl(i int)"
+	query_src_db "insert into $_src_tbl values(1)"
+	query_dst_db "create table $_dst_tbl(i int)"
+}
+
+function fixture_src_tbl_and_dst_tbl_have_different_schemas() {
+	local -n _src_tbl="$1" _dst_tbl="$2"
+	_src_tbl=foo _dst_tbl=foo
+	query_src_db "create table $_src_tbl(i int)"
+	query_src_db "insert into $_src_tbl values(1)"
+	query_dst_db "create table $_dst_tbl(i int, j int)"
+}
+
+function fixture_src_tbl_and_dst_tbl_have_different_names() {
+	local -n _src_tbl="$1" _dst_tbl="$2"
+	_src_tbl=foo _dst_tbl=bar
+	query_src_db "create table $_src_tbl(i int)"
+	query_src_db "insert into $_src_tbl values(1)"
+	query_dst_db "create table $_dst_tbl(b blob)"
+}
+
+function fixture_src_tbl_has_noblob_dst_tbl_has_blob() {
+	local -n _src_tbl="$1" _dst_tbl="$2"
+	_src_tbl=foo _dst_tbl=foo
+	query_src_db "create table $_src_tbl(i int)"
+	query_src_db "insert into $_src_tbl values(1)"
+	query_dst_db "create table $_dst_tbl(b blob)"
+}
+
+function fixture_src_tbl_has_blob_dst_tbl_has_no_blob() {
+	local -n _src_tbl="$1" _dst_tbl="$2"
+	_src_tbl=foo _dst_tbl=foo
+	query_src_db "create table $_src_tbl(b blob)"
+	query_src_db "insert into $_src_tbl values(x'f00f')"
+	query_dst_db "create table $_dst_tbl(i int)"
+}
+
+function fixture_src_tbl_was_schema_changed() {
+	local -n _src_tbl="$1" _dst_tbl="$2"
+	_src_tbl=foo _dst_tbl=foo
+	query_src_db "create table $_src_tbl(i int)"
+	query_src_db "insert into $_src_tbl values(1)"
+	query_src_db "alter table $_src_tbl add column j int"
+	query_src_db "insert into $_src_tbl values(2, 3)"
+	query_src_db "alter table $_src_tbl add column b blob"
+	query_src_db "insert into $_src_tbl values(4, 5, x'f00f')"
+	query_dst_db "create table $_dst_tbl(i int)"
+}
+
+FIXTURES=$(compgen -A function | grep -oh "fixture_\w*")
--- tests/bulkimport.test/util.sh
+++ tests/bulkimport.test/util.sh
@@ -1,25 +1,98 @@
 source constants.sh
 
+function echo_err() {
+	printf "%s\n" "$*" >&2;
+}
+
 function failexit() {
 	rc=$1
 	msg=$2
 
 	if ((rc != 0)); then
-		echo $msg
+		echo_err $msg
 		exit $rc
 	fi
 }
 
-function query_src_db() {
+function query_src_db_opts() {
 	query=$1
-	cdb2sql ${CDB2_OPTIONS} $SRC_DBNAME default "$query" &> /dev/null
-	return $?
+	opts=$2
+
+	cdb2sql ${CDB2_OPTIONS} ${opts} $SRC_DBNAME default "$query"
+}
+
+function query_dst_db_opts() {
+	query=$1
+	opts=$2
+
+	cdb2sql ${SECONDARY_CDB2_OPTIONS} ${opts} $DST_DBNAME default "$query"
+}
+
+function query_src_db() {
+	query_src_db_opts "$1"
 }
 
 function query_dst_db() {
-	query=$1
-	cdb2sql ${SECONDARY_CDB2_OPTIONS} $DST_DBNAME default "$query" &> /dev/null
-	return $?
+	query_dst_db_opts "$1"
+}
+
+function query_in_loop() {
+	query_func=$1
+	query=$2
+	wait_time=$3
+
+	rc=0
+
+	while true
+	do
+		$query_func "$query"
+		rc=$?
+
+		if (( rc != 0 ));
+		then
+			break
+		fi
+
+		sleep $wait_time
+	done
+
+	echo_err "ERROR returning $rc"
+	return $rc
+}
+
+function verify_equal() {
+	exp=$1
+	res=$2
+	what=$3
+
+	rc=0
+	
+	if [[ "$res" != "$exp" ]]; then
+		echo_err "Mismatch in $what -- src: $exp; dst : $res"
+		rc=1
+	fi
+
+	return $rc
+}
+
+function verify_records_are_superset() {
+	subset=$1
+	superset=$2
+	rc=0
+
+	for i in $subset;
+	do
+		echo "$superset" | grep $i
+		rc=$?
+
+		if (( rc != 0 )); then
+			echo_err "Failed to find src record $i in res records $superset"
+			break
+		fi
+
+	done
+
+	return $rc
 }
 
 function verify_node() {
@@ -28,48 +101,59 @@ function verify_node() {
 	exp_records=$3
 	exp_schemas=$4
 	tier=$5
+	verify_superset=$6
 	rc=0
 
 	res_records=$(cdb2sql ${SECONDARY_CDB2_OPTIONS} -tabs $DST_DBNAME $tier \
 	"select * from $dst_table")
-
-	if [[ "$res_records" != "$exp_records" ]]; then
-		echo "Mismatch -- src records: $exp_records; dst records: $res_records"
-		rc=1
-	fi
-
 	res_schemas=$(cdb2sql ${SECONDARY_CDB2_OPTIONS} -tabs $DST_DBNAME $tier \
 	"select version, csc2 from comdb2_schemaversions where tablename='$dst_table'")
 
-	if [[ "$res_schemas" != "$exp_schemas" ]]; then
-		echo "Mismatch -- src schemas: $exp_schemas; dst schemas: $res_schemas"
-		rc=1
+	if (( verify_superset == 1 ));
+	then
+		verify_records_are_superset "$exp_records" "$res_records"
+	else
+		verify_equal "$exp_records" "$res_records" "records"
 	fi
+
+	$(exit $?) && verify_equal "$exp_schemas" "$res_schemas" "schemas"
+
+	return $?
+}
+
+function verify_import() {
+	local nodes res_is_superset
+	rc=0
+	src_table=$1
+	dst_table=$2
+	exp_records=$3
+
+	if [[ -z $exp_records ]]; then res_is_superset=0; else res_is_superset=1; fi
+	if [[ -z "$CLUSTER" ]]; then nodes=$HOSTNAME; else nodes=$CLUSTER; fi
+
+	exp_records=${exp_records:=$(cdb2sql ${CDB2_OPTIONS} -tabs $SRC_DBNAME default "select * from $src_table")}
+	exp_schemas=$(cdb2sql ${CDB2_OPTIONS} -tabs $SRC_DBNAME default "select version, csc2 from comdb2_schemaversions where tablename='$src_table'")
+
+	for node in $nodes; do
+		verify_node "$src_table" "$dst_table" "$exp_records" "$exp_schemas" "--host $node" $res_is_superset
+
+		rc=$?
+		if ! [ $rc -eq 0 ]; then break; fi
+	done
 
 	return $rc
 }
 
-function verify_import() {
-	src_table=$1
-	dst_table=$2
-	rc=0
+function verify() {
+	rc=$1
+	src_tbl=$2
+	dst_tbl=$3
+	exp_records=$4
 
-	exp_records=$(cdb2sql ${CDB2_OPTIONS} -tabs $SRC_DBNAME default \
-	"select * from $src_table")
-	exp_schemas=$(cdb2sql ${CDB2_OPTIONS} -tabs $SRC_DBNAME default \
-	"select version, csc2 from comdb2_schemaversions where tablename='$src_table'")
-
-	if [[ -z "$CLUSTER" ]]; then
-		verify_node "$src_table" "$dst_table" "$exp_records" "$exp_schemas" default
+	if (( rc == 0 ));
+	then
+		verify_import $src_tbl $dst_tbl "$exp_records"
 		rc=$?
-	else
-		for node in $CLUSTER; do
-			verify_node "$src_table" "$dst_table" "$exp_records" "$exp_schemas" "--host $node"
-			rc=$?
-			if ((rc != 0)); then
-				break
-			fi
-		done
 	fi
 
 	return $rc
