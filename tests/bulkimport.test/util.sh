7705c470b7edfd06a40e82490fcd23980e0d2fde
Morgan Douglas <mdouglas47@bloomberg.net>
2024-08-01 18:21:12 -0400

    Refactor test
    Signed-off-by: Morgan Douglas <mdouglas47@bloomberg.net>

--- tests/bulkimport.test/util.sh
+++ tests/bulkimport.test/util.sh
@@ -60,82 +60,37 @@ function query_in_loop() {
 	return $rc
 }
 
-function verify_equal() {
-	exp=$1
-	res=$2
-	what=$3
-
-	rc=0
-	
-	if [[ "$res" != "$exp" ]]; then
-		echo_err "Mismatch in $what -- src: $exp; dst : $res"
-		rc=1
-	fi
-
-	return $rc
-}
-
-function verify_records_are_superset() {
-	subset=$1
-	superset=$2
-	rc=0
-
-	for i in $subset;
-	do
-		echo "$superset" | grep $i
-		rc=$?
-
-		if (( rc != 0 )); then
-			echo_err "Failed to find src record $i in res records $superset"
-			break
-		fi
-
-	done
-
-	return $rc
-}
-
 function verify_node() {
 	src_table=$1
 	dst_table=$2
-	exp_records=$3
-	exp_schemas=$4
-	tier=$5
-	verify_superset=$6
-	rc=0
+	hostname=$3
+	verify_superset=$4
 
-	res_records=$(cdb2sql ${SECONDARY_CDB2_OPTIONS} -tabs $DST_DBNAME $tier \
-	"select * from $dst_table")
-	res_schemas=$(cdb2sql ${SECONDARY_CDB2_OPTIONS} -tabs $DST_DBNAME $tier \
-	"select version, csc2 from comdb2_schemaversions where tablename='$dst_table'")
+	incorrect_schemas=$(cdb2sql ${SECONDARY_CDB2_OPTIONS} -tabs $DST_DBNAME --host $hostname "select version, csc2 from comdb2_schemaversions except select version,csc2 from LOCAL_$SRC_DBNAME.comdb2_schemaversions")
 
-	if (( verify_superset == 1 ));
+	incorrect_schemas="$incorrect_schemas$(cdb2sql ${SECONDARY_CDB2_OPTIONS} -tabs $DST_DBNAME --host $hostname "select version, csc2 from LOCAL_$SRC_DBNAME.comdb2_schemaversions except select version,csc2 from comdb2_schemaversions")"
+
+	incorrect_records=$(cdb2sql ${SECONDARY_CDB2_OPTIONS} -tabs $DST_DBNAME --host $hostname "select * from LOCAL_$SRC_DBNAME.$src_table except select * from $dst_table")
+
+	if (( verify_superset == 0 ));
 	then
-		verify_records_are_superset "$exp_records" "$res_records"
-	else
-		verify_equal "$exp_records" "$res_records" "records"
+		incorrect_records="$incorrect_records$(cdb2sql ${SECONDARY_CDB2_OPTIONS} -tabs $DST_DBNAME --host $hostname "select * from $dst_table except select * from LOCAL_$SRC_DBNAME.$src_table")"
 	fi
 
-	$(exit $?) && verify_equal "$exp_schemas" "$res_schemas" "schemas"
-
-	return $?
+	[[ -z $incorrect_records && -z $incorrect_schemas ]]
 }
 
 function verify_import() {
-	local nodes res_is_superset
+	local nodes
 	rc=0
 	src_table=$1
 	dst_table=$2
-	exp_records=$3
+	res_is_superset=$3
 
-	if [[ -z $exp_records ]]; then res_is_superset=0; else res_is_superset=1; fi
 	if [[ -z "$CLUSTER" ]]; then nodes=$HOSTNAME; else nodes=$CLUSTER; fi
 
-	exp_records=${exp_records:=$(cdb2sql ${CDB2_OPTIONS} -tabs $SRC_DBNAME default "select * from $src_table")}
-	exp_schemas=$(cdb2sql ${CDB2_OPTIONS} -tabs $SRC_DBNAME default "select version, csc2 from comdb2_schemaversions where tablename='$src_table'")
-
 	for node in $nodes; do
-		verify_node "$src_table" "$dst_table" "$exp_records" "$exp_schemas" "--host $node" $res_is_superset
+		verify_node "$src_table" "$dst_table" "$node" $res_is_superset
 
 		rc=$?
 		if ! [ $rc -eq 0 ]; then break; fi
@@ -148,11 +103,11 @@ function verify() {
 	rc=$1
 	src_tbl=$2
 	dst_tbl=$3
-	exp_records=$4
+	superset=$4
 
 	if (( rc == 0 ));
 	then
-		verify_import $src_tbl $dst_tbl "$exp_records"
+		verify_import $src_tbl $dst_tbl $superset
 		rc=$?
 	fi
 
