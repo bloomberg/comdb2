2144e0d625c97122e3b5c3a18c6ec4cc1760ce36
Dorin Hogea <dhogea@bloomberg.net>
2024-08-27 09:44:29 -0400

    PR switches remtran implementation to run over cdb2api instead of legacy remtran plugin. Currently it supports non-2pc transactions and socksql transaction isolation. It provides protocol versioning and solves the stale schema change issue, which the previous implementation did not support.
    PR adds a fdb compatibility testcase to check for ability to execute cross-cluster queries and writes with older and newer protocol
    versions.
    
    The new implementation eliminates some of the fragile synchronization between read and write remote cursors in the previous implementation,
    by running the write statements remotely (push mode extended to writes).
    
    Signed-off-by: Dorin Hogea <dhogea@bloomberg.net>

--- db/db_tunables.c
+++ db/db_tunables.c
@@ -102,6 +102,7 @@ extern int gbl_fdb_allow_cross_classes;
 extern int gbl_fdb_resolve_local;
 extern int gbl_fdb_push_redirect_foreign;
 extern int gbl_fdb_push_remote;
+extern int gbl_fdb_push_remote_write;
 extern int gbl_fdb_remsql_cdb2api;
 extern int gbl_goslow;
 extern int gbl_heartbeat_send;
@@ -1084,6 +1085,17 @@ static int fdb_default_ver_update(void *context, void *value)
     *(int*)tunable->var = val;
     return 0;
 }
+
+static int fdb_push_write_update(void *context, void *value)
+{
+    comdb2_tunable *tunable = (comdb2_tunable *)context;
+    int val = *(int*)value;
+    if (fdb_push_write_set(val))
+        return -1;
+    *(int*)tunable->var = val;
+    return 0;
+}
+
 
 
 /* Forward declaration */
