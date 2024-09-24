250961c4abda70c45a071d3a948f225078fb77b6
Salil Chandra <schandra107@bloomberg.net>
2024-09-24 11:55:35 -0400

    {176677734} Backport fix from upstream to make expert mode work with expression indices
    Signed-off-by: Salil Chandra <schandra107@bloomberg.net>

--- sqlite/ext/expert/sqlite3expert.c
+++ sqlite/ext/expert/sqlite3expert.c
@@ -1588,6 +1588,12 @@ static int idxPopulateOneStat1(
     const char *zComma = zCols==0 ? "" : ", ";
     const char *zName = (const char*)sqlite3_column_text(pIndexXInfo, 0);
     const char *zColl = (const char*)sqlite3_column_text(pIndexXInfo, 1);
+    if( zName==0 ){
+      /* This index contains an expression. Ignore it. */
+      sqlite3_free(zCols);
+      sqlite3_free(zOrder);
+      return sqlite3_reset(pIndexXInfo);
+    }
     zCols = idxAppendText(&rc, zCols, 
         "%sx.%Q IS rem(%d, x.%Q) COLLATE %s", zComma, zName, nCol, zName, zColl
     );
