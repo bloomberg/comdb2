bloomberg/comdb2

commit b21efc2cbda0d5af5fa48177cf31e734ca665be2

Exclude osql sessions from client stats
Don't show osql sessions in clientstats. They would appear as tag writers
which can be confusing when identifying legacy traffic

Signed-off-by: Rivers Zhang <hzhang320@bloomberg.net>

db/sltdbt.c
@@ -414,8 +414,9 @@ int handle_ireq(struct ireq *iq)
     reqlog_pushprefixf(iq->reqlogger, "%s:REQ %s ", getorigin(iq),
                        req2a(iq->opcode));
 
-    iq->rawnodestats =
-        get_raw_node_stats(NULL, NULL, iq->frommach, sbuf2fileno(iq->sb), 0 /* tag does not support ssl */);
+    if (!iq->sorese) /* don't count osql */
+        iq->rawnodestats =
+            get_raw_node_stats(NULL, NULL, iq->frommach, sbuf2fileno(iq->sb), 0 /* tag does not support ssl */);
     if (iq->rawnodestats && iq->opcode >= 0 && iq->opcode < MAXTYPCNT)
         iq->rawnodestats->opcode_counts[iq->opcode]++;
     if (gbl_print_deadlock_cycles && IQ_HAS_SNAPINFO(iq))

commit 3de6f8c77cc04f354ac11d202abe07143e4e8c96

Fixing misleading error message introduced in #5005
Signed-off-by: Rivers Zhang <hzhang320@bloomberg.net>

db/sltdbt.c
@@ -571,9 +571,10 @@ int handle_ireq(struct ireq *iq)
         osql_sess_reqlogquery(iq->sorese, iq->reqlogger);
         /* Free the sorese transaction buffer */
         free(iq->p_buf_out_start);
+    } else {
+        release_node_stats(NULL, NULL, iq->frommach);
     }
     reqlog_end_request(iq->reqlogger, rc, __func__, __LINE__);
-    release_node_stats(NULL, NULL, iq->frommach);
     if (gbl_print_deadlock_cycles)
         osql_snap_info = NULL;
 