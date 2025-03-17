acb27645063a52ca183006e7ca9fcd1f29abe2ec
Aakash Arayambeth <aarayambeth@bloomberg.net>
2025-03-17 18:09:24 -0400

    use separate sc_done type for genshards sc
    Signed-off-by: Aakash Arayambeth <aarayambeth@bloomberg.net>

--- schemachange/sc_callbacks.c
+++ schemachange/sc_callbacks.c
@@ -793,12 +793,12 @@ static int scdone_add(const char tablename[], void *arg, scdone_t type)
     }
 
     /* 'dbnames' is passed as a string arg while finalizing an add of a generic sharded table */
-    if (arg && strcmp(arg, "dbnames") == 0 && db->sqlaliasname) {
+    /*if (arg && strcmp(arg, "dbnames") == 0 && db->sqlaliasname) {
         rc = gen_shard_update_inmem_db(tran, db, db->sqlaliasname);
         if (rc != 0) {
             logmsg(LOGMSG_ERROR, "REPLICANT FAILED TO UPDATE GENERIC SHARD INFO\n");
         }
-    }
+    }*/
 
 
     _master_recs(tran, tablename, type);
@@ -1270,6 +1270,26 @@ static int scdone_alias(const char tablename[], void *arg, scdone_t type)
     _untran(tran, lid);
     return 0;
 }
+
+static int scdone_gen_shard(const char tablename[] , void *arg, scdone_t type) 
+{
+    tran_type *tran = NULL;
+    uint32_t lid = 0;
+    int rc = 0;
+    int bdberr = 0;
+    struct dbtable *db = get_dbtable_by_name(tablename);
+    tran = _tran(&lid, &bdberr, __func__, __LINE__);
+    if (!tran)
+        return bdberr;
+    
+    logmsg(LOGMSG_USER, "++++++ %s : calling update gen_shard_update_inmem_db\n", __func__);
+    rc = gen_shard_update_inmem_db(tran, db, db->sqlaliasname);
+    if (rc != 0) {
+        logmsg(LOGMSG_ERROR, "REPLICANT FAILED TO UPDATE GENERIC SHARD INFO\n");
+    }
+    _untran(tran, lid);
+    return rc;
+}
 /* keep this in sync with enum scdone */
 int (*SCDONE_CALLBACKS[])(const char *, void *, scdone_t) = {
     &scdone_alter,         &scdone_addandfastinit, /* fastinit AND add (doh) */
@@ -1280,7 +1300,8 @@ int (*SCDONE_CALLBACKS[])(const char *, void *, scdone_t) = {
     &scdone_llmeta_queue,  &scdone_genid48,        &scdone_genid48,
     &scdone_lua_sfunc,     &scdone_lua_afunc,      &scdone_rename_table,
     &scdone_change_stripe, &scdone_user_view,      &scdone_queue_file,
-    &scdone_queue_file,    &scdone_rename_table,   &scdone_alias};
+    &scdone_queue_file,    &scdone_rename_table,   &scdone_alias,
+    &scdone_gen_shard};
 
 /* TODO fail gracefully now that inline? */
 /* called by bdb layer through a callback as a detached thread,
--- schemachange/sc_logic.c
+++ schemachange/sc_logic.c
@@ -315,12 +315,6 @@ int llog_scdone_rename_wrapper(bdb_state_type *bdb_state,
             mashup = alloca(oldlen + newlen);
             memcpy(mashup, s->tablename, oldlen); /* old */
             memcpy(mashup + oldlen, dst, newlen); /* new */
-        } else if (s->done_type == add && s->partition.type == PARTITION_ADD_GENSHARD) {
-            char *key = "dbnames";
-            newlen = strlen(key) + 1;
-            mashup = alloca(oldlen + newlen);
-            memcpy(mashup, s->tablename, oldlen);
-            memcpy(mashup + oldlen, key, newlen);
         }
     }
     if (!tran)
@@ -329,6 +323,15 @@ int llog_scdone_rename_wrapper(bdb_state_type *bdb_state,
     else
         rc = bdb_llog_scdone_tran(bdb_state, s->done_type, tran, mashup,
                                   oldlen + newlen, bdberr);
+
+    /* if this is generic sharding sc, make sure finalize also updates shard specific metadata and in-mem structures*/
+    if (s->partition.type == PARTITION_ADD_GENSHARD || s->partition.type == PARTITION_REM_GENSHARD) {
+        assert(tran!=NULL);
+        rc = bdb_llog_scdone_tran(bdb_state, gen_shard, tran, s->partition.u.genshard.tablename, strlen(s->partition.u.genshard.tablename) + 1, bdberr);
+        if (rc) {
+            logmsg(LOGMSG_ERROR, "bdb_llog_scdone failed for genshard %s. rc: %d, bdberr: %d\n", s->partition.u.genshard.tablename, rc, *bdberr);
+        }
+    }
 
     return rc;
 }
