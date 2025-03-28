c595c36b0a0b3328d44b6797d68d69a564d31e4e
Akshat Sikarwar <asikarwar1@bloomberg.net>
2025-03-28 11:34:31 -0400

    net: Undo revert
    Reapply "net: decom on base-thd so do_close invoked correctly"
    Reapply "net: Overwrite callback if close is in progress"
    Reapply "{178547928} net: Fix race in closing connection"
    
    This reverts commit 0b9d774d18e27bf4eb0f24f137b9f7f6b3bc44b5.
    This reverts commit 5c0af6896fd39d34c97ca464995384894dd6f8fd.
    This reverts commit c60394d6cf295390a85816d632b30975d8993d46.
    
    Signed-off-by: Akshat Sikarwar <asikarwar1@bloomberg.net>

--- net/net_int.h
+++ net/net_int.h
@@ -335,7 +335,7 @@ const uint8_t *net_wire_header_get(wire_header_type *, const uint8_t *, const ui
 uint8_t *net_wire_header_put(const wire_header_type *, uint8_t *, const uint8_t *);
 
 void add_host(host_node_type *);
-void decom(char *);
+void dispatch_decom(char *);
 void do_appsock(netinfo_type *, struct sockaddr_in *, SBUF2 *, uint8_t);
 int findpeer(int, char *, int);
 int get_dedicated_conhost(host_node_type *, struct in_addr *);
