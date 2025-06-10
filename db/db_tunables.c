37d5598cb6d317ea58cac02b5c13e27467a23bb6
Mark Hannum <mhannum72@gmail.com>
2025-06-10 14:46:38 -0400

    Revert "Forbid repopnewlrl requests from remote machines"
    This reverts commit ddbf9b28cdf324532c03e59fe7e1d6e5e479775f.
    
    Signed-off-by: Mark Hannum <mhannum72@gmail.com>

--- db/db_tunables.c
+++ db/db_tunables.c
@@ -299,7 +299,6 @@ extern int gbl_logdelete_lock_trace;
 extern int gbl_flush_log_at_checkpoint;
 extern int gbl_online_recovery;
 extern int gbl_forbid_remote_admin;
-extern int gbl_forbid_remote_repopnewlrl;
 extern int gbl_abort_on_dta_lookup_error;
 extern int gbl_debug_children_lock;
 extern int gbl_serialize_reads_like_writes;
