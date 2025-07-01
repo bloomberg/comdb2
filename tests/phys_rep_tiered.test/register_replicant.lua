11c3ff9421209468f8fea0ebec296968b43eda18
Mark Hannum <mhannum72@gmail.com>
2025-07-01 13:57:35 -0400

    register_replicant filters inactive nodes
    Signed-off-by: Mark Hannum <mhannum72@gmail.com>

--- tests/phys_rep_tiered.test/register_replicant.lua
+++
@@ -1,7 +0,0 @@
-local function main(dbname, node, lsn)
-    local tab, rc = db:table('tmp', {{'tier', 'integer'}, {'dbname', 'string'}, {'host', 'string'}})
-    local dbname = db:getdbname()
-    local resultset, rc = db:exec("select 0 as tier, " .. dbname .. " as dbname, host from comdb2_cluster")
-    resultset:emit()
---    tab:emit()
-end
