72513dbe6b20a0ed66a26b0422fd370832816cdf
Mark Hannum <mhannum72@gmail.com>
2025-09-12 15:33:04 -0400

    Physrep poll-time set by physrep_pollms tunable
    Signed-off-by: Mark Hannum <mhannum@bloomberg.net>

--- db/db_tunables.c
+++ db/db_tunables.c
@@ -510,6 +510,7 @@ extern int gbl_physrep_reconnect_interval;
 extern int gbl_physrep_shuffle_host_list;
 extern int gbl_physrep_ignore_queues;
 extern int gbl_physrep_max_rollback;
+extern int gbl_physrep_pollms;
 
 /* source-name / host is from lrl */
 extern char *gbl_physrep_source_dbname;
