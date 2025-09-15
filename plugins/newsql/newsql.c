e01e95da2555575383a5f92e14a0729a510f1c42
Dorin Hogea <dhogea@bloomberg.net>
2025-09-15 09:38:27 -0400

    remove leftover trace
    Signed-off-by: Dorin Hogea <dhogea@bloomberg.net>

--- plugins/newsql/newsql.c
+++ plugins/newsql/newsql.c
@@ -2479,9 +2479,7 @@ newsql_loop_result newsql_loop(struct sqlclntstate *clnt, CDB2SQLQUERY *sql_quer
 
     /* coherent  _or_ in middle of transaction */
     if ((!incoh_reject(clnt->admin, thedb->bdb_env) || clnt->features.allow_incoherent) || clnt->ctrl_sqlengine != SQLENG_NORMAL_PROCESS) {
-        uuidstr_t us;
         comdb2uuid(clnt->unifieduuid);
-        fprintf(stderr, "GENERATED %s for \"%s\"\n", comdb2uuidstr(clnt->unifieduuid, us), clnt->sql);
 
         return NEWSQL_SUCCESS;
     }
