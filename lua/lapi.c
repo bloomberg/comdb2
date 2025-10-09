b600d1e112a13a744685cf0336d5c68e5af3665b
Akshat Sikarwar <asikarwar1@bloomberg.net>
2025-10-09 13:07:37 -0400

    lint
    Signed-off-by: Akshat Sikarwar <asikarwar1@bloomberg.net>

--- lua/lapi.c
+++ lua/lapi.c
@@ -32,11 +32,12 @@
 #include "luaglue.h"
 
 
+#ifndef lint
 const char lua_ident[] =
   "$Lua: " LUA_RELEASE " " LUA_COPYRIGHT " $\n"
   "$Authors: " LUA_AUTHORS " $\n"
   "$URL: www.lua.org $\n";
-
+#endif
 
 
 #define api_checknelems(L, n)	api_check(L, (n) <= (L->top - L->base))
