faaa429a3205352eb49ef7cd06a07e0b94fb1e8e
Shoumyo Chakravorti <schakravorti@bloomberg.net>
2025-10-28 21:01:45 +0000

    {178862843} fix(crc32c): avoid misaligned memory accesses
    This bug was initially reported by @hallfox (thank you!). The issue is that
    `crc32c_sse` may generate misaligned memory accesses because of the order of
    operations it performs:
    
    1. Compute the checksum until an 8 byte-aligned address is reached.
    2. Compute the checksum until an exact multiple of 1024 bytes is remaining.
    3. Compute the checksum for the remaining bytes in chunks of 1024 bytes.
    
    Unfortunately, Step 2 can kick us out of alignment, which breaks the
    requirement that we be at an alignment boundary at the beginning of Step 3.
    
    We can ensure alignment by doing the following instead:
    
    1. Compute the checksum until an 8 byte-aligned address is reached.
    2. Compute the checksum for as many 1024 byte chunks as possible.
    3. Compute the checksum for the remaining bytes.
    
    Reproducing the original issue uncovered another one, which was that some
    left-shifts performed in `crc32c_software` could result in overflow since we
    did not explicitly upcast the shifted bytes (`uint8_t`) to the target storage
    type (`uint32_t`). This is fixed by upcasting before shifting.
    
    Signed-off-by: Shoumyo Chakravorti <schakravorti@bloomberg.net>

--- crc32c/crc32c.c
+++ crc32c/crc32c.c
@@ -48,9 +48,9 @@ uint32_t crc32c_software(const uint8_t* buf, uint32_t sz, uint32_t crc)
 	while (buf < end) {
 		// read two little endian ints
 		uint32_t u32a, u32b;
-		u32a = (buf[0]<<0) | (buf[1]<<8) | (buf[2]<<16) | (buf[3]<<24);
+		u32a = ((uint32_t)buf[0]<<0) | ((uint32_t)buf[1]<<8) | ((uint32_t)buf[2]<<16) | ((uint32_t)buf[3]<<24);
 		buf += 4;
-		u32b = (buf[0]<<0) | (buf[1]<<8) | (buf[2]<<16) | (buf[3]<<24);
+		u32b = ((uint32_t)buf[0]<<0) | ((uint32_t)buf[1]<<8) | ((uint32_t)buf[2]<<16) | ((uint32_t)buf[3]<<24);
 		buf += 4;
 		crc ^= u32a;
 		uint32_t term1 = crc_tableil8_o88[crc & 0x000000FF] ^ crc_tableil8_o80[(crc >> 8) & 0x000000FF];
@@ -190,17 +190,21 @@ static inline uint32_t crc32c_8s(const uint8_t *buf, uint32_t sz, uint32_t crc)
 static uint32_t crc32c_sse(const uint8_t *buf, uint32_t sz, uint32_t crc)
 {
 	crc = crc32c_until_aligned(&buf, &sz, crc);
+
+	while (sz >= 1024) {
+		crc = crc32c_1024_sse_int(buf, crc);
+		buf += 1024;
+		sz -= 1024;
+	}
+
 	uint32_t i = sz % 1024;
-	if (i) {
-		sz -= i;
+
+	if (i > 0) {
 		crc = crc32c_8s(buf, i, crc);
 		buf += i;
-		i = 0;
+		sz -= i;
 	}
-	while (i < sz) {
-		crc = crc32c_1024_sse_int(&buf[i], crc);
-		i += 1024;
-	}
+
 	return crc;
 }
 
@@ -394,83 +398,4 @@ uint32_t crc32c_comdb2(const uint8_t* buf, uint32_t sz)
     return crc32c_func(buf, sz, CRC32C_SEED);
 }
 
-#endif
-
-
-
-#if TEST_CRC32C
-
-int logmsg(loglvl lvl, const char *fmt, ...) {
-    va_list args;
-    va_start(args, fmt);
-    int ret = printf(fmt, args);
-    va_end(args);
-    return ret;
-}
-
-#include <assert.h>
-#include <sys/time.h>
-
-void timediff(const char * s) {
-    static struct timeval tv;
-    struct timeval tmp;
-
-    gettimeofday(&tmp, NULL);
-    int sec = (tmp.tv_sec - tv.tv_sec)*1000000;
-    int usec = (tmp.tv_usec - tv.tv_usec);
-    if (tv.tv_sec)
-        printf("%20.20s diff = %12.dusec\n", s, sec + usec);
-    tv = tmp;
-}
-
-__attribute__((noinline)) int f(int a)
-{
-    return a;
-}
-
-int main()
-{
-    crc32c_init(1);
-
-    printf("Test that NULL does not crash hw %d", crc32c_comdb2(NULL, 0)); printf("...check\n");
-
-    printf("''   sw 0x%x hw 0x%x", crc32c_software((const uint8_t*)"", 0, 0), crc32c_comdb2((const uint8_t*)"", 0));
-    if (crc32c_comdb2((const uint8_t*)"", 0) != 0x0) {
-        printf("...crc2c for '' not correct\n");
-        exit(1);
-    } else
-        printf("...check\n");
-
-    printf("'a'  sw 0x%x hw 0x%x", crc32c_software((const uint8_t*)"a", 1, 0), crc32c_comdb2((const uint8_t*)"a", 1));
-    if (crc32c_comdb2((const uint8_t*)"a", 1) != 0x93ad1061) {
-        printf("...crc2c for 'a' not correct\n");
-        exit(1);
-    } else
-        printf("...check\n");
-#define MAXLEN 1<<15
-    uint8_t lbuf[MAXLEN];
-    int i;
-    lbuf[0] = 'a';
-
-    for(i = 1; i < MAXLEN; i++)
-        lbuf[i] = i;
-    timediff("start");
-
-    for(i = 1; i < MAXLEN; i++) {
-        int a = crc32c_software(lbuf, i, 0);
-        f(a);
-    }
-    timediff("software: ");
-    for(i = 1; i < MAXLEN; i++) {
-        int a = crc32c_comdb2(lbuf, i);
-        f(a);
-    }
-    timediff("hardware: ");
-
-    for(i = 1; i < MAXLEN; i++) {
-        assert(crc32c_software(lbuf, i, 0) == crc32c_comdb2(lbuf, i));
-    }
-    printf("successfully tested %d strings\n", i);
-    return 0;
-}
 #endif
