4376e9f2816bab7fc3416c3ae7001398e15ebf50
Rivers Zhang <hzhang320@bloomberg.net>
2025-10-29 13:56:01 -0400

    Do not overwrite test certificates
    The cdb2jdbc testcase regenerates and overwrites certificates in the same test directory,
    interfering with other tests on the fly.
    
    This patch changes the cdb2jdbc testcase to generate certificates in its own directory.
    
    Signed-off-by: Rivers Zhang <hzhang320@bloomberg.net>

--- tests/tools/keygen.sh
+++ tests/tools/keygen.sh
@@ -25,7 +25,7 @@ fi
 # Setup ssl certificate
 # Create root key
 openssl genrsa -out $CADIR/root.key 4096
-chmod 700 $CADIR/root.key
+chmod 600 $CADIR/root.key
 # Create and self sign the root certificate
 openssl req -x509 -new -nodes -key $CADIR/root.key -days 30 -out $CADIR/root.crt \
             -subj "/C=US/ST=New York/L=New York/O=Bloomberg/OU=Comdb2/CN=$CN"
@@ -85,7 +85,7 @@ openssl req -new -key $CADIR/server.key -out $CADIR/server.key.csr \
 openssl x509 -req -in $CADIR/server.key.csr -CA $CADIR/root.crt -CAkey $CADIR/root.key \
              -CAcreateserial -out $CADIR/server.crt -days 10
 # Change key permissions
-chmod 700 $CADIR/server.key
+chmod 600 $CADIR/server.key
 
 # Create client key
 openssl genrsa -out $CADIR/client.key 4096
@@ -96,7 +96,7 @@ openssl req -new -key $CADIR/client.key -out $CADIR/client.key.csr \
 openssl x509 -req -in $CADIR/client.key.csr -CA $CADIR/root.crt -CAkey $CADIR/root.key \
              -CAserial $CADIR/root.srl -out $CADIR/client.crt -days 10
 # Change key permissions
-chmod 700 $CADIR/client.key
+chmod 600 $CADIR/client.key
 
 # Create revoked key
 openssl genrsa -out $CADIR/revoked.key 4096
@@ -107,7 +107,7 @@ openssl req -new -key $CADIR/revoked.key -out $CADIR/revoked.key.csr \
 openssl x509 -req -in $CADIR/revoked.key.csr -CA $CADIR/root.crt -CAkey $CADIR/root.key \
              -CAserial $CADIR/root.srl -out $CADIR/revoked.crt -days 10
 # Change key permissions
-chmod 700 $CADIR/revoked.key
+chmod 600 $CADIR/revoked.key
 ## Revoke
 openssl ca -config $CADIR/ca.cnf -revoke $CADIR/revoked.crt \
            -keyfile $CADIR/root.key -cert $CADIR/root.crt
@@ -157,9 +157,7 @@ openssl req -new -key $CADIR/san.key -out $CADIR/san.key.csr \
 openssl x509 -req -in $CADIR/san.key.csr -CA $CADIR/root.crt -CAkey $CADIR/root.key \
              -CAcreateserial -out $CADIR/san.crt -days 10 -extensions v3_req -extfile $CADIR/san.cnf
 # Change key permissions
-chmod 700 $CADIR/san.key
-cp $CADIR/san.crt /tmp/san.crt
-cp $CADIR/san.key /tmp/san.key
+chmod 600 $CADIR/san.key
 
 myhostname=`hostname`
 # copy over SSL certificate and change permission on private key
@@ -188,12 +186,12 @@ for node in $CLUSTER; do
   openssl x509 -req -in $CADIR/server_$fqdn.key.csr -CA $CADIR/root.crt -CAkey $CADIR/root.key \
                -CAcreateserial -out $CADIR/server_$fqdn.crt -days 10 ${extensions}
   # Change key permissions
-  chmod 700 $CADIR/server_$fqdn.key
+  chmod 600 $CADIR/server_$fqdn.key
 
   scp -o StrictHostKeyChecking=no $CADIR/server_$fqdn.crt $node:$CADIR/server.crt
   scp -o StrictHostKeyChecking=no $CADIR/server_$fqdn.key $node:$CADIR/server.key
-  scp -o StrictHostKeyChecking=no $CADIR/san.crt $node:/tmp/san.crt
-  scp -o StrictHostKeyChecking=no $CADIR/san.key $node:/tmp/san.key
+  scp -o StrictHostKeyChecking=no $CADIR/san.crt $node:$CADIR/san.crt
+  scp -o StrictHostKeyChecking=no $CADIR/san.key $node:$CADIR/san.key
   scp -o StrictHostKeyChecking=no $CADIR/root.crt $CADIR/root.crl $node:$CADIR
 done
 
