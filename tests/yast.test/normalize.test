bloomberg/comdb2

commit 0ca7333d9c71603115bbad22fdb436ad9cc01304

Treat quoted string literals after from clause as table identifiers
Signed-off-by: lzhu286 <lzhu286@bloomberg.net>

sqlite/src/tokenize.c
@@ -810,6 +810,21 @@ static int isLeftOperand(int tokenType){
   }
 }
 
+/*
+** This function should return non-zero if the specified token type is
+** a keyword that requires the next token to be a table identifier.
+*/
+static int requiresTableId(int tokenType){
+  switch( tokenType ){
+    case TK_FROM:
+    case TK_JOIN:
+    case TK_INTO:
+    case TK_UPDATE:
+    case TK_TABLE:    return 1;
+    default:          return 0;
+  }
+}
+
 char *sqlite3Normalize_alternate(
   Vdbe *pVdbe,       /* VM being reprepared */
   const char *zSql,  /* The original SQL string */
@@ -827,6 +842,7 @@ char *sqlite3Normalize_alternate(
   int nParenAtIN;    /* Value of nParent at start of RHS of IN operator */
   int j;             /* Bytes of normalized SQL generated so far */
   sqlite3_str *pStr; /* The normalized SQL string under construction */
+  int useStrId = 0;  /* Treat string token as identifier */
 
   db = pVdbe ? sqlite3VdbeDb(pVdbe) : 0;
   tokenType = -1;
@@ -848,9 +864,7 @@ char *sqlite3Normalize_alternate(
           sqlite3_str_append(pStr, " NULL", 5);
           break;
         }
-        /* Fall through */
       }
-      case TK_STRING:
       case TK_INTEGER:
       case TK_FLOAT:
       case TK_VARIABLE:
@@ -878,6 +892,7 @@ char *sqlite3Normalize_alternate(
         sqlite3_str_append(pStr, ")", 1);
         break;
       }
+      case TK_STRING:
       case TK_ID: {
         iStartIN = 0;
         j = pStr->nChar;
@@ -887,7 +902,8 @@ char *sqlite3Normalize_alternate(
           int eType = 0;
           if( zId==0 ) break;
           sqlite3Dequote(zId);
-          if( zSql[i]=='"' && sqlite3VdbeUsesDoubleQuotedString(pVdbe, zId, iDefDqId) ){
+          int maybeStrLiteral = zSql[i] == '\'' || ( zSql[i]=='"' && sqlite3VdbeUsesDoubleQuotedString(pVdbe, zId, iDefDqId) );
+          if ( maybeStrLiteral && !useStrId ){
             sqlite3_str_append(pStr, "?", 1);
             sqlite3DbFree(db, zId);
             break;
@@ -927,6 +943,7 @@ char *sqlite3Normalize_alternate(
         /* fall through */
       }
       default: {
+        useStrId = requiresTableId(tokenType);
         if( sqlite3IsIdChar(zSql[i]) ) addSpaceSeparator(pStr);
         j = pStr->nChar;
         sqlite3_str_append(pStr, zSql+i, n);

tests/yast.test/normalize.test
@@ -417,6 +417,31 @@ foreach {tnum sql flags norm} {
   {SELECT -1-1+abs(+1);}
   0x2
   {0 {SELECT?-?+abs(?);}}
+
+  1210
+  {CREATE TABLE 't1' (a int UNIQUE);}
+  0x2
+  {0 {CREATE TABLE t1(a int UNIQUE);}}
+
+  1220
+  {INSERT INTO 't1' ('a') VALUES (1);}
+  0x2
+  {0 {INSERT INTO t1(a)VALUES(?);}}
+
+  1230
+  {UPDATE 't1' SET 'a'=2 WHERE 'a'=1;}
+  0x2
+  {0 {UPDATE t1 SET?=?WHERE?=?;}}
+
+  1240
+  {SELECT 'a' FROM 't1' JOIN 't2' ON 't1.a'='t2.b';}
+  0x2
+  {0 {SELECT?FROM t1 JOIN t2 ON?=?;}}
+
+  1250
+  {SELECT "t1" FROM "t1";}
+  0x2
+  {0 {SELECT?FROM t1;}}
 } {
   do_test $tnum {
     set code [catch {

commit 95af72d0145baa273fc997d7272990cca2a09e22

{181357957} fix(normalize): consolidate string literals in `IN` clauses
Prior to this fix, it was observed that string literals prevented `IN` clauses
from being consolidated in fingerprints:

```sql
testdb> select 'a' where 'a' in ();
testdb> select 'a' where 'a' in ('b');
testdb> select 'a' where 'a' in ('b', 'c');
testdb> select 'a' where 'a' in ('b', 'c', 'd');
testdb> select 'a' where 'a' in ('b', 'c', 'd', 'e');
testdb>
testdb> select fingerprint, count, normalized_sql from comdb2_fingerprints;
(fingerprint='cf0f87fad75d1874619c28808ebabe4a', count=1, normalized_sql='SELECT?WHERE?IN(?);')
(fingerprint='0dfdc160117b48efefcbaf8ddf0ad174', count=1, normalized_sql='SELECT?WHERE?IN(?,?);')
(fingerprint='11c33d8b5523d3d12a290f12cdd7be12', count=2, normalized_sql='SELECT?WHERE?IN(?,?,?);')
(fingerprint='b8fcde675ccf4c7d1d5a834fada58ec9', count=1, normalized_sql='SELECT?WHERE?IN(?,?,?,?);')
```

All of the above queries _should_ have the same fingerprint.

This likely regressed in 0ca7333d, which attempted to fix a different problem by
treating string literals similarly to identifiers. Unfortunately, when we see an
identifier, we disable the consolidation of `IN` clauses so that the identifier
doesn't get clobbered.  This behavior is not desirable for string literals.

Ensure that consolidation is disabled only if we see an identifier (quoted or
otherwise) in an `IN` clause, not just any string. New behavior:

```sql
testdb> select 'a' where 'a' in ();
testdb> select 'a' where 'a' in ('b');
testdb> select 'a' where 'a' in ('b', 'c');
testdb> select 'a' where 'a' in ('b', 'c', 'd');
testdb> select 'a' where 'a' in ('b', 'c', 'd', 'e');
testdb>
testdb> select fingerprint, count, normalized_sql from comdb2_fingerprints;
(fingerprint='11c33d8b5523d3d12a290f12cdd7be12', count=5, normalized_sql='SELECT?WHERE?IN(?,?,?);')
```

Signed-off-by: Shoumyo Chakravorti <schakravorti@bloomberg.net>

sqlite/src/tokenize.c
@@ -873,7 +873,7 @@ char *sqlite3Normalize_alternate(
   int prevType = 0;  /* Previous non-whitespace token */
   int nParen;        /* Number of nested levels of parentheses */
   int iStartIN;      /* Start of RHS of IN operator in z[] */
-  int nParenAtIN;    /* Value of nParent at start of RHS of IN operator */
+  int nParenAtIN;    /* Value of nParen at start of RHS of IN operator */
   int j;             /* Bytes of normalized SQL generated so far */
   sqlite3_str *pStr; /* The normalized SQL string under construction */
   int useStrId = 0;  /* Treat string token as identifier */
@@ -928,32 +928,51 @@ char *sqlite3Normalize_alternate(
       }
       case TK_STRING:
       case TK_ID: {
-        iStartIN = 0;
         j = pStr->nChar;
         if( sqlite3Isquote(zSql[i]) ){
+          /* Dequote the token to get just the identifier. */
           char *zId = sqlite3_mprintf("%.*s", n, zSql+i);
-          int nId;
-          int eType = 0;
           if( zId==0 ) break;
           sqlite3Dequote(zId);
-          int maybeStrLiteral = zSql[i] == '\'' || ( zSql[i]=='"' && sqlite3VdbeUsesDoubleQuotedString(pVdbe, zId, iDefDqId) );
-          if ( maybeStrLiteral && !useStrId ){
+
+          /* If it is a string literal, replace it with a placeholder. */
+          int isSingleQuotedLiteral = ( zSql[i]=='\'' );
+          int isDoubleQuotedLiteral = ( zSql[i]=='"'  && sqlite3VdbeUsesDoubleQuotedString(pVdbe, zId, iDefDqId) );
+          int isLiteral = ( isSingleQuotedLiteral || isDoubleQuotedLiteral ) && !useStrId;
+
+          if ( isLiteral ){
             sqlite3_str_append(pStr, "?", 1);
             sqlite3DbFree(db, zId);
             break;
           }
-          nId = sqlite3Strlen30(zId);
+
+          /* This is definitely an identifier. Re-tokenize the dequoted
+          ** string to check whether it contains any internal double quotes.
+          ** If it does, re-quote it and escape the internal double quotes. */
+          int eType = 0;
+          int nId = sqlite3Strlen30(zId);
+
           if( sqlite3GetToken((u8*)zId, &eType)==nId && eType==TK_ID ){
             addSpaceSeparator(pStr);
             sqlite3_str_append(pStr, zId, nId);
           }else{
             sqlite3_str_appendf(pStr, "\"%w\"", zId);
           }
+
           sqlite3DbFree(db, zId);
         }else{
+          /* Not quoted, this is definitely an identifier. */
           addSpaceSeparator(pStr);
           sqlite3_str_append(pStr, zSql+i, n);
         }
+
+        /* At this point, we know for sure we have an identifier and not a
+        ** string literal. If we are in an IN clause, prevent the identifier
+        ** from being clobbered with '?,?,?' later. */
+        iStartIN = 0;
+
+        /* Identifiers are case insensitive, "select a from t" and
+        ** "select A from t" should have the same normalized SQL. */
         while( j<pStr->nChar ){
           pStr->zText[j] = sqlite3Tolower(pStr->zText[j]);
           j++;

sqlite/src/vdbeaux.c
@@ -122,8 +122,8 @@ void sqlite3VdbeAddDblquoteStr(sqlite3 *db, Vdbe *p, const char *z){
 
 #ifdef SQLITE_ENABLE_NORMALIZE
 /*
-** zId of length nId is a double-quoted identifier.  Check to see if
-** that identifier is really used as a string literal.
+** zId of length nId is a double-quoted identifier. Return non-zero
+** if that identifier is really used as a string literal.
 */
 int sqlite3VdbeUsesDoubleQuotedString(
   Vdbe *pVdbe,            /* The prepared statement */

tests/yast.test/normalize.test
@@ -452,6 +452,175 @@ foreach {tnum sql flags norm} {
   {SELECT * FROM comdb2_transaction_logs('{0:1}') LIMIT 1}
   0x2
   {0 {SELECT*FROM comdb2_transaction_logs(?)LIMIT?;}}
+
+  1280
+  {
+    -- 'a' is a string literal, "a" is an identifier (column name).
+    -- 'h' and "h" are both string literals because t1 has no column
+    -- with that name.
+    SELECT 'a', "a", [b], `c`, 'h', "h" FROM t1
+  }
+  0x2
+  {0 {SELECT?,a,b,c,?,?FROM t1;}}
+
+  1290
+  {
+    -- IN clause contains identifiers.
+    SELECT a FROM t1 WHERE b IN (c, d)
+  }
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(c,d);}}
+
+  1291
+  {SELECT a FROM t1 WHERE b IN ("c", "d")}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(c,d);}}
+
+  1292
+  {SELECT a FROM t1 WHERE b IN ([c], [d])}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(c,d);}}
+
+  1293
+  {SELECT a FROM t1 WHERE b IN (`c`, `d`)}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(c,d);}}
+
+  1310
+  {
+    -- IN clause contains quoted and unquoted identifiers.
+    SELECT a FROM t1 WHERE b IN (c, "d", [e], `w`)
+  }
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(c,d,e,w);}}
+
+  1311
+  {SELECT a FROM t1 WHERE b IN ("c", [d], `e`, w)}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(c,d,e,w);}}
+
+  1312
+  {SELECT a FROM t1 WHERE b IN ([c], `d`, e, "w")}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(c,d,e,w);}}
+
+  1313
+  {SELECT a FROM t1 WHERE b IN (`c`, d, "e", [w])}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(c,d,e,w);}}
+
+  1330
+  {
+    -- IN clause contains string literals and unquoted identifiers.
+    SELECT a FROM t1 WHERE b IN ('a', c)
+  }
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(?,c);}}
+
+  1331
+  {SELECT a FROM t1 WHERE b IN ("foo", c)}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(?,c);}}
+
+  1332
+  {SELECT a FROM t1 WHERE b IN (c, 'a')}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(c,?);}}
+
+  1333
+  {SELECT a FROM t1 WHERE b IN (c, "foo")}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(c,?);}}
+
+  1340
+  {
+    -- IN clause contains string literals and quoted identifiers.
+    SELECT a FROM t1 WHERE b IN ('a', "c")
+  }
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(?,c);}}
+
+  1341
+  {SELECT a FROM t1 WHERE b IN ("foo", [c])}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(?,c);}}
+
+  1342
+  {SELECT a FROM t1 WHERE b IN (`c`, 'a')}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(c,?);}}
+
+  1343
+  {SELECT a FROM t1 WHERE b IN ("c", "foo")}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(c,?);}}
+
+  1350
+  {
+    -- IN clause contains a string literal with the same value
+    -- as the name of an identifier.
+    SELECT a FROM t1 WHERE b IN (a, 'a')
+  }
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(a,?);}}
+
+  1351
+  {SELECT a FROM t1 WHERE b IN ('a', a)}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(?,a);}}
+
+  1352
+  {SELECT a FROM t1 WHERE b IN ("a", 'a')}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(a,?);}}
+
+  1353
+  {SELECT a FROM t1 WHERE b IN ('a', "a")}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(?,a);}}
+
+  1354
+  {SELECT a FROM t1 WHERE b IN ('a', [a])}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(?,a);}}
+
+  1355
+  {SELECT a FROM t1 WHERE b IN (`a`, 'a')}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(a,?);}}
+
+  1360
+  {
+    -- String literals in IN clauses should be consolidated.
+    SELECT a FROM t1 WHERE b IN ('foo', "bar", 'baz', "qux", 'quux')
+  }
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(?,?,?);}}
+
+  1361
+  {SELECT a FROM t1 WHERE b IN ('foo', "bar", 'baz', "qux")}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(?,?,?);}}
+
+  1362
+  {SELECT a FROM t1 WHERE b IN ('foo', "bar", 'baz')}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(?,?,?);}}
+
+  1363
+  {SELECT a FROM t1 WHERE b IN ('foo', "bar")}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(?,?,?);}}
+
+  1364
+  {SELECT a FROM t1 WHERE b IN ('foo')}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(?,?,?);}}
+
+  1365
+  {SELECT a FROM t1 WHERE b IN ()}
+  0x2
+  {0 {SELECT a FROM t1 WHERE b IN(?,?,?);}}
 } {
   do_test $tnum {
     set code [catch {