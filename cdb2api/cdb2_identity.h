298f64e2c2c09fef0b487f4b23490ec26f36be6d
Salil Chandra <schandra107@bloomberg.net>
2026-02-24 17:30:58 -0500

    Port cdb2api thread changes
    Signed-off-by: Salil Chandra <schandra107@bloomberg.net>

---
+++ cdb2api/cdb2_identity.h
@@ -0,0 +1,11 @@
+/*
+ * CDB2 IDENTITY
+ *
+ */
+
+#ifndef INCLUDED_CDB2_IDENTITY_H
+#define INCLUDED_CDB2_IDENTITY_H
+
+enum cdb2_identity_flags { CDB2_USE_OPTIONAL_IDENTITY = 1, CDB2_NON_THREADED_IDENTITY = 1 << 1 };
+
+#endif
