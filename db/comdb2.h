bc094f8d794828a7e7189922e910c747068403c2
mkhullar <mohit.khullar@gmail.com>
2026-03-12 12:27:52 -0400

    Allow option to use passwords with IAM for transition
    Signed-off-by: mkhullar <mohit.khullar@gmail.com>

--- db/comdb2.h
+++ db/comdb2.h
@@ -1654,6 +1654,7 @@ extern int gbl_vtab_externalauth;
 extern int gbl_uses_simpleauth;
 extern int gbl_uses_externalauth_connect;
 extern int gbl_externalauth_warn;
+extern int gbl_passwords_with_externalauth;
 extern int gbl_identity_cache_max;
 extern int gbl_authorization_cache_max;
 extern int gbl_authentication_cache_ageout;
