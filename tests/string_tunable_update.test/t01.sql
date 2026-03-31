5c8f5c460e6ac23a3d585d3d38eb077ce5a76bf6
Emelia Lei <wlei29@bloomberg.net>
2026-03-31 13:56:52 -0400

    fix test comment typo
    Signed-off-by: Emelia Lei <wlei29@bloomberg.net>

--- tests/string_tunable_update.test/t01.sql
+++ tests/string_tunable_update.test/t01.sql
@@ -1,4 +1,4 @@
-# Verify that updating a string tunable with initial value
-# but no update handler at runtime does not crash.
+-- Verify that updating a string tunable with initial value
+-- but no update handler at runtime does not crash.
 PUT TUNABLE debug_default_string_update 'comdb2-string-tunable-test-runtime';
 SELECT value FROM comdb2_tunables WHERE name = 'debug_default_string_update';
