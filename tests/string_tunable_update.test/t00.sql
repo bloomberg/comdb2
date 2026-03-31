bd265850114044d21dfe3b890abfd285fff887ae
Emelia Lei <wlei29@bloomberg.net>
2026-03-31 13:56:52 -0400

    fix test comment typo
    Signed-off-by: Emelia Lei <wlei29@bloomberg.net>

--- tests/string_tunable_update.test/t00.sql
+++ tests/string_tunable_update.test/t00.sql
@@ -1,4 +1,4 @@
-# Verify that setting a string tunable via LRL does not crash.
+-- Verify that setting a string tunable via LRL does not crash.
 SELECT value FROM comdb2_tunables WHERE name = 'iam_metrics_namespace';
 PUT TUNABLE mask_internal_tunables 0
 SELECT value FROM comdb2_tunables WHERE name = 'debug_default_string_update';
