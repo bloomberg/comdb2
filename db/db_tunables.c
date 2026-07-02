5f6a4a8c0e95c7ddeda5bc5deaebf06e19ac32dd
Rishitha Kalicheti <rkalicheti1@bloomberg.net>
2026-07-02 14:56:13 -0400

    reproduce and fix race condition push_clnt_cols, and l_send_back_row
    Signed-off-by: Rishitha Kalicheti <rkalicheti1@bloomberg.net>
    
    fix failing tests, add tunable to the tunables.test
    
    Signed-off-by: Rishitha Kalicheti <rkalicheti1@bloomberg.net>

--- db/db_tunables.c
+++ db/db_tunables.c
@@ -178,6 +178,7 @@ extern int gbl_llmeta_deadlock_poll;
 extern int gbl_test_scindex_deadlock;
 extern int gbl_test_sc_resume_race;
 extern int gbl_test_sc_delay_after_unset_running;
+extern int gbl_test_emit_race_delay;
 extern int gbl_track_weighted_queue_metrics_separately;
 extern int gbl_typessql;
 extern int gbl_typessql_records_max;
