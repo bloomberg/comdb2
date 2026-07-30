78d9ac34dde991b5b25873f6b423421b79b8d8d5
Akshat Sikarwar <asikarwar1@bloomberg.net>
2026-07-30 12:55:57 -0400

    comdb2_tunables: access_flag = CDB2_ALLOW_ALL
    Signed-off-by: Akshat Sikarwar <asikarwar1@bloomberg.net>

--- sqlite/ext/comdb2/tunables.c
+++ sqlite/ext/comdb2/tunables.c
@@ -296,7 +296,7 @@ const sqlite3_module systblTunablesModule = {
     0,                        /* xRelease */
     0,                        /* xRollbackTo */
     0,                        /* xShadowName */
-    .access_flag = CDB2_ALLOW_USER,
+    .access_flag = CDB2_ALLOW_ALL,
 };
 
 #endif /* (!defined(SQLITE_CORE) || defined(SQLITE_BUILDING_FOR_COMDB2))       \
