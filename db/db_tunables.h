769a1a18d33f02a012f34edbe8cf2eb9553a4745
Rivers Zhang <hzhang320@bloomberg.net>
2026-09-19 05:36:18 -0400

    comdb2uuid: generate v7 uuid from a thread-local prng
    libuuid's `uuid_generate` goes through glibc random() which takes a
    process-global lock, serializing every id under concurrency. Emit
    RFC 9562 v7 (ms-timestamp prefix, time-ordered) from a per-thread prng,
    guarded by tunable uuid_v7. Route the SQL guid()/guidstr() and Lua
    db:guid()/db:guid_str() functions through comdb2uuid so they pick this
    up too. Add a benchmark inserting into a guid() dbstore table.
    
    Signed-off-by: Rivers Zhang <hzhang320@bloomberg.net>

--- db/db_tunables.h
+++ db/db_tunables.h
@@ -2300,6 +2300,8 @@ REGISTER_TUNABLE("ufid_log", "Generate ufid logs.  (Default: on)", TUNABLE_BOOLE
 
 REGISTER_TUNABLE("utxnid_log", "Generate utxnid logs. (Default: on)", TUNABLE_BOOLEAN, &gbl_utxnid_log,
                  NOARG|READEARLY, NULL, NULL, NULL, NULL);
+REGISTER_TUNABLE("uuid_v7", "Generate v7 uuids from a thread-local prng instead of libuuid. (Default: on)",
+                 TUNABLE_BOOLEAN, &gbl_uuid_v7, NOARG, NULL, NULL, NULL, NULL);
 
 REGISTER_TUNABLE("ufid_add_on_collect", "Add to ufid-hash on collect.  (Default: off)", TUNABLE_BOOLEAN, 
                  &gbl_ufid_add_on_collect, EXPERIMENTAL | INTERNAL | READONLY, NULL, NULL, NULL, NULL);
